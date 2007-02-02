d70cf3861667c5a5bca76a52d8dc909ad50394cb
matthew <matthew>
2007-02-02 21:39:21 +0000

    Submitting to multiple councils, so might not be an integer any more.

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@
 -- Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
 -- Email: matthew@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: schema.sql,v 1.18 2007-02-02 16:31:52 matthew Exp $
+-- $Id: schema.sql,v 1.19 2007-02-02 21:39:21 matthew Exp $
 --
 
 -- secret
@@ -125,7 +125,7 @@ create table problem (
     postcode text not null,
     easting double precision not null,
     northing double precision not null,
-    council integer references contacts(area_id),
+    council text not null, -- integer references contacts(area_id),
     title text not null,
     detail text not null,
     photo bytea,
