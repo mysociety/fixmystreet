c4d7a70f1955add9a496aceca91d6f8b5fd381d0
matthew <matthew>
2007-07-19 13:22:45 +0000

    Disable submit button when clicked.

--- web/js.js
+++ web/js.js
@@ -45,6 +45,7 @@ YAHOO.util.Event.onContentReady('map', function() {
 
 YAHOO.util.Event.onContentReady('mapForm', function() {
     this.onsubmit = function() {
+        this.submit_problem.disabled = true;
         this.x.value = x + 2;
         this.y.value = y + 2;
         return true;
