a29d29e79d3f48c02bda6d84978746cce220e7f2
matthew <matthew>
2009-07-10 16:54:51 +0000

    Translation of strings in DB used for RSS titles/descriptions.

---
+++ db/alert_types_eha.pl
@@ -0,0 +1,31 @@
+# This file is only here so that the strings from alert_types_eha.sql
+# get into the .po translation file.
+
+# New updates on a particular problem report
+    _('Updates on {{title}}'),
+    _('Updates on {{title}}'),
+    _('Update by {{name}}'),
+
+# New problems anywhere on the site
+    _('New reports on reportemptyhomes.com'),
+    _('The latest empty properties reported by users'),
+
+# New fixed problems anywhere on the site
+    _('Properties recently reported as put back to use on reportemptyhomes.com'),
+    _('The latest properties reported back to use by users'),
+
+# New problems around a location
+    _('New local reports on reportemptyhomes.com'),
+    _('The latest local reports reported by users'),
+
+# New problems sent to a particular council
+    _('New reports to {{COUNCIL}} on reportemptyhomes.com'),
+    _('The latest reports for {{COUNCIL}} reported by users'),
+
+# New problems within a particular ward sent to a particular council
+    _('New reports for {{COUNCIL}} within {{WARD}} ward on reportemptyhomes.com'),
+    _('The latest reports for {{COUNCIL}} within {{WARD}} ward reported by users'),
+
+# New problems within a particular voting area (ward, constituency, whatever)
+    _('New reports within {{NAME}}\'s boundary on reportemptyhomes.com'),
+    _('The latest reports within {{NAME}}\'s boundary reported by users'),
