464192a47cb1eb09a7128f1e7fc9f5c37e094f09
louise <louise>
2009-08-20 12:39:35 +0000

    Adding cobranding module

---
+++ perllib/Cobrand.pm
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+#
+# Cobrand.pm:
+# Cobranding for FixMyStreet.
+#
+# 
+# Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
+# Email: louise@mysociety.org. WWW: http://www.mysociety.org
+#
+# $Id: Cobrand.pm,v 1.1 2009-08-20 12:39:35 louise Exp $
+
+package Cobrand;
+
+use strict;
+use Carp;
+
+=item get_allowed_cobrands
+
+Return an array of allowed cobrand subdomains
+
+=cut
+sub get_allowed_cobrands{
+    my $allowed_cobrand_string = mySociety::Config::get('ALLOWED_COBRANDS');
+    my @allowed_cobrands = split(/:/, $allowed_cobrand_string);
+    return @allowed_cobrands;
+}
