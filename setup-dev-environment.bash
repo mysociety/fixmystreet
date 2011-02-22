36e118f44144e24e6f1a98ea845b4cc2fed799d8
Edmund von der Burg <evdb@mysociety.org>
2011-02-22 14:21:07 +0000

    Add note to prompt when env setup ignore local-lib5

--- setup-dev-environment.bash
+++ setup-dev-environment.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Set the Perl environment variables as needed
 eval $(perl -Iperllib -Mlocal::lib=local-lib5)
+
+# put a note in the promp so that we know environment is setup
+PS1="(fms) $PS1"
+
