b0ea7a45ef55e01a911121f3e2785a01bb619505
Edmund von der Burg <evdb@mysociety.org>
2011-02-22 18:13:19 +0000

    Added template handling

---
+++ perllib/FixMyStreet/App/View/Web.pm
@@ -0,0 +1,27 @@
+package FixMyStreet::App::View::Web;
+use base 'Catalyst::View::TT';
+
+use strict;
+use warnings;
+
+use FixMyStreet::App;
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.html',
+    INCLUDE_PATH       => [          #
+        FixMyStreet::App->path_to( 'templates', 'web' ),
+    ],
+    render_die => 1,
+);
+
+=head1 NAME
+
+FixMyStreet::App::View::Web - TT View for FixMyStreet::App
+
+=head1 DESCRIPTION
+
+TT View for FixMyStreet::App.
+
+=cut
+
+1;
