104b6966e080bb1db112c0c935a5bc3063a73ab5
Edmund von der Burg <evdb@mysociety.org>
2011-02-22 14:51:19 +0000

    add perllib to perl path in setup env script

--- setup-dev-environment.bash
+++ setup-dev-environment.bash
@@ -3,6 +3,9 @@
 # Set the Perl environment variables as needed
 eval $(perl -Iperllib -Mlocal::lib=local-lib5)
 
+# add the non-standard perllib path to PERL5LIB
+PERL5LIB=perllib:$PERL5LIB
+
 # put a note in the promp so that we know environment is setup
 PS1="(fms) $PS1"
 
