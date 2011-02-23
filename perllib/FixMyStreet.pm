b7df296229dcca8dd35f2138826e5415e0edede9
Edmund von der Burg <evdb@mysociety.org>
2011-02-23 18:47:07 +0000

    Allow access to the config through FixMyStreet

--- perllib/FixMyStreet.pm
+++ perllib/FixMyStreet.pm
@@ -4,6 +4,15 @@ use strict;
 use warnings;
 
 use Path::Class;
+my $ROOT_DIR = file(__FILE__)->parent->parent->absolute;
+
+use Readonly;
+
+use mySociety::Config;
+
+# load the config file and store the contents in a readonly hash
+mySociety::Config::set_file( __PACKAGE__->path_to("conf/general") );
+Readonly::Hash my %CONFIG, %{ mySociety::Config::get_list() };
 
 =head1 NAME
 
@@ -27,13 +36,30 @@ the FixMyStreet directory.
 
 =cut
 
-my $ROOT_DIR = file(__FILE__)->parent->parent->absolute;
-
 sub path_to {
-    my $self = shift;
+    my $class = shift;
     return $ROOT_DIR->file(@_);
 }
 
+=head2 config
 
+    my $config_hash_ref = FixMyStreet->config();
+    my $config_value    = FixMyStreet->config($key);
+
+Returns a hashref to the config values. This is readonly so any attempt to
+change it will fail.
+
+Or you can pass it a key and it will return the value for that key, or undef if
+it can't find it.
+
+=cut
+
+sub config {
+    my $class = shift;
+    return \%CONFIG unless scalar @_;
+
+    my $key = shift;
+    return exists $CONFIG{$key} ? $CONFIG{$key} : undef;
+}
 
 1;
