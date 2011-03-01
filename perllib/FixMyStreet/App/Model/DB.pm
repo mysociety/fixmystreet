c7b4bdbbe16f0cdb4196c08e2bf785cda5826948
Edmund von der Burg <evdb@mysociety.org>
2011-03-01 16:00:42 +0000

    Added FixMyStreet::DB as a model to FixMyStreet::App

---
+++ perllib/FixMyStreet/App/Model/DB.pm
@@ -0,0 +1,24 @@
+package FixMyStreet::App::Model::DB;
+use base 'Catalyst::Model::DBIC::Schema';
+
+use strict;
+use warnings;
+
+use FixMyStreet;
+
+__PACKAGE__->config(
+    schema_class => 'FixMyStreet::DB',
+    connect_info => FixMyStreet->dbic_connect_info,
+);
+
+=head1 NAME
+
+FixMyStreet::App::Model::DB - Catalyst DBIC Schema Model
+
+=head1 DESCRIPTION
+
+L<Catalyst::Model::DBIC::Schema> Model using schema L<FixMyStreet::DB>
+
+=cut
+
+1;
