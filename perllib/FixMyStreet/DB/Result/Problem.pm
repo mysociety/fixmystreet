632dd7b8e84faa4bcb19401678565751677729cc
Edmund von der Burg <evdb@mysociety.org>
2011-03-28 14:21:10 +0100

    Add 'abuse' table and put abuse logic into problem

--- perllib/FixMyStreet/DB/Result/Problem.pm
+++ perllib/FixMyStreet/DB/Result/Problem.pm
@@ -140,3 +140,28 @@ sub check_for_errors {
     return \%errors;
 }
 
+=head2 is_from_abuser
+
+    $bool = $problem->is_from_abuser(  );
+
+Returns true if the user's email or its domain is listed in the 'abuse' table.
+
+=cut
+
+sub is_from_abuser {
+    my $self = shift;
+
+    # get the domain
+    my $email = $self->user->email;
+    my ($domain) = $email =~ m{ @ (.*) \z }x;
+
+    # search for an entry in the abuse table
+    my $abuse_rs = $self->result_source->schema->resultset('Abuse');
+
+    return
+         $abuse_rs->find( { email => $email } )
+      || $abuse_rs->find( { email => $domain } )
+      || undef;
+}
+
+1;
