8ccb7ae193e7fe21a59929b61119689c7527c0f9
Struan Donald <struan@exo.org.uk>
2011-05-17 12:07:04 +0100

    more removal of ported code

--- perllib/FixMyStreet/App/Controller/Report.pm
+++ perllib/FixMyStreet/App/Controller/Report.pm
@@ -89,52 +89,18 @@ sub display : Path('') : Args(1) {
 #         $banner = $q->p({id => 'fixed'}, _('This problem has been fixed') . '.');
 #     }
 
-#     my $contact_url = Cobrand::url($cobrand, NewURL($q, -retain => 1, pc => undef, x => undef, 'y' => undef, -url=>'/contact?id=' . $input{id}), $q);
-#     my $back = Cobrand::url($cobrand, NewURL($q, -url => '/',
-#         lat => $short_lat, lon => $short_lon,
-#         -retain => 1, pc => undef, x => undef, 'y' => undef, id => undef
-#     ), $q);
 #     my $fixed = ($input{fixed}) ? ' checked' : '';
 
     $c->forward( 'format_problem_for_display' );
 #     my %vars = (
-#         unsuitable => $q->a({rel => 'nofollow', href => $contact_url}, _('Offensive? Unsuitable? Tell us')),
-#         more_problems => '<a href="' . $back . '">' . _('More problems nearby') . '</a>',
 #         url_home => Cobrand::url($cobrand, '/', $q),
-#         alert_link => Cobrand::url($cobrand, NewURL($q, -url => '/alert?type=updates;id='.$input_h{id}, -retain => 1, pc => undef, x => undef, 'y' => undef ), $q),
-#         subscribe => _('Subscribe'),
-#         blurb => _('Receive email when updates are left on this problem'),
-#         cobrand_form_elements1 => Cobrand::form_elements($cobrand, 'alerts', $q),
-#         form_alert_action => Cobrand::url($cobrand, '/alert', $q),
-#         rss_url => Cobrand::url($cobrand,  NewURL($q, -retain=>1, -url => '/rss/'.$input_h{id}, pc => undef, x => undef, 'y' => undef, id => undef), $q),
-#         rss_title => _('RSS feed'),
-#         rss_alt => _('RSS feed of updates to this problem'),
 #         field_errors => \%field_errors,
 #         add_alert_checked => ($input{add_alert} || !$input{submit_update}) ? ' checked' : '',
-#         post_label => _('Post'),
-#         cobrand_form_elements => Cobrand::form_elements($cobrand, 'updateForm', $q),
 #         form_action => Cobrand::url($cobrand, '/', $q),
-#         input_h => \%input_h,
 #     );
 #
 #     $vars{update_blurb} = $q->p($q->small(_('Please note that updates are not sent to the council. If you leave your name it will be public. Your information will only be used in accordance with our <a href="/faq#privacy">privacy policy</a>')))
 #         unless $q->{site} eq 'emptyhomes'; # No council blurb
-#
-#     if (@errors) {
-#         $vars{errors} = '<ul class="error"><li>' . join('</li><li>', @errors) . '</li></ul>';
-#     }
-#
-#     my $allow_photo_upload = Cobrand::allow_photo_upload($cobrand);
-#     if ($allow_photo_upload) {
-#         my $photo_label = _('Photo:');
-#         $vars{enctype} = ' enctype="multipart/form-data"';
-#         $vars{photo_element} = <<EOF;
-# <div id="fileupload_normalUI">
-# <label for="form_photo">$photo_label</label>
-# <input type="file" name="photo" id="form_photo">
-# </div>
-# EOF
-#     }
 #
 #     my %params = (
 #         rss => [ _('Updates to this problem, FixMyStreet'), "/rss/$input_h{id}" ],
@@ -230,6 +196,8 @@ sub format_problem_for_display : Private {
     $c->forward('generate_map_tags');
     $c->forward('generate_problem_photo');
     $c->forward('generate_problem_meta');
+
+    #         problem_updates => Page::display_problem_updates($input{id}, $q),
 
     return 1;
 }
