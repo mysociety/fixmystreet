52347ee7a0b02c12a277fe778b65c26eff2a73a3
Struan Donald <struan@exo.org.uk>
2011-06-06 19:33:03 +0100

    add admin_log table to models

---
+++ perllib/FixMyStreet/DB/Result/AdminLog.pm
@@ -0,0 +1,44 @@
+package FixMyStreet::DB::Result::AdminLog;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->load_components("FilterColumn", "InflateColumn::DateTime");
+__PACKAGE__->table("admin_log");
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "admin_log_id_seq",
+  },
+  "admin_user",
+  { data_type => "text", is_nullable => 0 },
+  "object_type",
+  { data_type => "text", is_nullable => 0 },
+  "object_id",
+  { data_type => "integer", is_nullable => 0 },
+  "action",
+  { data_type => "text", is_nullable => 0 },
+  "whenedited",
+  {
+    data_type     => "timestamp",
+    default_value => \"ms_current_timestamp()",
+    is_nullable   => 0,
+  },
+);
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-06-06 18:52:09
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:VKN3o9SHoDhG2/H5NInf2w
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
