49e90bb2b91fb145c0e4cc96b3976170a711361d
Matthew Somerville <matthew@mysociety.org>
2011-06-13 12:57:59 +0100

    Default cobrand has a moniker of an empty string, to match old data.

--- perllib/FixMyStreet/Cobrand/Default.pm
+++ perllib/FixMyStreet/Cobrand/Default.pm
@@ -39,7 +39,9 @@ the last part of the class name lowercased - eg 'F::C::SomeCobrand' becomes
 sub moniker {
     my $class = ref( $_[0] ) || $_[0];    # deal with object or class
     my ($last_part) = $class =~ m{::(\w+)$};
-    return lc($last_part);
+    $last_part = lc($last_part);
+    return '' if $last_part eq 'default';
+    return $last_part;
 }
 
 =head2 is_default
@@ -52,7 +54,7 @@ Returns true if this is the default cobrand, false otherwise.
 
 sub is_default {
     my $self = shift;
-    return $self->moniker eq 'default';
+    return $self->moniker eq '';
 }
 
 =head2 path_to_web_templates
