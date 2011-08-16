f93ff062c986847f97aef76673c2ca7742f1f125
Struan Donald <struan@exo.org.uk>
2011-08-16 10:43:43 +0100

    fix syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -180,7 +180,7 @@ create table problem (
     lastupdate timestamp not null default ms_current_timestamp(),
     whensent timestamp,
     send_questionnaire boolean not null default 't',
-    extra text -- extra fields required for open311
+    extra text, -- extra fields required for open311
     flagged boolean not null default 'f'
 );
 create index problem_state_latitude_longitude_idx on problem(state, latitude, longitude);
