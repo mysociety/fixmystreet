17bf62de05999e5afb886bb1d4530e017a2ac4af
Matthew Somerville <matthew@mysociety.org>
2012-01-13 18:23:04 +0000

    Give all council cobrands a common parent to use.

---
+++ perllib/FixMyStreet/Cobrand/UKCouncils.pm
@@ -0,0 +1,75 @@
+package FixMyStreet::Cobrand::UKCouncils;
+use base 'FixMyStreet::Cobrand::Default';
+
+use strict;
+use warnings;
+
+use Carp;
+use URI::Escape;
+
+sub site_restriction {
+    my $self = shift;
+    return ( "and council='" . $self->council_id . "'", $self->council_url, { council => sprintf('%d', $self->council_id) } );
+}
+
+sub problems_clause {
+    my $self = shift;
+    return { council => sprintf('%d', $self->council_id) };
+}
+
+sub problems {
+    my $self = shift;
+    return $self->{c}->model('DB::Problem')->search( $self->problems_clause );
+}
+
+sub base_url {
+    my $self = shift;
+    my $base_url = mySociety::Config::get('BASE_URL');
+    my $u = $self->council_url;
+    if ( $base_url !~ /$u/ ) {
+        $base_url =~ s{http://(?!www\.)}{http://$u.}g;
+        $base_url =~ s{http://www\.}{http://$u.}g;
+    }
+    return $base_url;
+}
+
+sub site_title {
+    my ($self) = @_;
+    return $self->council_name . ' FixMyStreet';
+}
+
+sub enter_postcode_text {
+    my ($self) = @_;
+    return 'Enter a ' . $self->council_area . ' postcode, or street name and area';
+}
+
+sub council_check {
+    my ( $self, $params, $context ) = @_;
+
+    my $councils = $params->{all_councils};
+    my $council_match = defined $councils->{$self->council_id};
+    if ($council_match) {
+        return 1;
+    }
+    my $url = 'http://www.fixmystreet.com/';
+    $url .= 'alert' if $context eq 'alert';
+    $url .= '?pc=' . URI::Escape::uri_escape( $self->{c}->req->param('pc') )
+      if $self->{c}->req->param('pc');
+    my $error_msg = "That location is not covered by " . $self->council_name . ".
+Please visit <a href=\"$url\">the main FixMyStreet site</a>.";
+    return ( 0, $error_msg );
+}
+
+# All reports page only has the one council.
+sub all_councils_report {
+    return 0;
+}
+
+sub recent_photos {
+    my ( $self, $num, $lat, $lon, $dist ) = @_;
+    $num = 2 if $num == 3;
+    return $self->problems->recent_photos( $num, $lat, $lon, $dist );
+}
+
+1;
+
--- t/00-check-config.t
+++ t/00-check-config.t
@@ -2,11 +2,12 @@ use strict;
 use warnings;
 
 use Test::More;
+use YAML;
 
 use FixMyStreet;
 
-# check that all the fields listed in general-example are also present in
-# general - helps prevent later test failures due to un-noticed additions to the
+# check that all the fields listed in general.yml-example are also present in
+# general.yml - helps prevent later test failures due to un-noticed additions to the
 # config file.
 
 # This code will bail_out to prevent the test suite proceeding to save time if
@@ -14,10 +15,8 @@ use FixMyStreet;
 
 # load the config file and store the contents in a readonly hash
 
-mySociety::Config::set_file( FixMyStreet->path_to("conf/general-example") );
-my $example_config = mySociety::Config::get_list();
-mySociety::Config::set_file( FixMyStreet->path_to("conf/general") );
-my $local_config = mySociety::Config::get_list();
+my $example_config = YAML::LoadFile( FixMyStreet->path_to("conf/general.yml-example") );
+my $local_config = YAML::LoadFile( FixMyStreet->path_to("conf/general.yml") );
 
 # find all keys missing from each config
 my @missing_from_example = find_missing( $example_config, $local_config );
@@ -25,12 +24,12 @@ my @missing_from_local   = find_missing( $local_config,   $example_config );
 
 if ( @missing_from_example || @missing_from_local ) {
 
-    fail "Missing from 'general': $_"         for @missing_from_local;
-    fail "Missing from 'general-example': $_" for @missing_from_example;
+    fail "Missing from 'general.yml': $_"         for @missing_from_local;
+    fail "Missing from 'general.yml-example': $_" for @missing_from_example;
 
     # bail out to prevent other tests failing due to config issues
     BAIL_OUT( "Config has changed"
-          . " - update your 'general' and add/remove the keys listed above" );
+          . " - update your 'general.yml' and add/remove the keys listed above" );
 }
 else {
     pass "configs contain the same keys";
