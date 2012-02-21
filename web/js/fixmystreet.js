b715d591301d219ffd35a2e8b4680c235fe5e78a
Josh Angell <josh@supercooldesign.co.uk>
2012-02-21 13:42:01 +0000

    Remove old js for email alert box
    Signed-off-by: Josh Angell <josh@supercooldesign.co.uk>

--- web/js/fixmystreet.js
+++ web/js/fixmystreet.js
@@ -181,32 +181,6 @@ $(function(){
         $('#form_name').addClass('required').removeClass('valid');
     } );
 
-    $('#email_alert').click(function(e) {
-        if (!$('#email_alert_box').length) {
-            return true;
-        }
-        e.preventDefault();
-        if ($('#email_alert_box').is(':visible')) {
-            email_alert_close();
-        } else {
-            var pos = $(this).position();
-            $('#email_alert_box').css( { 'left': ( pos.left - 20 ) + 'px', 'top': ( pos.top + 20 ) + 'px' } );
-            $('#email_alert_box').show('fast');
-            $('#alert_rznvy').focus();
-        }
-    }).hover(function() {
-        window.clearTimeout(timer);
-    }, function() {
-        timer = window.setTimeout(email_alert_close, 2000);
-    });
-
-    $('#email_alert_box').hover(function() {
-        window.clearTimeout(timer);
-    }, function() {
-        timer = window.setTimeout(email_alert_close, 2000);
-    });
-
-
     $('#form_category').change( form_category_onchange );
 
     // Geolocation
