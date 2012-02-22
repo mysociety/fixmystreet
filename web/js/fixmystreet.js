56c8c3f424ea9dd71ea251a31795145edd124bf1
Matthew Somerville <matthew@mysociety.org>
2012-02-22 15:28:11 +0000

    Make sure error divs go white when valid (fixes #222).

--- web/js/fixmystreet.js
+++ web/js/fixmystreet.js
@@ -144,7 +144,8 @@ $(function(){
         // we do this to stop things jumping around on blur
         success: function (err) { if ( form_submitted ) { err.addClass('label-valid').removeClass('label-valid-hidden').html( '&nbsp;' ); } else { err.addClass('label-valid-hidden'); } },
         errorPlacement: function( error, element ) {
-            element.parent('div').before( error );
+            var l = element.prev('label') || element;
+            l.before( error );
         },
         submitHandler: function(form) {
             if (form.submit_problem) {
