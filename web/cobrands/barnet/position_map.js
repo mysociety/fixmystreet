79887eeb1f6312adf8a10c62c08c5d9bbd02097a
Struan Donald <struan@exo.org.uk>
2012-04-30 15:31:34 +0100

    map display fixes for ie6

--- web/cobrands/barnet/position_map.js
+++ web/cobrands/barnet/position_map.js
@@ -2,13 +2,13 @@ function position_map_box() {
     var map_pos = 'absolute', map_height = '100%';
     if ($('html').hasClass('ie6')) {
         map_pos = 'absolute';
-        map_height = $(window).height();
+        map_height = $('.wrapper').height();
     }
     $('#map_box').prependTo('.wrapper').css({
         zIndex: 0, position: map_pos,
         top: $('.wrapper').top, left: $('.wrapper').left,
         right: $('.wrapper').right, bottom: $('.wrapper').bottom,
-        width: '100%', height: map_height,
+        width: '900px', height: map_height,
         margin: 0
     });
 }
