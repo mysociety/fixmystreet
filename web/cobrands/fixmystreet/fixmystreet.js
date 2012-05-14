6f80cdbf67468e50d0ca47a8cfbb4b593585fb56
Matthew Somerville <matthew@mysociety.org>
2012-05-14 15:24:18 +0100

    Include submit button input for JS submission, so that server directs correctly.

--- web/cobrands/fixmystreet/fixmystreet.js
+++ web/cobrands/fixmystreet/fixmystreet.js
@@ -503,6 +503,7 @@ $.fn.drawer = function(id, ajax) {
     $('.container').on('click', '#alert_email_button', function(e){
         e.preventDefault();
         var form = $('<form/>').attr({ method:'post', action:"/alert/subscribe" });
+        form.append($('<input name="alert" value="Subscribe me to an email alert" type="hidden" />'));
         $('#alerts input[type=text], #alerts input[type=hidden], #alerts input[type=radio]:checked').each(function() {
             var $v = $(this);
             $('<input/>').attr({ name:$v.attr('name'), value:$v.val(), type:'hidden' }).appendTo(form);
