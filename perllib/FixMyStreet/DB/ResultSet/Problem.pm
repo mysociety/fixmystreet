3aa48b3e93fa9388124550ede902b280beae7b33
Struan Donald <struan@exo.org.uk>
2012-05-22 17:29:57 +0100

    use correct photo url

--- perllib/FixMyStreet/DB/ResultSet/Problem.pm
+++ perllib/FixMyStreet/DB/ResultSet/Problem.pm
@@ -262,7 +262,7 @@ sub send_reports {
         $h{phone_line} = $h{phone} ? _('Phone:') . " $h{phone}\n\n" : '';
         if ($row->photo) {
             $h{has_photo} = _("This web page also contains a photo of the problem, provided by the user.") . "\n\n";
-            $h{image_url} = $email_base_url . '/photo?id=' . $row->id;
+            $h{image_url} = $email_base_url . '/photo/' . $row->id . '.full.jpeg';
         } else {
             $h{has_photo} = '';
             $h{image_url} = '';
