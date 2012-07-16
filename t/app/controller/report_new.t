mysociety/fixmystreet

commit b1ccde8a525f1e70b65a14f4d9b7073f16ebb648

incorporate new dashboard design

templates/web/default/dashboard/index.html
@@ -1,39 +1,52 @@
-[% 
+[% extra_css = BLOCK %]
+	    <link rel="stylesheet" href="[% version('/cobrands/fixmystreet/dashboard.css') %]">
+[% END %]
+
+[%
     INCLUDE 'header.html'
         title = loc('Dashboard')
         robots = 'noindex, nofollow'
         bodyclass = 'fullwidthpage'
 %]
 
-<style>
-    th[scope=row] { text-align: left; }
-    tr.subtotal { background-color: #eee; }
-    #overview tr:nth-child(2) { background-color: #fee; }
-    select { width: auto; }
-</style>
-
 <form>
 
-<p>Ward: <select name="ward"><option value=''>All</option>
-    [% FOR w IN children.values.sort('name') %]
-        <option value="[% w.id %]"[% ' selected' IF w.id == ward %]>[% w.name %]</option>
-    [% END %]
-</select>
+<hgroup>
+    <h2>Reports, Statistics and Actions for</h2>
+    <h1>[% council.name %]</h1>
+</hgroup>
 
-<p>Report category: <select name="category"><option value=''>All</option>
-    [% FOR cat_op IN category_options %]
-        <option value='[% cat_op | html %]'[% ' selected' IF category == cat_op %]>[% cat_op | html %]</option>
-    [% END %]
-    </select>
+<div class="filters">
+    <p>
+        <label for="ward">Ward:</label>
+        <select name="ward"><option value=''>All</option>
+            [% FOR w IN children.values.sort('name') %]
+                <option value="[% w.id %]"[% ' selected' IF w.id == ward %]>[% w.name %]</option>
+            [% END %]
+        </select>
+    </p>
 
-<p><input type="submit" value="Look up">
+    <p>
+        <label for="category">Report category:</label>
+        <select name="category"><option value=''>All</option>
+            [% FOR cat_op IN category_options %]
+                <option value='[% cat_op | html %]'[% ' selected' IF category == cat_op %]>[% cat_op | html %]</option>
+            [% END %]
+        </select>
+    </p>
 
-<h2>Summary Statistics for [% council.name %]</h2>
+    <p>
+        <input type="submit" value="Look up">
+    </p>
+
+    <br clear="all" />
+</div>
+
 
 <table width="100%" id="overview">
     <tr>
-        <td>&nbsp;</td>
-        <th scope="col">WTD</th>
+        <th>&nbsp;</th>
+        <th scope="col"><abbr title="Week To Date">WTD</abbr></th>
         <th scope="col">Last 7 days</th>
         <th scope="col">Last 4 weeks</th>
         <th scope="col">YTD</th>
@@ -147,6 +160,6 @@
 
 [% BLOCK list %]
 [% FOR p IN list %]
-<li><a href="/report/[% p.id %]">[% p.title | html %]</a></li>
+<li><a href="/report/[% p.id %]">[% p.title | html %]</a> <date>[% p.confirmed.dmy('/') %]</date></li>
 [% END %]
 [% END %]

web/cobrands/fixmystreet/dashboard.scss
@@ -1,0 +1,97 @@
+    select {
+        width: auto;
+    }
+
+    #overview {
+        margin-top: 1em;
+
+        th[scope=col] {
+            font-size:0.8em;
+        }
+
+        tr:nth-child(2) {
+            background-color:#fee;
+        }
+    }
+
+    th {
+        text-align:left;
+        &[scope=col] {
+            border-left:1px solid #ccc;
+            padding:10px 0 10px 10px;
+            font-weight:bold;
+        }
+
+        &[scope=row] {
+            font-weight: normal;text-align:left;
+            border-top:1px solid #ddd;
+            padding-left:10px;
+        }
+
+        abbr[title] {
+            border: none;
+        }
+    }
+
+    td {
+        width:10%;
+        border-left:1px solid #ccc;
+        border-top:1px solid #ddd;
+        padding-left:10px;
+    }
+
+    tr.subtotal {
+        background-color:#eee;
+        border-bottom:20px solid white;
+        border-top:2px solid #aaa;
+        font-weight: bold;
+
+        th {
+            font-weight: bold;
+        }
+    }
+
+
+    #reports{
+        th {
+            padding-left: 20px;
+            border-bottom: none;
+        }
+        date {
+            font-size: 0.7em;
+            color: #ccc;
+        }
+    }
+
+    .filters{
+        background-color:#ffec99;
+        margin:0 -1em;
+        border-top:#ffd000 solid 0.75em;
+        padding:0 1em;
+
+        p {
+            float:left;
+            padding:0 1em;
+            width:25%;
+            font-size:0.75em;
+            color:#333333;
+            input {
+                margin-top: 2.5em;
+            }
+        }
+        select {
+            width:100%;
+        }
+    }
+
+    hgroup {
+        h2 {
+            color:#737373;
+            font-size:1.25em;
+            margin-bottom:0.5em;
+        }
+        h1 {
+            color:#333;
+            font-size:2.25em;
+        }
+    }

commit 917aa4ec1d7a4d902396bae41c09a113c9faf2f7

A test for the previous commit 5778fd3 (passes there, fails in b1ccde8).

t/app/controller/report_new.t
@@ -67,11 +67,25 @@ my $contact5 = FixMyStreet::App->model('DB::Contact')->find_or_create( {
     category => 'Trees',
     email => 'trees@example.com',
 } );
+my $contact6 = FixMyStreet::App->model('DB::Contact')->find_or_create( {
+    %contact_params,
+    area_id => 2434, # Lichfield
+    category => 'Trees',
+    email => 'trees@example.com',
+} );
+my $contact7 = FixMyStreet::App->model('DB::Contact')->find_or_create( {
+    %contact_params,
+    area_id => 2240, # Lichfield
+    category => 'Street lighting',
+    email => 'highways@example.com',
+} );
 ok $contact1, "created test contact 1";
 ok $contact2, "created test contact 2";
 ok $contact3, "created test contact 3";
 ok $contact4, "created test contact 4";
 ok $contact5, "created test contact 5";
+ok $contact6, "created test contact 6";
+ok $contact7, "created test contact 7";
 
 # test that the various bit of form get filled in and errors correctly
 # generated.
@@ -949,10 +963,78 @@ for my $test (
     };
 }
 
+SKIP: {
+    skip( "Need 'lichfielddc' in ALLOWED_COBRANDS config", 100 )
+        unless FixMyStreet::Cobrand->exists('lichfielddc');
+
+    my $test_email = 'test-22@example.com';
+    $mech->host( 'http://lichfielddc.fixmystreet.com/' );
+    $mech->clear_emails_ok;
+    $mech->log_out_ok;
+
+    $mech->get_ok('/around');
+    $mech->submit_form_ok( { with_fields => { pc => 'WS13 7RD' } }, "submit location" );
+    $mech->follow_link_ok( { text_regex => qr/skip this step/i, }, "follow 'skip this step' link" );
+    $mech->submit_form_ok(
+        {
+            button      => 'submit_register',
+            with_fields => {
+                title         => 'Test Report',
+                detail        => 'Test report details.',
+                photo         => '',
+                name          => 'Joe Bloggs',
+                may_show_name => '1',
+                email         => $test_email,
+                phone         => '07903 123 456',
+                category      => 'Street lighting',
+            }
+        },
+        "submit good details"
+    );
+    is_deeply $mech->page_errors, [], "check there were no errors";
+
+    # check that the user has been created/ not changed
+    my $user =
+      FixMyStreet::App->model('DB::User')->find( { email => $test_email } );
+    ok $user, "user found";
+
+    # find the report
+    my $report = $user->problems->first;
+    ok $report, "Found the report";
+
+    # Check the report has been assigned appropriately
+    is $report->council, 2240;
+
+    # receive token
+    my $email = $mech->get_email;
+    ok $email, "got an email";
+    like $email->body, qr/confirm the problem/i, "confirm the problem";
+
+    my ($url) = $email->body =~ m{(http://\S+)};
+    ok $url, "extracted confirm url '$url'";
+
+    # confirm token
+    $mech->get_ok($url);
+    $report->discard_changes;
+    is $report->state, 'confirmed', "Report is now confirmed";
+
+    # Shouldn't be found, as it was a county problem
+    is $mech->get( '/report/' . $report->id )->code, 404, "report not found";
+
+    # But should be on the main site
+    $mech->host( 'www.fixmystreet.com' );
+    $mech->get_ok( '/report/' . $report->id );
+    is $report->name, 'Joe Bloggs', 'name updated correctly';
+
+    $mech->delete_user($user);
+}
+
 $contact1->delete;
 $contact2->delete;
 $contact3->delete;
 $contact4->delete;
 $contact5->delete;
+$contact6->delete;
+$contact7->delete;
 
 done_testing();