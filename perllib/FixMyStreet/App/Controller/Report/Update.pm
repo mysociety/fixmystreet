0bc2183e1d3b8cca7fab6ff87016c1ada63522a3
Matthew Somerville <matthew@mysociety.org>
2012-07-18 16:59:27 +0100

    Couple more URLs to right site (e.g. already answered questionnaire).

--- perllib/FixMyStreet/App/Controller/Report/Update.pm
+++ perllib/FixMyStreet/App/Controller/Report/Update.pm
@@ -348,7 +348,7 @@ sub redirect_or_confirm_creation : Private {
     if ( $update->confirmed ) {
         $c->forward( 'update_problem' );
         $c->forward( 'signup_for_alerts' );
-        my $report_uri = $c->uri_for( '/report', $update->problem_id );
+        my $report_uri = $c->cobrand->base_url_for_report( $update->problem ) . $update->problem->url;
         $c->res->redirect($report_uri);
         $c->detach;
     }
