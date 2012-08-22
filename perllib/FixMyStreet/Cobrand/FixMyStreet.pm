89e9282ee21295b08f02667eb2552bdf7b211b10
Struan Donald <struan@exo.org.uk>
2012-08-22 15:39:40 +0100

    sort out small issues from merge

--- perllib/FixMyStreet/Cobrand/FixMyStreet.pm
+++ perllib/FixMyStreet/Cobrand/FixMyStreet.pm
@@ -1,17 +1,9 @@
 package FixMyStreet::Cobrand::FixMyStreet;
 use base 'FixMyStreet::Cobrand::UK';
 
-sub area_types          { return qw(DIS LBO MTD UTA CTY COI); }
-sub area_min_generation { 10 }
-
 # FixMyStreet should return all cobrands
 sub restriction {
     return {};
-}
-
-sub enter_postcode_text {
-    my ( $self ) = @_;
-    return _("Enter a nearby GB postcode, or street name and area");
 }
 
 sub admin_base_url {
