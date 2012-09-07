d7bb66cac659a1d0ab9887b09385438049310401
Dave Whiteland <dave@mysociety.org>
2012-09-07 16:43:19 +0100

    added Stevenage cobrand (scss just placeholders for now)

---
+++ perllib/FixMyStreet/Cobrand/Stevenage.pm
@@ -0,0 +1,36 @@
+package FixMyStreet::Cobrand::Barnet;
+use parent 'FixMyStreet::Cobrand::UKCouncils';
+
+use strict;
+use warnings;
+
+sub council_id { return 2347; }
+sub council_area { return 'Stevenage'; }
+sub council_name { return 'Stevenage Council'; }
+sub council_url { return 'stevenage'; }
+
+sub path_to_web_templates {
+    my $self = shift;
+    return [
+        FixMyStreet->path_to( 'templates/web', $self->moniker )->stringify,
+        FixMyStreet->path_to( 'templates/web/fixmystreet' )->stringify
+    ];
+}
+
+sub disambiguate_location {
+    my $self = shift;
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        town   => 'Stevenage',
+        centre => '51.904330,-0.189364',
+        span   => '0.063112,0.087585',
+        bounds => [ 51.869319, -0.234382, 51.932431, -0.146796 ],
+    };
+}
+
+sub example_places {
+    return [ 'SG1 1HN', 'Lyton Way' ];
+}
+
+1;
+
