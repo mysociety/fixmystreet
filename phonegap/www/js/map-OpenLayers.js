0ff334fbf898256c5b27b74bb566f735c4b59a3e
Struan Donald <struan@exo.org.uk>
2012-11-15 11:19:31 +0000

    add zoom controls back in as pinch zoom does not work on all android phones

--- phonegap/www/js/map-OpenLayers.js
+++ phonegap/www/js/map-OpenLayers.js
@@ -326,6 +326,41 @@ OpenLayers.Control.Crosshairs.prototype =
     CLASS_NAME: "OpenLayers.Control.Crosshairs"
 });
 
+OpenLayers.Control.PanZoomFMS = OpenLayers.Class(OpenLayers.Control.PanZoom, {
+    buttonDown: function (evt) {
+        if (!OpenLayers.Event.isLeftClick(evt)) {
+            return;
+        }
+
+        switch (this.action) {
+            case "panup":
+                this.map.pan(0, -this.getSlideFactor("h"));
+                break;
+            case "pandown":
+                this.map.pan(0, this.getSlideFactor("h"));
+                break;
+            case "panleft":
+                this.map.pan(-this.getSlideFactor("w"), 0);
+                break;
+            case "panright":
+                this.map.pan(this.getSlideFactor("w"), 0);
+                break;
+            case "zoomin":
+                this.map.zoomIn();
+                break;
+            case "zoomout":
+                this.map.zoomOut();
+                break;
+            case "zoomworld":
+                this.map.zoomTo(0);
+                break;
+        }
+
+        OpenLayers.Event.stop(evt);
+    }
+});
+
+
 /* Overriding Permalink so that it can pass the correct zoom to OSM */
 OpenLayers.Control.PermalinkFMS = OpenLayers.Class(OpenLayers.Control.Permalink, {
     updateLink: function() {
