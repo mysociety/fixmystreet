5e80ef675801207e9577aa6169778ea993198aca
Struan Donald <struan@exo.org.uk>
2012-11-26 16:27:55 +0000

    display a sensible name for app

--- phonegap/Android/gen/org/mysociety/FixMyStreet/R.java
+++ phonegap/Android/gen/org/mysociety/FixMyStreet/R.java
@@ -24,8 +24,8 @@ public final class R {
         public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello=0x7f050001;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_main=0x7f050003;
     }
