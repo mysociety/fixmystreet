23068c4a32d87c7d11cd31b9fd8d6dfb0d7f7dad
Dave Whiteland <dave@mysociety.org>
2013-01-26 13:28:23 +0000

    stay on location page for MM login, pending running whole service over https, hmm

--- web/cobrands/fixmybarangay/message_manager_client.js
+++ web/cobrands/fixmybarangay/message_manager_client.js
@@ -455,8 +455,8 @@ var message_manager = (function() {
                                 var err_msg = "Unable to load messages: ";
                                 if (st === 0 && textStatus === 'error') { // x-domain hard to detect, sometimes intermittent?
                                     if (_url_root.indexOf('https')===0 && ! location.protocol != 'https:') {
-                                        var surl = "https://" + location.host;
-                                        err_msg += "this is an insecure URL.<br/>Try from <a href='" + surl + "'>"  + surl + "</a>";
+                                        var surl = _url_root.replace(/\/$/, '') + location.pathname + location.search;
+                                        err_msg += 'this is an insecure URL.<br/><a href="' + surl + '">Try from HTTPS instead?</a>';
                                         msg_is_html = true;
                                     } else {
                                         err_msg += "maybe try refreshing page?";
