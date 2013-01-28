db6f6e988118285c34c5172971816d883a06787d
Struan Donald <struan@exo.org.uk>
2013-01-28 16:51:29 +0000

    only try and generate meta if the problem is confirmed otherwise we get errors

--- perllib/FixMyStreet/DB/Result/Problem.pm
+++ perllib/FixMyStreet/DB/Result/Problem.pm
@@ -759,7 +759,7 @@ sub as_hashref {
         used_map  => $self->used_map,
         is_fixed  => $self->fixed_states->{ $self->state } ? 1 : 0,
         photo     => $self->get_photo_params,
-        meta      => $self->meta_line( $c ),
+        meta      => $self->confirmed ? $self->meta_line( $c ) : '',
     };
 }
 
