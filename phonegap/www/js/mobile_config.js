306c57e706af6c18c2ace3c8900bc498ff504a5c
Struan Donald <struan@exo.org.uk>
2013-02-28 17:03:06 +0000

    disable jquery mobile features that upset routing

--- phonegap/www/js/mobile_config.js
+++ phonegap/www/js/mobile_config.js
@@ -1,4 +1,6 @@
 $(document).bind("mobileinit", function(){
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.ajaxEnabled = false;
     $.mobile.ignoreContentEnabled = true;
     $.mobile.defaultPageTransition = 'slide';
 });
