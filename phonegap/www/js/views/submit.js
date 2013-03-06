47049d3d1ad4fdb1a961d85bd0c3a16e0f25e2d8
Struan Donald <struan@exo.org.uk>
2013-03-06 11:07:30 +0000

    basic validation for user details

--- phonegap/www/js/views/submit.js
+++ phonegap/www/js/views/submit.js
@@ -35,10 +35,41 @@
                 return this;
             },
 
+            validate: function() {
+                this.clearValidationErrors();
+                var isValid = 1;
+
+                var email = $('#form_email').val();
+                if ( !email ) {
+                    isValid = 0;
+                    this.validationError('form_email', FMS.validationStrings.email.required);
+                // regexp stolen from jquery validate module
+                } else if ( ! /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(email) ) {
+                    isValid = 0;
+                    this.validationError('form_email', FMS.validationStrings.email.email);
+                }
+
+                var name = $('#form_name').val();
+                if ( !name ) {
+                    isValid = 0;
+                    this.validationError('form_name', FMS.validationStrings.name.required );
+                } else {
+                    var validNamePat = /\ba\s*n+on+((y|o)mo?u?s)?(ly)?\b/i;
+                    if ( name.length < 6 || !name.match( /\S/ ) || name.match( validNamePat ) ) {
+                        isValid = 0;
+                        this.validationError('form_name', FMS.validationStrings.name.validName);
+                    }
+                }
+
+                return isValid;
+            },
+
             onClickSubmit: function(e) {
                 this.model.set( 'submit_clicked', $(e.target).attr('id') );
 
-                this.model.save();
+                if ( this.validate() ) {
+                    this.model.save();
+                }
             },
 
             onReportSync: function(model, resp, options) {
