922c128580f824708aeb672bcab4060c3efacb8c
Matthew Somerville <matthew@mysociety.org>
2013-03-14 21:27:28 +0000

    Set override language for Zurich.

--- perllib/FixMyStreet/Cobrand/Zurich.pm
+++ perllib/FixMyStreet/Cobrand/Zurich.pm
@@ -33,7 +33,8 @@ sub example_places {
     return [ 'Langstrasse', 'Basteiplatz' ];
 }
 
-sub languages { [ 'de-ch,Deutsch,de_CH', 'en-gb,English,en_GB' ] };
+sub languages { [ 'de-ch,Deutsch,de_CH', 'en-gb,English,en_GB' ] }
+sub language_override { 'de-ch' }
 
 # If lat/lon are in the URI, we must have zoom as well, otherwise OpenLayers defaults to 0.
 sub uri {
