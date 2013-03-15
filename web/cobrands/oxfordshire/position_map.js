5696f0ad550b6a6eba101140f39028b35d2bf420
Dave Whiteland <dave@mysociety.org>
2013-03-15 16:34:29 +0000

    add Oxfordshire's requested map instructions

--- web/cobrands/oxfordshire/position_map.js
+++ web/cobrands/oxfordshire/position_map.js
@@ -6,12 +6,29 @@ function position_map_box() {
     } else {
         oxfordshire_right = '0em';
     }
+    
+    var map_box_width = 464;
+    var map_box_height = 464;
     // Do the same as CSS (in case resized from mobile).
     $('#map_box').prependTo('.content').css({
         zIndex: 1, position: 'absolute',
         top: '1em', left: '', right: oxfordshire_right, bottom: '',
-        width: '464px', height: '464px',
+        width: map_box_width + 'px', height: map_box_height +'px',
         margin: 0
+    });
+    
+    if (1 || ! $('#occ-map-instructions').count) {
+        $('#map_box').after("<div id='occ-map-instructions'><p>" +
+            "Click on a pin and then on the “details” link to see an individual report detail.</p></div>");
+    }
+    var mb_y = $('#map_box').position().top + map_box_height ;
+    console.log(" $('#occ-map-instructions')=" +  $('#occ-map-instructions') + ", y=" + mb_y);
+    $('#occ-map-instructions').show().css({
+        left: '', 
+        right: oxfordshire_right, 
+        bottom: '', 
+        top: mb_y +'px',
+        width: map_box_width
     });
 }
 
