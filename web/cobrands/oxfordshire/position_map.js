ad8023f190569e2e733241391efd1a69dc1b8288
Dave Whiteland <dave@mysociety.org>
2013-04-08 01:42:40 +0100

    add Oxfordshire 'click on pin' message (fixed bad js)
    see 9a8601aa9a057320c8e9dd8e432e0f5278ff093a where this was removed

--- web/cobrands/oxfordshire/position_map.js
+++ web/cobrands/oxfordshire/position_map.js
@@ -16,8 +16,23 @@ function position_map_box() {
         width: map_box_width + 'px', height: map_box_height +'px',
         margin: 0
     });
+    if($('#map_box').length) {
+        if (! $('#occ-map-instructions').length) {
+            $('#map_box').after("<div id='occ-map-instructions'><p>" +
+                "Click on a pin and then on the &ldquo;details&rdquo; link to see an individual report detail.</p></div>");
+        }
+        var mb_y = $('#map_box').position().top + map_box_height ;
+        $('#occ-map-instructions').show().css({
+            left: '',
+            right: oxfordshire_right,
+            bottom: '',
+            top: mb_y +'px',
+            width: map_box_width
+        });
+    }
     
 }
 
 function map_fix() {}
 var slide_wards_down = 1;
+
