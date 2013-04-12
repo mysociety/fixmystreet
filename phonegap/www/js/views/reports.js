474ead3b82e2f416a28e380bc801a88156175ad6
Struan Donald <struan@exo.org.uk>
2013-04-12 13:30:40 +0100

    quick list of current draft reports

--- phonegap/www/js/views/reports.js
+++ phonegap/www/js/views/reports.js
@@ -3,7 +3,22 @@
         ReportsView: FMS.FMSView.extend({
             template: 'reports',
             id: 'reports',
-            next: 'home'
+            next: 'home',
+
+            render: function(){
+                if ( !this.template ) {
+                    console.log('no template to render');
+                    return;
+                }
+                template = _.template( tpl.get( this.template ) );
+                if ( this.model ) {
+                    this.$el.html(template({ model: this.model.toJSON(), drafts: FMS.allDrafts }));
+                } else {
+                    this.$el.html(template());
+                }
+                this.afterRender();
+                return this;
+            }
         })
     });
 })(FMS, Backbone, _, $);
