b4ab231c0ed42ae14f6a5f5101cb07e5be95a9fb
Struan Donald <struan@exo.org.uk>
2013-04-15 17:01:20 +0100

    small tweaks to remove a few issues with android and phonegap 1.5

--- phonegap/Android/gen/org/mysociety/FixMyStreet/R.java
+++ phonegap/Android/gen/org/mysociety/FixMyStreet/R.java
@@ -15,22 +15,19 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_main=0x7f050003;
-    }
-    public static final class style {
-        public static final int AppTheme=0x7f060000;
     }
     public static final class xml {
         public static final int config=0x7f040000;
