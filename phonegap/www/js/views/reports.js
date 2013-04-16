9289d34618262159d74e61ac390dc0f8d431c797
Struan Donald <struan@exo.org.uk>
2013-04-16 15:58:50 +0100

    add delete link to my reports page

--- phonegap/www/js/views/reports.js
+++ phonegap/www/js/views/reports.js
@@ -5,6 +5,25 @@
             id: 'reports',
             next: 'home',
 
+            events: {
+                'pagehide': 'destroy',
+                'pageshow': 'afterDisplay',
+                'click .del_report': 'deleteReport',
+                'click .ui-btn-left': 'onClickButtonPrev',
+                'click .ui-btn-right': 'onClickButtonNext'
+            },
+
+            deleteReport: function(e) {
+                var el = $(e.target);
+                var del = FMS.removeDraft( el.attr('id'), true );
+                var that = this;
+                del.done( function() { that.onRemoveDraft(el); } );
+            },
+
+            onRemoveDraft: function(el) {
+                el.parent('li').remove();
+            },
+
             render: function(){
                 if ( !this.template ) {
                     console.log('no template to render');
