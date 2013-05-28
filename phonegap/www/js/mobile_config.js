c4162c78f8049fad218b56658546a8ff1332c241
Struan Donald <struan@exo.org.uk>
2013-05-28 18:38:45 +0100

    turn off more unwanted jquery mobile features

--- phonegap/www/js/mobile_config.js
+++ phonegap/www/js/mobile_config.js
@@ -2,6 +2,8 @@ $(document).bind("mobileinit", function(){
     $.mobile.hashListeningEnabled = false;
     $.mobile.ajaxEnabled = false;
     $.mobile.linkBindingEnabled = false;
+    $.mobile.pushStateEnabled = false;
     $.mobile.ignoreContentEnabled = true;
     $.mobile.defaultPageTransition = 'slide';
+    $.mobile.buttonMarkup.hoverDelay = 0;
 });
