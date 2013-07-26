e7c62cbd10d8d751b816517939ccf466f5e804dd
Matthew Somerville <matthew-github@dracos.co.uk>
2013-07-26 23:50:10 +0100

    Update to v1.2.2.

--- bin/site-specific-install.sh
+++ bin/site-specific-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set this to the version we want to check out
-VERSION=v1.2.1
+VERSION=v1.2.2
 
 PARENT_SCRIPT_URL=https://github.com/mysociety/commonlib/blob/master/bin/install-site.sh
 
