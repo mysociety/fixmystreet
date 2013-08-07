8acaea9f91c7db24871e6e1bd9268d9bf3b0d52d
Dave Whiteland <dave@mysociety.org>
2013-08-07 11:28:23 +0100

    hide open311 inputs for bodies unless needed
    basically if send method is not email (or there's an endpoint value), don't show the form
    Maybe we need to have an "show all fields" button/link but for normal use I think this is OK

--- web/js/fixmystreet-admin.js
+++ web/js/fixmystreet-admin.js
@@ -1,4 +1,25 @@
 $(function(){
     // available for admin pages
+
+    // hide the open311_only section and reveal it only when send_method is relevant
+    var $open311_only = $('.admin-open311-only');
+    if ($open311_only) {
+        function hide_or_show_open311() {
+            var send_method = $('#send_method').val();
+            var show_open311 = false;
+            if ($('#endpoint').val()) {
+                show_open311 = true; // always show the form if there is an endpoint value
+            } else if (send_method && send_method.toLowerCase() != 'email') {
+                show_open311 = true;
+            }
+             if (show_open311) {
+                 $open311_only.slideDown();
+             } else {
+                $open311_only.slideUp();
+            }
+        }
+        $('#send_method').on('change', hide_or_show_open311);
+        hide_or_show_open311();
+    }
 });
 
