c832942f6ec757b4763cb7e6a5ee59b6622cf594
Chris Mytton <self@hecticjeff.net>
2013-08-08 16:43:35 +0100

    Specify width/height units in position_map

--- web/cobrands/oxfordshire/position_map.js
+++ web/cobrands/oxfordshire/position_map.js
@@ -7,13 +7,13 @@ function position_map_box() {
         oxfordshire_right = '0em';
     }
 
-    var map_box_width = 464;
-    var map_box_height = 464;
+    var map_box_width = "464px";
+    var map_box_height = "464px";
     // Do the same as CSS (in case resized from mobile).
     $('#map_box').prependTo('.content').css({
         zIndex: 1, position: 'absolute',
         top: '1em', left: '', right: oxfordshire_right, bottom: '',
-        width: map_box_width + 'px', height: map_box_height +'px',
+        width: map_box_width, height: map_box_height,
         margin: 0
     });
     if($('#map_box').length) {
@@ -21,7 +21,7 @@ function position_map_box() {
             $('#map_box').after("<div id='occ-map-instructions'><p>" +
                 "Click on a pin and then on the &ldquo;details&rdquo; link to see an individual report detail.</p></div>");
         }
-        var mb_y = $('#map_box').position().top + map_box_height ;
+        var mb_y = $('#map_box').position().top + parseInt(map_box_height, 10) ;
         $('#occ-map-instructions').show().css({
             left: '',
             right: oxfordshire_right,
