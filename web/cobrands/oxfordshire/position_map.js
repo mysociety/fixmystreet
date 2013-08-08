e48fd8ff8fa25c0a1fc0f9a626ae304d9a067012
Chris Mytton <self@hecticjeff.net>
2013-08-08 18:52:26 +0100

    Make the oxfordshire map wrapper full width

--- web/cobrands/oxfordshire/position_map.js
+++ web/cobrands/oxfordshire/position_map.js
@@ -13,14 +13,14 @@ function position_map_box() {
         oxfordshire_right = '0em';
     }
 
-    var map_box_width = "464px";
+    var map_box_width = "956px";
     var map_box_height = "464px";
     // Do the same as CSS (in case resized from mobile).
-    $('#map_box').prependTo('.content').css({
+    $('#map_box').prependTo('.wrapper').css({
         zIndex: 1, position: 'absolute',
         top: '1em', left: '', right: oxfordshire_right, bottom: '',
         width: map_box_width, height: map_box_height,
-        margin: 0
+        margin: "0 1em"
     });
     if($('#map_box').length) {
         if (! $('#occ-map-instructions').length) {
