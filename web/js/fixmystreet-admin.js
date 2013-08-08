26a116610778e8099a309bf8f6a14fe247bef0a7
Dave Whiteland <dave@mysociety.org>
2013-08-08 15:01:00 +0100

    add toggle button to hide contacts, closes #505

--- web/js/fixmystreet-admin.js
+++ web/js/fixmystreet-admin.js
@@ -35,5 +35,24 @@ $(function(){
         }
     });
 
+    // on a body's page, hide/show deleted contact categories
+    var $table_with_deleted_contacts = $('table tr.is-deleted td.contact-category');
+    if ($table_with_deleted_contacts.length == 1) {
+        $table_with_deleted_contacts = $table_with_deleted_contacts.closest('table');
+        var $toggle_deleted_btn = $("<input type='submit' class='btn' value='Hide deleted contacts' id='toggle-deleted-contacts-btn' style='margin:1em 0;'/>");
+        $table_with_deleted_contacts.before($toggle_deleted_btn);
+        $toggle_deleted_btn.on('click', function(e){
+            e.preventDefault();
+            var $cols = $table_with_deleted_contacts.find('tr.is-deleted');
+            if ($cols.first().is(':visible')) {
+                $cols.hide();
+                $(this).prop("value", 'Show deleted contacts');
+            } else {
+                $cols.show();
+                $(this).prop("value", 'Hide deleted contacts');
+            }
+        })
+    }
+
 });
 
