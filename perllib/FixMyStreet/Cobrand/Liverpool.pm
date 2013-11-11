affb8d8251af05ac3f9096c9466405f057483e72
Chris Mytton <self@hecticjeff.net>
2013-11-11 12:37:09 +0000

    [Liverpool] Add Cobrand module with basic settings
    This contains the basic settings which can be used to make a start on
    the frontend customisations. There will likely be further changes to
    this if, e.g., Liverpool want to use a custom url for their install.
    
    Part of mysociety/FixMyStreet-Commercial#432

---
+++ perllib/FixMyStreet/Cobrand/Liverpool.pm
@@ -0,0 +1,23 @@
+package FixMyStreet::Cobrand::Liverpool;
+use base 'FixMyStreet::Cobrand::UKCouncils';
+
+use strict;
+use warnings;
+
+sub council_id { return 2527; }
+sub council_area { return 'Liverpool'; }
+sub council_name { return 'Liverpool City Council'; }
+sub council_url { return 'liverpool'; }
+
+sub disambiguate_location {
+    my $self = shift;
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        town   => 'Liverpool',
+        centre => '53.3954859587086,-2.91705385926665',
+        span   => '0.163440332081315,0.201179283552342',
+        bounds => [ 53.3115426747989, -3.01917947387853, 53.4749830068802, -2.81800019032618 ],
+    };
+}
+
+1;
