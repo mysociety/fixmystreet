fe1110d79173be89b6e34eef6dd31e40795f8c54
Chris Mytton <self@hecticjeff.net>
2013-11-12 15:47:50 +0000

    [Liverpool] Add position_map.js script
    This allows us to move the map into the main page flow and remove the
    inline styling.

---
+++ web/cobrands/liverpool/position_map.js
@@ -0,0 +1,17 @@
+function position_map_box() {
+    var $html = $('html');
+    if ($html.hasClass('ie6')) {
+        $('#map_box').prependTo('body').css({
+            zIndex: 0, position: 'absolute',
+            top: 0, left: 0, right: 0, bottom: 0,
+            width: '100%', height: $(window).height(),
+            margin: 0
+        });
+    } else {
+        // Move the map into the content div and remove inline CSS.
+        $('#map_box').prependTo('.container > .content').css({});
+    }
+}
+
+function map_fix() {}
+var slide_wards_down = 0;
