6e6ce1e11a2f4c752e36c98a041ce19f7a50289d
Chris Mytton <self@hecticjeff.net>
2013-11-13 14:29:33 +0000

    [Liverpool] Fix map z-index issue
    This makes the map appear underneath the content div.
    
    https://github.com/mysociety/FixMyStreet-Commercial/issues/438

--- web/cobrands/liverpool/position_map.js
+++ web/cobrands/liverpool/position_map.js
@@ -9,7 +9,7 @@ function position_map_box() {
         });
     } else {
         // Move the map into the content div and remove inline CSS.
-        $('#map_box').prependTo('.container > .content').removeAttr('style');
+        $('#map_box').prependTo('.wrapper > .table-cell > .container').removeAttr('style');
     }
 }
 
