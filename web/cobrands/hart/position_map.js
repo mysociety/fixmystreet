59f67e39b97ecbf6697ac46a1acaf6c15c8db826
Dave Arter <davea@mysociety.org>
2014-01-23 17:16:38 +0000

    Some more renaming of Bromley to Hart

--- web/cobrands/hart/position_map.js
+++ web/cobrands/hart/position_map.js
@@ -1,15 +1,15 @@
 function position_map_box() {
     var $html = $('html');
-    var bromley_right;
+    var hart_right;
     if ($html.hasClass('ie6') || $html.hasClass('ie7')) {
-        bromley_right = '-480px';
+        hart_right = '-480px';
     } else {
-        bromley_right = '0em';
+        hart_right = '0em';
     }
     // Do the same as CSS (in case resized from mobile).
     $('#map_box').prependTo('.content').css({
         zIndex: 1, position: 'absolute',
-        top: '1em', left: '', right: bromley_right, bottom: '',
+        top: '1em', left: '', right: hart_right, bottom: '',
         width: '464px', height: '464px',
         margin: 0
     });
