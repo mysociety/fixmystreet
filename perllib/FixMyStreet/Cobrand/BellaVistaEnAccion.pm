db6a59c52b0184f7a0d11661c7d7792f24104fae
Dave Whiteland <dave@mysociety.org>
2014-03-07 18:58:25 +0000

    add BellaVistaEnAccion: FMS Santiago

---
+++ perllib/FixMyStreet/Cobrand/BellaVistaEnAccion.pm
@@ -0,0 +1,33 @@
+package FixMyStreet::Cobrand::BellaVistaEnAccion;
+use base 'FixMyStreet::Cobrand::Default';
+
+use strict;
+use warnings;
+
+sub path_to_web_templates {
+    my $self = shift;
+    return [
+        FixMyStreet->path_to( 'templates/web', $self->moniker )->stringify,
+        FixMyStreet->path_to( 'templates/web/fixmystreet' )->stringify
+    ];
+}
+
+sub country {
+    return 'CL';
+}
+
+sub example_places {
+    return ( 'Dominica, Recoleta', 'Pio Nono' );
+}
+
+sub languages { [ 'es-cl,Castellano,es_CL', 'en-gb,English,en_GB' ] }
+
+sub disambiguate_location {
+    return {
+        country => 'cl',
+        bing_country => 'Santiago',
+    };
+}
+
+1;
+
