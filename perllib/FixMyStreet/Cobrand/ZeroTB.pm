8d81147d1f757e70393e08947c0be1b73ebfb3ae
Struan Donald <struan@exo.org.uk>
2014-03-11 15:10:30 +0000

    cobrand file with geocoder etc config
    also, make readonly, disable photos, questionnaires, never
    hide old pins
    
    Fixes #701

---
+++ perllib/FixMyStreet/Cobrand/ZeroTB.pm
@@ -0,0 +1,31 @@
+package FixMyStreet::Cobrand::ZeroTB;
+use base 'FixMyStreet::Cobrand::Default';
+
+use strict;
+use warnings;
+
+sub site_title { return 'ZeroTB'; }
+
+sub country {
+    return 'IN';
+}
+
+sub languages { [ 'en-gb,English,en_GB' ] }
+sub language_override { 'en-gb' }
+
+sub disambiguate_location {
+    return {
+        country => 'in',
+        town => 'Delhi',
+        bounds => [ 28.404625000000024, 76.838845800000072, 28.884380600000028, 77.347877500000067 ],
+    };
+}
+
+sub only_authed_can_create { return 1; }
+sub allow_photo_display { return 0; }
+sub allow_photo_upload{ return 0; }
+sub send_questionnaires { return 0; }
+sub on_map_default_max_pin_age { return 0; }
+
+1;
+
