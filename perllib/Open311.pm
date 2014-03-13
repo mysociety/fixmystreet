mysociety/fixmystreet

commit cf20c709e13af0909620ac5387b7d070b66b8c52

Alter Open311 description to only include English on FixMyStreet.
This could be made cobrand-customisable in future.

perllib/Open311.pm
@@ -172,19 +172,18 @@ sub _generate_service_request_description {
     my $problem = shift;
     my $extra = shift;
 
-    my $description = <<EOT;
-detail: @{[$problem->detail()]}
-
-url: $extra->{url}
-
-Submitted via FixMyStreet
-EOT
-;
-    if ($self->extended_description ne 'oxfordshire') {
-        $description = <<EOT . $description;
-title: @{[$problem->title()]}
-
-EOT
+    my $description = "";
+    if ($problem->cobrand eq 'fixmystreet') {
+        $description .= "detail: " . $problem->detail . "\n\n";
+        $description .= "url: " . $extra->{url} . "\n\n";
+        $description .= "Submitted via FixMyStreet\n";
+        if ($self->extended_description ne 'oxfordshire') {
+            $description = "title: " . $problem->title . "\n\n$description";
+        }
+    } else {
+        $description .= $problem->title . "\n\n";
+        $description .= $problem->detail . "\n\n";
+        $description .= $extra->{url} . "\n";
     }
 
     return $description;

t/open311.t
@@ -39,6 +39,7 @@ my $p = FixMyStreet::App->model('DB::Problem')->new( {
     user => $u,
     id => 1,
     name => 'A User',
+    cobrand => 'fixmystreet',
 } );
 
 my $expected_error = qr{Failed to submit problem 1 over Open311}ism;
@@ -63,6 +64,7 @@ my $problem = FixMyStreet::App->model('DB::Problem')->new( {
     longitude => 2,
     user => $user,
     name => 'Test User',
+    cobrand => 'fixmystreet',
 } );
 
 subtest 'posting service request' => sub {

commit 797546c15d00dee15051495901c1dbbf96b0508f

Fix slight regression in cf20c709, to still match all UK cobrands.

perllib/Open311.pm
@@ -173,7 +173,7 @@ sub _generate_service_request_description {
     my $extra = shift;
 
     my $description = "";
-    if ($problem->cobrand eq 'fixmystreet') {
+    if ($extra->{easting_northing}) { # Proxy for cobrand being in the UK
         $description .= "detail: " . $problem->detail . "\n\n";
         $description .= "url: " . $extra->{url} . "\n\n";
         $description .= "Submitted via FixMyStreet\n";

t/open311.t
@@ -70,6 +70,7 @@ my $problem = FixMyStreet::App->model('DB::Problem')->new( {
 subtest 'posting service request' => sub {
     my $extra = {
         url => 'http://example.com/report/1',
+        easting_northing => 'SET',
     };
 
     my $results = make_service_req( $problem, $extra, $problem->category, '<?xml version="1.0" encoding="utf-8"?><service_requests><request><service_request_id>248</service_request_id></request></service_requests>' );