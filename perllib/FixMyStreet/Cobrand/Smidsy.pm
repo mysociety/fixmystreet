b3598d1ea0d825b80fec3a4b712bcaab44ce765a
Hakim Cassimally <hakim@mysociety.org>
2014-03-19 16:03:36 +0000

    [Smidsy] first cut at process_extras

--- perllib/FixMyStreet/Cobrand/Smidsy.pm
+++ perllib/FixMyStreet/Cobrand/Smidsy.pm
@@ -5,6 +5,8 @@ use strict;
 use warnings;
 
 use FixMyStreet;
+use DateTime;
+use DateTime::Format::Strptime;
 
 # http://mapit.mysociety.org/area/2247.html
 use constant area_id => 2247;
@@ -29,6 +31,133 @@ sub pin_colour {
 }
 
 sub use_pin_colour_for_big_icons { 1 }
+
+sub process_extras {
+    my $self    = shift;
+    my $ctx     = shift;
+    my $body_id = shift;
+    my $extra   = shift;
+    my $fields  = shift || [];
+
+    my @fields = (
+        @$fields,
+        {
+            name => 'severity',
+            validator => sub {
+                my $sev = shift;
+                if ($sev > 0 and $sev <= 100) {
+                    return $sev+0;
+                }
+                die "Severity must be between 1 and 100\n";
+            },
+        },
+        {
+            name => 'incident_date',
+            validator => sub {
+                my $data = shift;
+                my $date;
+
+                if ($data eq 'today') {
+                    $date = DateTime->today;
+                }
+                else {
+                    $date = DateTime::Format::Strptime->new(
+                        pattern => '%d/%m/%Y'
+                    )->parse_datetime($data);
+                }
+                if (! $date) {
+                    die "Please input a valid date in format dd/mm/yyyy\n";
+                }
+                return $date->iso8601;
+            },
+        },
+        {
+            name => 'incident_time',
+            validator => sub {
+                my $data = shift or return;
+                die "Please input a valid time in format hh:mm\n" 
+                    unless $data =~ /^\d{1,2}:\d{2}$/;
+                return $data;
+            },
+        },
+        {
+            name => 'participants',
+            validator => sub {
+                my $data = shift;
+                die "Invalid option!\n"
+                    unless {
+                        "bike-car" => 1,
+                        "bike-other" => 1,
+                        "pedestrian-bike" => 1,
+                    }->{ $data };
+                return $data;
+            },
+        },
+        {
+            name => 'road_type',
+            validator => sub {
+                my $data = shift;
+                die "Invalid option!\n"
+                    unless {
+                        "road" => 1,
+                        "lane-onroad" => 1,
+                        "lane-separate" => 1,
+                        "pavement" => 1,
+                    }->{ $data };
+                return $data;
+            },
+        },
+        {
+            name => 'registration',
+            validator => sub {
+                # ok not to pass one, just accept anything for now
+                return shift;
+            },
+        },
+        {
+            name => 'media_url',
+            validator => sub {
+                my $data = shift
+                    or return '';
+                # die "Please enter a valid URL\n" if $data =~ ... # TODO
+                $data = 'http://' . $data
+                    unless $data =~ m{://};
+                return $data;
+            },
+        },
+    );
+
+
+    for my $field ( @fields ) {
+        my $field_name = ref $field ? $field->{name} : $field;
+        my $description;
+        my $value = $ctx->request->param( $field );
+
+        if (ref $field) {
+            $description = $field->{value} || uc $field_name;
+
+            eval {
+                $value = $field->{validator}->($value);
+            };
+            if ($@) {
+                $ctx->stash->{field_errors}->{ $field_name } = _('This information is required');
+            }
+
+        }
+        else {
+            if ( !$value ) {
+                $ctx->stash->{field_errors}->{ $field_name } = $@;
+            }
+            $description = uc $field_name;
+        }
+
+        push @$extra, {
+            name => $field_name,
+            description => $description,
+            value => $value || '',
+        };
+    }
+}
 
 # this is required to use new style templates
 sub path_to_web_templates {
