7f2053aa4a6e2f4d185a409f8f15726635f4bffe
Dave Arter <davea@mysociety.org>
2014-07-04 11:13:05 +0100

    East Sussex: Build templates from fragments published on eastsussex.gov.uk

---
+++ templates/web/eastsussex/build_eastsussex_templates.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import urllib2
+
+TEMPLATES = {
+    "header.html.template": (
+        "HtmlTag",
+        "MetadataDesktop",
+        (
+            "HeaderDesktop",
+            (
+                ("<header>", '<header id="site-header" class="eastsussex">'),
+            )
+        ),
+    ),
+    "footer.html.template": (
+        "FooterDesktop",
+    )
+}
+
+BASE_URL = "https://www.eastsussex.gov.uk/masterpages/remote/control.aspx?control={fragment}&host=mysociety.org"
+
+
+def patch_fragment(fragment, patches):
+    if not patches:
+        return fragment
+    for search, replacement in patches:
+        fragment = fragment.replace(search, replacement)
+    return fragment
+
+
+def main():
+    for template_path, fragment_names in TEMPLATES.items():
+        template = open(template_path).read()
+        fragments = {}
+        for name in fragment_names:
+            if isinstance(name, tuple):
+                name, patches = name
+            else:
+                patches = None
+            url = BASE_URL.format(fragment=name)
+            content = urllib2.urlopen(url).read().replace("\r", "")
+            fragments[name] = patch_fragment(content, patches)
+            open("{0}.html".format(name), "wb").write(fragments[name])
+        with open(template_path[:-9], "wb") as outfile:
+            outfile.write(template.format(**fragments))
+
+if __name__ == '__main__':
+    main()
