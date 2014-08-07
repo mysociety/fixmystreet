98e3efa546aed5fb4e5acfcdb3d9da5e620b1e7e
Dave Whiteland <dave@mysociety.org>
2014-08-07 03:08:19 +0100

    Add MakeMyIsland cobrand module (really for title)
    Perhaps useful as we might be adding more stuff if the project expands,
    e.g., geocoder disambiguation.

---
+++ perllib/FixMyStreet/Cobrand/MakeMyIsland.pm
@@ -0,0 +1,14 @@
+package FixMyStreet::Cobrand::MakeMyIsland;
+use base 'FixMyStreet::Cobrand::Default';
+
+use strict;
+use warnings;
+
+sub site_title { return 'MakeMyIsland'; }
+
+sub country {
+    return 'MV';
+}
+
+1;
+
