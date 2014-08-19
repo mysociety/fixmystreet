5044739a929014204d2cfcb9603a8864e3cd11f5
vagrant <vagrant@precise64.(none)>
2014-08-19 13:27:17 +0000

    added blank page for FMS for councils design, added details to static.pm

--- perllib/FixMyStreet/App/Controller/Static.pm
+++ perllib/FixMyStreet/App/Controller/Static.pm
@@ -35,10 +35,10 @@ sub faq : Global : Args(0) {
     # There should be a faq template for each language in a cobrand or default.
     # This is because putting the FAQ translations into the PO files is
     # overkill.
-    
+
     # We rely on the list of languages for the site being restricted so that there
     # will be a faq template for that language/cobrand combo.
-        
+
     my $lang_code = $c->stash->{lang_code};
     my $template  = "faq/faq-$lang_code.html";
     $c->stash->{template} = $template;
@@ -58,6 +58,10 @@ sub iphone : Global : Args(0) {
 }
 
 sub council : Global : Args(0) {
+    my ( $self, $c ) = @_;
+}
+
+sub fmsforcouncils : Global : Args(0) {
     my ( $self, $c ) = @_;
 }
 
