f8ca5c21ceca94370e6166b0bd2bf0b61b2cb133
Zarino Zappia <mail@zarino.co.uk>
2014-09-12 12:46:00 +0100

    new reporting form questions and markup

--- web/cobrands/smidsy/js/report.js
+++ web/cobrands/smidsy/js/report.js
@@ -1,56 +1,32 @@
 $(function() {
 
-$( "#form_incident_date" ).datepicker({ 
-    minDate: -7, 
-    maxDate: +0,
-    defaultDate: +0,
-    dateFormat: 'dd/mm/yy'
-});
+    $('#form_incident_date').datepicker({
+        minDate: -7,
+        maxDate: +0,
+        defaultDate: +0,
+        dateFormat: 'dd/mm/yy'
+    });
 
-var select = $( "#form_severity" );
+    $('.describe-injury').hide();
 
-var slider = $( "<div id='severity-slider'></div>" );
+    $('input[name="severity"]').on('change', function(){
+        // Assumes the severity radio buttons have numeric values,
+        // where a value over 0 implies injury.
+        if( 0 + $('#mapForm')[0].severity.value > 0 ) {
+            $('.describe-injury').slideDown();
+        } else {
+            $('.describe-injury').slideUp();
+        }
+    });
 
-function colour_severity () {
-    var val = select.val();
-    var idx = select[0].selectedIndex;
-    if (val < severity.minor_threshold) {
-        slider.addClass('severity-low');
-        slider.removeClass('severity-medium');
-        slider.removeClass('severity-high');
-    }
-    else if (val < severity.major_threshold) {
-        slider.removeClass('severity-low');
-        slider.addClass('severity-medium');
-        slider.removeClass('severity-high');
-    }
-    else {
-        slider.removeClass('severity-low');
-        slider.removeClass('severity-medium');
-        slider.addClass('severity-high');
-    }
-
-    console.log( severity, severity.categories, idx );
-
-    $('#severity_description').html( severity.categories[ idx ].description );
-}
-
-slider.insertBefore( select ).slider({
-    min: 1,
-    max: severity.categories.length,
-    range: "min",
-    value: select[ 0 ].selectedIndex + 1,
-    slide: function( event, ui ) {
-        select[ 0 ].selectedIndex = ui.value - 1;
-        colour_severity();
-    }
-});
-
-select.change(function() {
-    slider.slider( "value", this.selectedIndex + 1 );
-    colour_severity();
-});
-
-colour_severity();
+    $('#form_participants').on('change', function(){
+        // In a stroke of genius, jQuery returns true for the :selected selector,
+        // if *any* of the matched elements are :selected, rather than *all* of them.
+        if( $('option[value="bike-car"], option[value="bike-motorcycle"], option[value="bike-hgv"], option[value="bike-other"]').is(':selected') ) {
+            $('.vehicle-registration-number').slideDown();
+        } else {
+            $('.vehicle-registration-number').slideUp();
+        }
+    });
 
 });
