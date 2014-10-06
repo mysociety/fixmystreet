eadcecde0c3b04828e68b3de03846622bc52bf84
Hakim Cassimally <hakim@mysociety.org>
2014-10-06 16:42:47 +0000

    Smidsy. fix map with maxResolution
    as per MapQuest work and suggested implementation by matthew

--- web/js/map-tonerlite.js
+++ web/js/map-tonerlite.js
@@ -4,8 +4,11 @@ function set_map_config(perm) {
         //new OpenLayers.Control.LayerSwitcher(),
         new OpenLayers.Control.Navigation(),
         new OpenLayers.Control.Permalink(),
-        new OpenLayers.Control.PanZoomFMS()
+        new OpenLayers.Control.PanZoomFMS({id: 'fms_pan_zoom' })
     ];
     fixmystreet.map_type = OpenLayers.Layer.Stamen;
+    fixmystreet.layer_options = [ {
+        maxResolution: 156543.03390625/Math.pow(2, fixmystreet.zoomOffset)
+    } ];
     fixmystreet.layer_style = 'toner-lite';
 }
