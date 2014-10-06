3a8e4e33c4a2f1b5446443e68fa76b3f589c42f2
Matthew Somerville <matthew@mysociety.org>
2014-10-06 15:47:21 +0000

    Support Stamen toner-lite and Bing Maps tiles.

---
+++ perllib/FixMyStreet/Map/OSM/TonerLite.pm
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+#
+# FixMyStreet:Map::OSM::TonerLite
+# OSM TonerLite maps on FixMyStreet.
+#
+# Map tiles by <http://stamen.com> Stamen Design,
+# under <http://creativecommons.org/licenses/by/3.0>CC BY 3.0.
+# Data by <http://openstreetmap.org> OpenStreetMap,
+# under <http://www.openstreetmap.org/copyright> ODbL.
+#
+# Copyright (c) 2014 UK Citizens Online Democracy. All rights reserved.
+# Email: hakim@mysociety.org; WWW: http://www.mysociety.org/
+
+package FixMyStreet::Map::OSM::TonerLite;
+use base 'FixMyStreet::Map::OSM';
+
+use strict;
+
+sub map_type { 'OpenLayers.Layer.Stamen' }
+
+sub map_template { 'osm-toner-lite' }
+
+sub copyright {
+    'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+}
+
+sub map_tiles {
+    my ( $self, %params ) = @_;
+    my ( $x, $y, $z ) = ( $params{x_tile}, $params{y_tile}, $params{zoom_act} );
+    return [
+        "https://stamen-tiles-a.a.ssl.fastly.net/toner-lite/$z/" . ($x - 1) . "/" . ($y - 1) . ".png",
+        "https://stamen-tiles-b.a.ssl.fastly.net/toner-lite/$z/$x/" . ($y - 1) . ".png",
+        "https://stamen-tiles-c.a.ssl.fastly.net/toner-lite/$z/" . ($x - 1) . "/$y.png",
+        "https://stamen-tiles-d.a.ssl.fastly.net/toner-lite/$z/$x/$y.png",
+    ];
+}
+
+1;
---
+++ web/js/map-fms.js
@@ -0,0 +1,74 @@
+var fms_tile_base = [ [ '', 'a-', 'b-', 'c-' ], '//{S}tilma.mysociety.org/sv' ];
+
+function set_map_config(perm) {
+    _set_map_config();
+
+    if (fixmystreet.map_type) {
+        fms_tile_base = fixmystreet.map_type;
+    }
+    fixmystreet.map_type = OpenLayers.Layer.BingUK;
+}
+
+OpenLayers.Layer.BingUK = OpenLayers.Class(OpenLayers.Layer.Bing, {
+    uk_bounds: [
+        new OpenLayers.Bounds(-6.6, 49.8, 1.102680, 51),
+        new OpenLayers.Bounds(-5.4, 51, 2.28, 54.94),
+        new OpenLayers.Bounds(-5.85, 54.94, -1.15, 55.33),
+        new OpenLayers.Bounds(-9.35, 55.33, -0.7, 60.98)
+    ],
+
+    in_uk: function(c) {
+        c = c.clone();
+        c.transform(
+            fixmystreet.map.getProjectionObject(),
+            new OpenLayers.Projection("EPSG:4326")
+        );
+        if ( this.uk_bounds[0].contains(c.lon, c.lat) || this.uk_bounds[1].contains(c.lon, c.lat) || this.uk_bounds[2].contains(c.lon, c.lat) || this.uk_bounds[3].contains(c.lon, c.lat) ) {
+            return true;
+        }
+        return false;
+    },
+
+    setMap: function() {
+        OpenLayers.Layer.Bing.prototype.setMap.apply(this, arguments);
+        this.map.events.register("moveend", this, this.updateAttribution);
+    },
+
+    updateAttribution: function() {
+        var z = this.map.getZoom() + this.zoomOffset;
+        var copyrights;
+        var logo = '';
+        var c = this.map.getCenter();
+        var in_uk = c ? this.in_uk(c) : true;
+        if (z >= 16 && in_uk) {
+            copyrights = 'Contains Ordnance Survey data &copy; Crown copyright and database right 2014';
+        } else {
+            logo = '<a href="https://www.bing.com/maps/"><img border=0 src="//dev.virtualearth.net/Branding/logo_powered_by.png"></a>';
+            copyrights = '&copy; 2011 <a href="https://www.bing.com/maps/">Microsoft</a>. &copy; AND, Navteq, Ordnance Survey';
+        }
+        this._updateAttribution(copyrights, logo);
+    },
+
+    get_urls: function(bounds, z) {
+        var urls;
+        var in_uk = this.in_uk(bounds.getCenterLonLat());
+        if (z >= 16 && in_uk) {
+            urls = [];
+            for (var i=0; i< fms_tile_base[0].length; i++) {
+                urls.push( fms_tile_base[1].replace('{S}', fms_tile_base[0][i]) + "/${z}/${x}/${y}.png" );
+            }
+        } else {
+            var type = '';
+            if (z > 10 && in_uk) { type = '&productSet=mmOS'; }
+            urls = [
+                "//ecn.t0.tiles.virtualearth.net/tiles/r${id}.png?g=3293" + type,
+                "//ecn.t1.tiles.virtualearth.net/tiles/r${id}.png?g=3293" + type,
+                "//ecn.t2.tiles.virtualearth.net/tiles/r${id}.png?g=3293" + type,
+                "//ecn.t3.tiles.virtualearth.net/tiles/r${id}.png?g=3293" + type
+            ];
+        }
+        return urls;
+    },
+
+    CLASS_NAME: "OpenLayers.Layer.BingUK"
+});
