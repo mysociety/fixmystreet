0f5d47de00cb993684d255ed1306a67406d48ddf
Matthew Somerville <matthew@mysociety.org>
2014-10-09 14:13:44 +0100

    [FixMyStreet.com] Add information for councils.
    Fixes #765.

--- perllib/FixMyStreet/App/Controller/Static.pm
+++ perllib/FixMyStreet/App/Controller/Static.pm
@@ -35,10 +35,10 @@ sub faq : Global : Args(0) {
     # There should be a faq template for each language in a cobrand or default.
     # This is because putting the FAQ translations into the PO files is
     # overkill.
-    
+
     # We rely on the list of languages for the site being restricted so that there
     # will be a faq template for that language/cobrand combo.
-        
+
     my $lang_code = $c->stash->{lang_code};
     my $template  = "faq/faq-$lang_code.html";
     $c->stash->{template} = $template;
