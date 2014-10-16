6296cb33c93f47e1ff31859471f66f008c78d358
Hakim Cassimally <hakim@mysociety.org>
2014-10-16 13:25:52 +0000

    [Warwickshire] raise z-index of map, working around chrome bug
    works around
    https://github.com/mysociety/FixMyStreet-Commercial/issues/614
    
    (This is a chrome bug, but as it doesn't happen with standard
    FMS, we investigated further with @stevenday and @zarino.
    For reasons I don't understand, the issue is fixed by:
     - removing negative z-index from map
     - this then makes the site-header not display, so we set
     position:relative -- not having done this before might be
     the reason for the previous negative index? )

--- web/cobrands/warwickshire/position_map.js
+++ web/cobrands/warwickshire/position_map.js
@@ -9,7 +9,7 @@ function position_map_box() {
         });
     } else {
         $('#map_box').prependTo('body').css({
-            zIndex: -1, position: 'fixed',
+            zIndex: 0, position: 'fixed',
             top: 0, left: 0, right: 0, bottom: 0,
             width: '100%', height: '100%',
             margin: 0
