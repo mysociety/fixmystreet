237646fbeddce8c36c61f5e9e3250a78a056186b
Hakim Cassimally <hakim@mysociety.org>
2014-11-14 14:32:04 +0000

    TO REBASE flatten label and add tooltip
    See https://github.com/mysociety/FixMyStreet-Commercial/issues/642
    
    Try to avoid label looking like a button by flattening it
    (e.g. removing box shadow).
    
    The cursor icon change is helpfully set by the fact we're in a <label>.
    As that might be a feature, instead of disabling the change, I've added
    a <title> attribute to at least generate a tooltip.

--- web/cobrands/eastsussex/report-form.js
+++ web/cobrands/eastsussex/report-form.js
@@ -2,6 +2,8 @@ $(function () {
     setup_anonymous_checkbox( $('#form_sign_in_no') );
     setup_anonymous_checkbox( $('#form-box--logged-in-name') );
     setup_anonymous_checkbox( $('#update_form') );
+
+    $('.public-warning').attr({ 'title': 'This information will be be visible to the public on the report' });
 });
 
 function setup_anonymous_checkbox (div) {
