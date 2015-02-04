a0608370c678147dd27ec3c37dd3b0ddef70ee1f
Hakim Cassimally <hakim@mysociety.org>
2015-02-04 14:52:12 +0000

    [Zurich] all admin users can see stats
    Admin CR4 https://github.com/mysociety/FixMyStreet-Commercial/issues/673
    
    seems like admin_pages should be able to generate the menu, but
    this change also needs template change.

--- perllib/FixMyStreet/Cobrand/Zurich.pm
+++ perllib/FixMyStreet/Cobrand/Zurich.pm
@@ -309,11 +309,13 @@ sub admin_pages {
     my $c = $self->{c};
 
     my $type = $c->stash->{admin_type};
+
     my $pages = {
         'summary' => [_('Summary'), 0],
         'reports' => [_('Reports'), 2],
         'report_edit' => [undef, undef],
         'update_edit' => [undef, undef],
+        'stats' => [_('Stats'), 4],
     };
     return $pages if $type eq 'sdm';
 
@@ -325,7 +327,6 @@ sub admin_pages {
 
     $pages = { %$pages,
         'users' => [_('Users'), 3],
-        'stats' => [_('Stats'), 4],
         'user_edit' => [undef, undef],
     };
     return $pages if $type eq 'super';
