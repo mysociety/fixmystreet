970e731151171989b8595392941fef6bcbd364a9
Dave Arter <davea@mysociety.org>
2015-02-25 17:23:48 +0000

    Oxfordshire: Update categories drop-down when dragging map

--- web/js/map-OpenLayers.js
+++ web/js/map-OpenLayers.js
@@ -109,6 +109,34 @@ function fms_markers_resize() {
     fixmystreet.markers.redraw();
 }
 
+function fms_categories_update() {
+    var bbox = fixmystreet.bbox_strategy.getMapBounds().toBBOX();
+    var default_categories = [{label: "Everything", value: ""}];
+    $.getJSON("/ajax/categories", {bbox: bbox})
+        .done(function(data) {
+            var categories = default_categories.slice();
+            for (var i = 0; i < data.categories.length; i++) {
+                categories.push({value: data.categories[i]});
+            }
+            fms_set_categories_options(categories);
+        })
+        .fail(function() {
+            fms_set_categories_options(default_categories);
+        });
+}
+
+function fms_set_categories_options(options) {
+    var select = $("#categories");
+    select.prop("disabled", false);
+    var old_value = select.find("option:selected").val();
+    select.empty();
+    for (var i = 0; i < options.length; i++) {
+        var value = options[i].value;
+        var label = options[i].label || value;
+        $("<option>").val(value).html(label).appendTo(select);
+    }
+}
+
 function fixmystreet_onload() {
     if ( fixmystreet.area.length ) {
         for (var i=0; i<fixmystreet.area.length; i++) {
@@ -240,6 +268,12 @@ function fixmystreet_onload() {
         fixmystreet.map.addControl( fixmystreet.select_feature );
         fixmystreet.select_feature.activate();
         fixmystreet.map.events.register( 'zoomend', null, fms_markers_resize );
+
+        // Zoom-level-specific pin sizes on Oxfordshire
+        var cobrand = $('meta[name="cobrand"]').attr('content');
+        if (cobrand === 'oxfordshire') {
+            fixmystreet.markers.events.register( 'loadend', null, fms_categories_update );
+        }
     } else if (fixmystreet.page == 'new') {
         fixmystreet_activate_drag();
     }
