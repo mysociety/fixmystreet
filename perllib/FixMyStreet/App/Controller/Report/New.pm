6a5f6b6c7d21db398ed093aae1cd4f39c9400250
Hakim Cassimally <hakim@mysociety.org>
2015-02-25 18:04:58 +0000

    [Zurich] Email template var for confirmed address.
    See mysociety/FixMyStreet-Commercial#669

--- perllib/FixMyStreet/App/Controller/Report/New.pm
+++ perllib/FixMyStreet/App/Controller/Report/New.pm
@@ -1159,6 +1159,9 @@ sub redirect_or_confirm_creation : Private {
         }
     } );
     $c->stash->{token_url} = $c->uri_for_email( '/P', $token->token );
+    if ($c->cobrand->can('problem_confirm_email_extras')) {
+        $c->cobrand->problem_confirm_email_extras($report);
+    }
     $c->send_email( 'problem-confirm.txt', {
         to => [ $report->name ? [ $report->user->email, $report->name ] : $report->user->email ],
     } );
