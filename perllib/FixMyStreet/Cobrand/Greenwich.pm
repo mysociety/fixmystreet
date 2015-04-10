0b863736562220128d5555301fa54fb12cf61072
Dave Arter <davea@mysociety.org>
2015-04-10 16:51:09 +0100

    [Greenwich] Colour pins based on status

--- perllib/FixMyStreet/Cobrand/Greenwich.pm
+++ perllib/FixMyStreet/Cobrand/Greenwich.pm
@@ -31,4 +31,12 @@ sub disambiguate_location {
     };
 }
 
+sub pin_colour {
+    my ( $self, $p, $context ) = @_;
+    return 'grey' if $p->state eq 'not responsible';
+    return 'green' if $p->is_fixed || $p->is_closed;
+    return 'red' if $p->state eq 'confirmed';
+    return 'yellow';
+}
+
 1;
