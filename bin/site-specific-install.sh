ac39951581a0eefe069c8a707bb89977227d0bce
Matthew Somerville <matthew@mysociety.org>
2015-10-23 12:55:38 +0100

    Version 1.7.
    Include new contributing document.

--- bin/site-specific-install.sh
+++ bin/site-specific-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set this to the version we want to check out
-VERSION=${VERSION_OVERRIDE:-v1.6.1}
+VERSION=${VERSION_OVERRIDE:-v1.7}
 
 PARENT_SCRIPT_URL=https://github.com/mysociety/commonlib/blob/master/bin/install-site.sh
 
