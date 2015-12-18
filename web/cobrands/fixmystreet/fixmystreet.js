5b6deb93fface15549452d1f25c0ef1bf219208a
Matthew Somerville <matthew@mysociety.org>
2015-12-18 11:46:26 +0000

    Tidy up CSS of top banner.
    Include UK donations CSS used over Christmas 2015 for any future use.

--- web/cobrands/fixmystreet/fixmystreet.js
+++ web/cobrands/fixmystreet/fixmystreet.js
@@ -509,8 +509,8 @@ $.fn.drawer = function(id, ajax) {
         }).fadeOut(500);
     });
 
-    $('#message_close').live('click', function() {
-        $('#country_banner').hide();
+    $('.top_banner__close').live('click', function() {
+        $('.top_banner--country').hide();
         $.cookie('has_seen_country_message', 1, {expires: 365, path: '/'});
     });
 
@@ -521,7 +521,7 @@ $.fn.drawer = function(id, ajax) {
                 success: function(data) {
                     if ( data ) {
                         $('body').prepend(data);
-                        $('#country_banner').slideDown('slow');
+                        $('.top_banner--country').slideDown('slow');
                     }
                 }
             });
