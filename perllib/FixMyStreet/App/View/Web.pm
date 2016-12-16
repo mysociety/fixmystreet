mysociety/fixmystreet

commit fddf7f9585e50a60acca01b84bc8f9cfc267dd0b

Add offline support of static files/fallback page.
Use a list to store JavaScript files, so it can be shared
between the HTML footer and the appcache manifest.

perllib/FixMyStreet/App/View/Web.pm
@@ -140,7 +140,8 @@ sub escape_js {
 
 my %version_hash;
 sub version {
-    my ( $self, $c, $file ) = @_;
+    my ( $self, $c, $file, $url ) = @_;
+    $url ||= $file;
     _version_get_mtime($file);
     if ($version_hash{$file} && $file =~ /\.js$/) {
         # See if there's an auto.min.js version and use that instead if there is
@@ -149,7 +150,7 @@ sub version {
         $file = $file_min if $version_hash{$file_min} >= $version_hash{$file};
     }
     my $admin = $self->template->context->stash->{admin} ? FixMyStreet->config('ADMIN_BASE_URL') : '';
-    return "$admin$file?$version_hash{$file}";
+    return "$admin$url?$version_hash{$file}";
 }
 
 sub _version_get_mtime {

perllib/FixMyStreet/App/Controller/Offline.pm
@@ -1,0 +1,32 @@
+package FixMyStreet::App::Controller::Offline;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+FixMyStreet::App::Controller::Offline - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Offline pages Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+sub manifest : Path("/offline/appcache.manifest") {
+    my ($self, $c) = @_;
+    $c->res->content_type('text/cache-manifest; charset=utf-8');
+    $c->res->header(Cache_Control => 'no-cache, no-store');
+}
+
+sub appcache : Path("/offline/appcache") {
+    my ($self, $c) = @_;
+    $c->detach('/page_error_404_not_found', []) if keys %{$c->req->params};
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;

perllib/FixMyStreet/Cobrand/Base.pm
@@ -38,6 +38,20 @@ sub moniker {
     return $last_part;
 }
 
+=head2 asset_moniker
+
+    $moniker = $cobrand_class->asset_moniker();
+
+Same as moniker, except for the cobrand with the 'fixmystreet' moniker, when it
+returns 'fixmystreet.com', as to avoid confusion that's where its assets are.
+
+=cut
+
+sub asset_moniker {
+    my $self = shift;
+    return $self->moniker eq 'fixmystreet' ? 'fixmystreet.com' : $self->moniker;
+}
+
 =head2 is_default
 
     $bool = $cobrand->is_default();

templates/web/angus/maps/fms.html
@@ -1,11 +1,11 @@
-[% map_js = BLOCK %]
-<!-- <script type="text/javascript" src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0&mkt=en-GB"></script> -->
-<script type="text/javascript" src="[% version('/js/OpenLayers/OpenLayers.angus.js') %]"></script>
-<script type="text/javascript" src="[% version('/js/map-OpenLayers.js') %]"></script>
-<script type="text/javascript" src="[% version('/js/map-bing-ol.js') %]"></script>
-<script type="text/javascript" src="[% version('/js/map-fms.js') %]"></script>
-<script src="[% version('/cobrands/fixmystreet/assets.js') %]"></script>
-<script src="[% version('/cobrands/angus/js.js') %]"></script>
-[% END %]
-
-[% map_html = INCLUDE maps/openlayers.html include_key = 1 %]
+[%
+map_js = [
+    version('/js/OpenLayers/OpenLayers.angus.js'),
+    version('/js/map-OpenLayers.js'),
+    version('/js/map-bing-ol.js'),
+    version('/js/map-fms.js'),
+    version('/cobrands/fixmystreet/assets.js'),
+    version('/cobrands/angus/js.js'),
+];
+map_html = INCLUDE maps/openlayers.html include_key = 1
+%]

templates/web/base/common_footer_tags.html
@@ -1,28 +1,13 @@
-[% USE date %][% USE Math %]
-
 [% TRY %][% PROCESS 'footer_extra.html' %][% CATCH file %][% END %]
 
-<script type="text/javascript" src="[% start %]/js/translation_strings.[% lang_code %].js?[% Math.int( date.now / 3600 ) %]"></script>
-<script type="text/javascript" src="[% version('/jslib/jquery-1.7.2.min.js') %]"></script>
+[% PROCESS 'common_scripts.html' %]
+
 <!--[if lte IE 9]>
-  <script type="text/javascript" src="[% version('/js/history.polyfill.min.js') %]"></script>
+  <script src="[% version('/js/history.polyfill.min.js') %]"></script>
 <![endif]-->
-<script type="text/javascript" src="[% version('/js/validation_rules.js') %]"></script>
-<script src="[% version('/js/jquery.validate.min.js') %]" type="text/javascript" charset="utf-8"></script>
-<script type="text/javascript" src="[% version('/js/dropzone.min.js') %]"></script>
-<script type="text/javascript" src="[% version('/js/jquery.multi-select.js') %]"></script>
-
-<script type="text/javascript" src="[% version('/js/geo.min.js') %]"></script>
-<script type="text/javascript" src="[% version('/cobrands/fixmystreet/fixmystreet.js') %]"></script>
-
-[% map_js %]
-<script src="[% version('/cobrands/fixmystreet/map.js') %]"></script>
-
-[% IF admin %]
-  <script src="[% version('/js/jquery-ui/js/jquery-ui-1.10.3.custom.min.js') %]"></script>
-  <script type="text/javascript" src="[% version('/js/fixmystreet-admin.js') %]"></script>
+[% FOR script IN scripts ~%]
+    [% script = script.0 ? script : [ script ] ~%]
+    <script src="[% script.0 %]"
+        [%~ FOR attr IN script.1 %] [% attr.key %]="[% attr.value %]"[% END ~%]
+        ></script>
 [% END %]
-
-[% extra_js %]
-
-[% TRY %][% PROCESS 'footer_extra_js.html' %][% CATCH file %][% END %]

templates/web/base/common_scripts.html
@@ -1,0 +1,44 @@
+[%
+
+USE date;
+USE Math;
+
+scripts = [];
+
+scripts.push(
+    start _ "/js/translation_strings." _ lang_code _ ".js?" _ Math.int( date.now / 3600 ),
+    version('/jslib/jquery-1.7.2.min.js'),
+    version('/js/validation_rules.js'),
+    version('/js/jquery.validate.min.js'),
+    version('/js/dropzone.min.js'),
+    version('/js/jquery.multi-select.js'),
+    version('/js/geo.min.js'),
+    version('/cobrands/fixmystreet/fixmystreet.js'),
+);
+
+FOR script IN map_js;
+    scripts.push(script);
+END;
+
+scripts.push(
+    version('/cobrands/fixmystreet/map.js'),
+    version('/cobrands/fixmystreet/offline.js'),
+);
+
+IF admin;
+    scripts.push(
+        version('/js/jquery-ui/js/jquery-ui-1.10.3.custom.min.js'),
+        version('/js/fixmystreet-admin.js'),
+    );
+END;
+
+FOR script IN extra_js;
+    scripts.push(script);
+END;
+
+TRY;
+    PROCESS 'footer_extra_js.html';
+CATCH file;
+END;
+
+~%]

templates/web/base/front/javascript.html
@@ -1,7 +1,12 @@
-[%# Assume using OpenStreetMap maps %]
-<script src="[% version('/js/yepnope.js') %]"></script>
-<script id="script_front" src="[% version('/cobrands/fixmystreet/front.js') %]"
-    data-scripts="
-        [%~ version('/js/OpenLayers/OpenLayers.fixmystreet.js') %],
-        [%~ version('/js/map-OpenLayers.js') %],
-        [%~ version('/js/map-OpenStreetMap.js') %]"></script>
+[%
+# Assume using OpenStreetMap maps
+map_js = [
+    version('/js/yepnope.js'),
+    [ version('/cobrands/fixmystreet/front.js'), {
+        id = 'script_front',
+        'data-scripts' = version('/js/OpenLayers/OpenLayers.fixmystreet.js') _ ',' _
+            version('/js/map-OpenLayers.js') _ ',' _
+            version('/js/map-OpenStreetMap.js')
+    } ],
+]
+%]

templates/web/base/header.html
@@ -7,7 +7,10 @@
 <!--[if IE 7]>   <html class="no-js ie7 iel8"[% html_att %]><![endif]-->
 <!--[if IE 8]>   <html class="no-js ie8 iel8"[% html_att %]><![endif]-->
 <!--[if IE 9]>   <html class="no-js ie9"[% html_att %]><![endif]-->
-<!--[if gt IE 9]><!--><html class="no-js"[% html_att %]><!--<![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js"[% html_att %]
+[% IF appcache ~%]
+ manifest="/offline/appcache.manifest"
+[%~ END %]><!--<![endif]-->
     <head>
         <meta name="viewport" content="initial-scale=1.0">
 
@@ -16,19 +19,11 @@
         <meta name="mobileoptimized" content="0">
 
         [% INCLUDE 'header_opengraph.html' %]
-        [%
-           # For clarity, the 'fixmystreet' moniker (for fixmystreet.com) puts
-           # it stylesheets under fixmystreet.com
-           IF c.cobrand.moniker == 'fixmystreet';
-               SET css_dir = 'fixmystreet.com';
-           ELSE;
-               SET css_dir = c.cobrand.moniker;
-           END %]
-        <link rel="stylesheet" href="[% version('/cobrands/' _ css_dir _ '/base.css') %]">
-        <link rel="stylesheet" href="[% version('/cobrands/' _ css_dir _ '/layout.css') %]" media="(min-width:48em)">
+        <link rel="stylesheet" href="[% version('/cobrands/' _ c.cobrand.asset_moniker _ '/base.css') %]">
+        <link rel="stylesheet" href="[% version('/cobrands/' _ c.cobrand.asset_moniker _ '/layout.css') %]" media="(min-width:48em)">
         [% extra_css %]
         <!--[if (lt IE 9) & (!IEMobile)]>
-            <link rel="stylesheet" href="[% version('/cobrands/' _ css_dir _ '/layout.css') %]">
+            <link rel="stylesheet" href="[% version('/cobrands/' _ c.cobrand.asset_moniker _ '/layout.css') %]">
         <![endif]-->
 
         [% INCLUDE 'common_header_tags.html' %]

commit db50779030f36673f8b63d1209e149d1ce34ba11

Fix use of .auto.min.js files.
fddf7f95 broke the use of minimized JS files, because it always used an
unchanged $url in the output. Make sure it is updated if $file is.

perllib/FixMyStreet/App/View/Web.pm
@@ -147,7 +147,7 @@ sub version {
         # See if there's an auto.min.js version and use that instead if there is
         (my $file_min = $file) =~ s/\.js$/.auto.min.js/;
         _version_get_mtime($file_min);
-        $file = $file_min if $version_hash{$file_min} >= $version_hash{$file};
+        $url = $file = $file_min if $version_hash{$file_min} >= $version_hash{$file};
     }
     my $admin = $self->template->context->stash->{admin} ? FixMyStreet->config('ADMIN_BASE_URL') : '';
     return "$admin$url?$version_hash{$file}";