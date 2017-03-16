1bc35ed00b5bf0c2df2f9b14d9bbe3bd95d921d8
pezholio <pezholio@gmail.com>
2017-03-16 09:35:42 +0000

    Use temporary proxy

--- web/cobrands/greenwich/js.js
+++ web/cobrands/greenwich/js.js
@@ -5,17 +5,17 @@ if (!fixmystreet.maps) {
 }
 
 $(fixmystreet.add_assets({
-    wfs_url: "http://royalaf3.miniserver.com:8080/geoserver/gis/ows?service=WFS",
+    wfs_url: "https://warm-bastion-39610.herokuapp.com/wfs",
     wfs_feature: "streetlights",
     asset_category: "Street lighting",
     asset_item: 'street light',
     asset_type: 'spot',
     max_resolution: 2.388657133579254,
     min_resolution: 0.5971642833948135,
-    asset_id_field: 'UNITNUMB',
-    propertyNames: [ 'UNITNUMB', 'the_geom' ],
+    asset_id_field: 'Unit_Numbe',
+    propertyNames: [ 'Unit_Numbe', 'the_geom' ],
     attributes: {
-        column_id: 'UNITNUMB'
+        column_id: 'Unit_Numbe'
     },
     geometryName: 'the_geom'
 }));
