5b808cc1a687465d33041bcc929552d9d2757715
pezholio <pezholio@gmail.com>
2017-03-29 12:52:35 +0100

    Use symology layer

--- web/cobrands/greenwich/js.js
+++ web/cobrands/greenwich/js.js
@@ -6,16 +6,16 @@ if (!fixmystreet.maps) {
 
 $(fixmystreet.add_assets({
     wfs_url: "https://warm-bastion-39610.herokuapp.com/wfs",
-    wfs_feature: "streetlights",
+    wfs_feature: "streetlights-symology",
     asset_category: "Street lighting",
     asset_item: 'street light',
     asset_type: 'spot',
     max_resolution: 2.388657133579254,
     min_resolution: 0.5971642833948135,
-    asset_id_field: 'Unit_Numbe',
-    propertyNames: [ 'Unit_Numbe', 'the_geom' ],
+    asset_id_field: 'UnitID',
+    propertyNames: [ 'UnitID', 'the_geom' ],
     attributes: {
-        lamp_column_id: 'Unit_Numbe'
+        lamp_column_id: 'UnitID'
     },
     geometryName: 'the_geom'
 }));
