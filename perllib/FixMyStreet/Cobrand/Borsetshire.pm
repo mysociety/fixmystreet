f4a7250eccdc70e3413c18f80b01aec7042a17da
Zarino Zappia <mail@zarino.co.uk>
2017-05-31 16:19:51 +0100

    [Borsetshire] New cobrand for fictional council

---
+++ perllib/FixMyStreet/Cobrand/Borsetshire.pm
@@ -0,0 +1,28 @@
+package FixMyStreet::Cobrand::Borsetshire;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+sub council_id { return 2608; }
+sub council_area { return 'Borsetshire'; }
+sub council_name { return 'Borsetshire County Council'; }
+sub council_url { return 'demo'; }
+
+sub example_places {
+    return ( 'BS36 2NS', 'Coalpit Heath' );
+}
+
+sub pin_colour {
+    my ( $self, $p, $context ) = @_;
+    #return 'grey' if $p->state eq 'not responsible';
+    #return 'green' if $p->is_fixed || $p->is_closed;
+    #return 'red' if $p->state eq 'confirmed';
+    return 'yellow';
+}
+
+sub send_questionnaires {
+    return 0;
+}
+
+1;
