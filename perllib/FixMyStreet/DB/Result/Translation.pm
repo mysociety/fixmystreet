46b9d8989d5ac2f8eedd196d11ceb4baf57728dd
Matthew Somerville <matthew-github@dracos.co.uk>
2017-07-13 09:48:27 +0100

    Add translation table.

---
+++ perllib/FixMyStreet/DB/Result/Translation.pm
@@ -0,0 +1,44 @@
+use utf8;
+package FixMyStreet::DB::Result::Translation;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+__PACKAGE__->load_components("FilterColumn", "InflateColumn::DateTime", "EncodedColumn");
+__PACKAGE__->table("translation");
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "translation_id_seq",
+  },
+  "tbl",
+  { data_type => "text", is_nullable => 0 },
+  "object_id",
+  { data_type => "integer", is_nullable => 0 },
+  "col",
+  { data_type => "text", is_nullable => 0 },
+  "lang",
+  { data_type => "text", is_nullable => 0 },
+  "msgstr",
+  { data_type => "text", is_nullable => 0 },
+);
+__PACKAGE__->set_primary_key("id");
+__PACKAGE__->add_unique_constraint(
+  "translation_tbl_object_id_col_lang_key",
+  ["tbl", "object_id", "col", "lang"],
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07035 @ 2017-07-14 23:24:32
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:///VNqg4BOuO29xKhnY8vw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
