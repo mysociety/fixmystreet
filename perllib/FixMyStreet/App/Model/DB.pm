2b4ed83466ddf54f6d7e77088dc7e755f9259e91
Matthew Somerville <matthew-github@dracos.co.uk>
2017-08-18 21:12:15 +0100

    Use a schema cache to remember body lookups.

--- perllib/FixMyStreet/App/Model/DB.pm
+++ perllib/FixMyStreet/App/Model/DB.pm
@@ -5,11 +5,20 @@ use strict;
 use warnings;
 
 use FixMyStreet;
+use Moose;
+
+with 'Catalyst::Component::InstancePerContext';
 
 __PACKAGE__->config(
     schema_class => 'FixMyStreet::DB::Schema',
     connect_info => sub { FixMyStreet::DB->schema->storage->dbh },
 );
+
+sub build_per_context_instance {
+    my ( $self, $c ) = @_;
+    $self->schema->cache({});
+    return $self;
+}
 
 =head1 NAME
 
@@ -17,7 +26,7 @@ FixMyStreet::App::Model::DB - Catalyst DBIC Schema Model
 
 =head1 DESCRIPTION
 
-L<Catalyst::Model::DBIC::Schema> Model using schema L<FixMyStreet::DB>
+L<Catalyst::Model::DBIC::Schema> Model using schema L<FixMyStreet::DB::Schema>
 
 =cut
 
