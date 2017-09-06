5dcd423da867a2139016e17791c2729cdd649b07
Matthew Somerville <matthew-github@dracos.co.uk>
2017-09-06 15:30:39 +0100

    [Borsetshire] Redirect users variously on login.

--- web/cobrands/borsetshire/js.js
+++ web/cobrands/borsetshire/js.js
@@ -1,9 +1,26 @@
 (function(){
+
+    function set_redirect(form) {
+        var e = form.email.value;
+        if (e == 'inspector@example.org') {
+            form.r.value = 'my/planned';
+        } else if (e == 'cs@example.org') {
+            form.r.value = 'reports';
+        } else if (e == 'super@example.org') {
+            form.r.value = 'admin';
+        }
+    }
+
     $('#demo-user-list dt').click(function(){
         var form = document.forms.general_auth;
         form.email.value = $(this).text();
         form.password_sign_in.value = 'password';
-        form.r.value = 'admin';
+        set_redirect(form);
         form.submit();
     });
+
+    $('form[name=general_auth]').on('submit', function() {
+        set_redirect(this);
+    });
+
 })();
