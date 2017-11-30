2dbf1d81c83fb64250828da8679a1d0557e8514c
Matthew Somerville <matthew-github@dracos.co.uk>
2017-11-30 13:31:37 +0000

    Stop using 'readable' dates in CSV export.
    We want machine-readable dates in a data export.

--- perllib/FixMyStreet/App/Controller/Dashboard.pm
+++ perllib/FixMyStreet/App/Controller/Dashboard.pm
@@ -346,11 +346,11 @@ sub export_as_csv {
             my $problem_state = $comment->problem_state or next;
             next unless $comment->state eq 'confirmed';
             next if $problem_state eq 'confirmed';
-            $hashref->{acknowledged_pp} //= $c->cobrand->prettify_dt( $comment->created );
-            $hashref->{fixed_pp} //= $fixed_states->{ $problem_state } || $comment->mark_fixed ?
-                $c->cobrand->prettify_dt( $comment->created ): undef;
+            $hashref->{acknowledged} //= $comment->confirmed;
+            $hashref->{fixed} //= $fixed_states->{ $problem_state } || $comment->mark_fixed ?
+                $comment->confirmed : undef;
             if ($closed_states->{ $problem_state }) {
-                $hashref->{closed_pp} = $c->cobrand->prettify_dt( $comment->created );
+                $hashref->{closed} = $comment->confirmed;
                 last;
             }
         }
@@ -369,11 +369,11 @@ sub export_as_csv {
                 'detail',
                 'user_name_display',
                 'category',
-                'created_pp',
-                'confirmed_pp',
-                'acknowledged_pp',
-                'fixed_pp',
-                'closed_pp',
+                'created',
+                'confirmed',
+                'acknowledged',
+                'fixed',
+                'closed',
                 'state',
                 'latitude', 'longitude',
                 'postcode',
