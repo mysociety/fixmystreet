89effa74e2fcb3e631a08102bed51744f705975c
Matthew Somerville <matthew-github@dracos.co.uk>
2017-12-19 17:13:25 +0000

    Fix colour wrapping bug, where it only added red.
    As colours.length increased as colours were added,
    it would always return the first entry.

--- web/js/dashboard.js
+++ web/js/dashboard.js
@@ -179,8 +179,8 @@ $(function(){
             rowValues.push( parseInt($(this).find('td').text(), 10) );
         });
 
-        for (var i=colours.length; i<rowLabels.length; i++) {
-            colours[i] = colours[i % colours.length];
+        for (var l=colours.length, i=l; i<rowLabels.length; i++) {
+            colours[i] = colours[i % l];
         }
 
         var barChart = new Chart($canvas, {
