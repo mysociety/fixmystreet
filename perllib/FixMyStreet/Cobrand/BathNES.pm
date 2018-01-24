0c1e942ea5989ef86e8ce93004c0ee61d23d24e9
Dave Arter <davea@mysociety.org>
2018-01-24 14:14:56 +0000

    [BANES] Send report title and detail in individual Open311 attributes
    The BANES Confirm integration puts the report title and description in different
    fields within Confirm. Because Open311 has no ‘title’ field for ServiceRequests,
    FMS will by default send the report title and description (along with a link) in
    a single string in the ‘description’ ServiceRequest field. The BANES
    open311-adapter code includes a title and description attribute on all published
    services, so this commit will fill those with the appropriate FMS fields when
    sending reports.

--- perllib/FixMyStreet/Cobrand/BathNES.pm
+++ perllib/FixMyStreet/Cobrand/BathNES.pm
@@ -71,5 +71,21 @@ sub category_extra_hidden {
     return 0;
 }
 
+sub open311_config {
+    my ($self, $row, $h, $params) = @_;
+
+    my $extra = $row->get_extra_fields;
+    push @$extra,
+        { name => 'report_url',
+          value => $h->{url} },
+        { name => 'title',
+          value => $row->title },
+        { name => 'description',
+          value => $row->detail };
+
+    $row->set_extra_fields(@$extra);
+}
+
+
 
 1;
