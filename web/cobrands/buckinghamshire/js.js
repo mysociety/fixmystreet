e2eeea01f89dd5d56d5776bf83b19e5e364d6883
Dave Arter <davea@mysociety.org>
2018-02-12 15:39:46 +0000

    [Buckinghamshire] Factor out shared asset config

--- web/cobrands/buckinghamshire/js.js
+++ web/cobrands/buckinghamshire/js.js
@@ -4,57 +4,56 @@ if (!fixmystreet.maps) {
     return;
 }
 
-fixmystreet.assets.add({
+var defaults = {
     http_options: {
         url: "https://davea.tilma.dev.mysociety.org/mapserver/bucks",
+        // url: "https://confirmdev.eu.ngrok.io/tilma/mapserver/bucks",
         params: {
             SERVICE: "WFS",
             VERSION: "1.1.0",
             REQUEST: "GetFeature",
-            SRSNAME: "urn:ogc:def:crs:EPSG::3857",
+            SRSNAME: "urn:ogc:def:crs:EPSG::3857"
+        }
+    },
+    asset_type: 'spot',
+    max_resolution: 2.388657133579254,
+    min_resolution: 0.5971642833948135,
+    asset_id_field: 'central_as',
+    attributes: {
+        central_asset_id: 'central_as',
+        site_code: 'Site_code'
+    },
+    geometryName: 'msGeometry',
+    srsName: "EPSG:3857",
+    strategy_class: OpenLayers.Strategy.FixMyStreet
+};
+
+fixmystreet.assets.add($.extend(true, {}, defaults, {
+    http_options: {
+        params: {
             TYPENAME: "Grit_Bins"
         }
     },
-    asset_category: "Grit bins",
-    asset_item: 'grit bin',
-    asset_type: 'spot',
-    max_resolution: 2.388657133579254,
-    min_resolution: 0.5971642833948135,
-    asset_id_field: 'central_as',
     attributes: {
         central_asset_id: 'central_as',
-        site_code: 'Site_code'
+        site_code: 'site_code' // different capitalisation, sigh
     },
-    geometryName: 'msGeometry',
-    srsName: "EPSG:3857",
-    strategy_class: OpenLayers.Strategy.FixMyStreet
-});
+    asset_category: "Grit bins",
+    asset_item: 'grit bin'
+}));
 
-fixmystreet.assets.add({
+fixmystreet.assets.add($.extend(true, {}, defaults, {
     http_options: {
-        url: "https://davea.tilma.dev.mysociety.org/mapserver/bucks",
         params: {
-            SERVICE: "WFS",
-            VERSION: "1.1.0",
-            REQUEST: "GetFeature",
-            SRSNAME: "urn:ogc:def:crs:EPSG::3857",
-            TYPENAME: "SL_Merged"
+            TYPENAME: "StreetLights_Merged"
         }
     },
-    asset_category: "Street lighting",
-    asset_item: 'street light',
-    asset_type: 'spot',
-    max_resolution: 2.388657133579254,
-    min_resolution: 0.5971642833948135,
-    asset_id_field: 'central_as',
     attributes: {
         central_asset_id: 'central_as',
         site_code: 'Site_code'
     },
-    geometryName: 'msGeometry',
-    srsName: "EPSG:3857",
-    strategy_class: OpenLayers.Strategy.FixMyStreet
-});
-
+    asset_category: "Street lighting",
+    asset_item: 'street light'
+}));
 
 })();
