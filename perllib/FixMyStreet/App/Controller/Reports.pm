0c4e267f43c573f638dd4771e5c39f6b3861dd3d
Matthew Somerville <matthew-github@dracos.co.uk>
2018-04-16 17:26:12 +0100

    Add hook to alter list of bodies on /reports page.
    This allows a cobrand to e.g. display a subset
    of bodies based on a query parameter.

--- perllib/FixMyStreet/App/Controller/Reports.pm
+++ perllib/FixMyStreet/App/Controller/Reports.pm
@@ -93,6 +93,7 @@ sub index : Path : Args(0) {
         }
     } else {
         my @bodies = $c->model('DB::Body')->active->translated->with_area_count->all_sorted;
+        @bodies = @{$c->cobrand->call_hook('reports_hook_restrict_bodies_list', \@bodies) || \@bodies };
         $c->stash->{bodies} = \@bodies;
     }
 
