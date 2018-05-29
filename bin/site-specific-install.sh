4f4943d87fc418c9bc8fada34dd987e3b031fa4a
Matthew Somerville <matthew-github@dracos.co.uk>
2018-05-29 13:38:11 +0100

    Version 2.3.2.

--- bin/site-specific-install.sh
+++ bin/site-specific-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set this to the version we want to check out
-VERSION=${VERSION_OVERRIDE:-v2.3.1}
+VERSION=${VERSION_OVERRIDE:-v2.3.2}
 
 PARENT_SCRIPT_URL=https://github.com/mysociety/commonlib/blob/master/bin/install-site.sh
 
