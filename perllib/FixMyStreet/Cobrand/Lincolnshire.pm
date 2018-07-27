eaa6187014f03499ca459761b508a95638cb629d
Dave Arter <davea@mysociety.org>
2018-07-27 08:31:03 +0100

    [Lincolnshire] Disable questionnaires

--- perllib/FixMyStreet/Cobrand/Lincolnshire.pm
+++ perllib/FixMyStreet/Cobrand/Lincolnshire.pm
@@ -16,7 +16,7 @@ sub council_url { return 'lincolnshire'; }
 sub is_two_tier { 1 }
 
 sub enable_category_groups { 1 }
-
+sub send_questionnaires { 0 }
 sub report_sent_confirmation_email { 1 }
 
 sub example_places {
