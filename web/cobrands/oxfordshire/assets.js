eabfd66db23861f464c4eac10e3b96a0bb0da914
Struan Donald <struan@exo.org.uk>
2018-09-12 12:26:56 +0100

    [Oxfordshire] display message if user selects PROW category
    Adds some div wrappers round the non category bit of the form so we can
    then hide easily.

---
+++ web/cobrands/oxfordshire/assets.js
@@ -0,0 +1,18 @@
+(function(){
+  function disable_form(disable) {
+    $('#post_category_details_form').toggle(!disable);
+    $('#private_form').toggle(!disable);
+  }
+
+  function check_rights_of_way() {
+    if (OpenLayers.Util.indexOf(fixmystreet.bodies, 'Oxfordshire County Council') > -1 && $('#form_category').val() == 'Countryside Paths / Public Rights of Way (usually not tarmac)') {
+        $('#category_meta').html('<p class="category-message">Please report problems with rights of way using <a href="https://publicrightsofway.oxfordshire.gov.uk/web/standardmap.aspx">this page</a>.</p>');
+        disable_form(true);
+    } else {
+        $('#category_meta').html('');
+        disable_form(false);
+    }
+  }
+
+  $(fixmystreet).on('report_new:category_change', check_rights_of_way);
+})();
