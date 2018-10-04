96bb52d449517b1018841e56e6008dedd03e6f3b
Matthew Somerville <matthew-github@dracos.co.uk>
2018-10-04 14:39:19 +0100

    [Oxfordshire] Only run PROW code in Oxfordshire.

--- web/cobrands/oxfordshire/assets.js
+++ web/cobrands/oxfordshire/assets.js
@@ -5,7 +5,11 @@
   }
 
   function check_rights_of_way() {
-    if (OpenLayers.Util.indexOf(fixmystreet.bodies, 'Oxfordshire County Council') > -1 && $('#form_category').val() == 'Countryside Paths / Public Rights of Way (usually not tarmac)') {
+    if (OpenLayers.Util.indexOf(fixmystreet.bodies, 'Oxfordshire County Council') == -1) {
+        return;
+    }
+
+    if ($('#form_category').val() == 'Countryside Paths / Public Rights of Way (usually not tarmac)') {
         $('#category_meta').html('<p class="category-message">Please report problems with rights of way using <a href="https://publicrightsofway.oxfordshire.gov.uk/web/standardmap.aspx">this page</a>.</p>');
         disable_form(true);
     } else {
