bc25569ba73ac37730780a4efea6f84f254d54b7
Struan Donald <struan@exo.org.uk>
2019-04-25 14:02:51 +0100

    [Isle of Wight] Initial cobrand.
    Co-authored-by: Matthew Somerville <matthew@mysociety.org>

---
+++ perllib/FixMyStreet/Cobrand/IsleOfWight.pm
@@ -0,0 +1,46 @@
+package FixMyStreet::Cobrand::IsleOfWight;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+sub council_area_id { 2636 }
+sub council_area { 'Isle of Wight' }
+sub council_name { 'Isle of Wight Council' }
+sub council_url { 'isleofwight' }
+sub example_places { ( 'PO30 5XJ', "Daish Way" ) }
+
+sub base_url {
+    my $self = shift;
+    return $self->next::method() if FixMyStreet->config('STAGING_SITE');
+    # uncoverable statement
+    return 'https://islandroads.fixmystreet.com';
+}
+
+sub on_map_default_status { 'open' }
+
+sub contact_email {
+    my $self = shift;
+    return join( '@', 'info', 'islandroads.com' );
+}
+
+sub send_questionnaires { 0 }
+
+sub enable_category_groups { 1 }
+
+sub map_type { 'OSM' }
+
+sub disambiguate_location {
+    my $self    = shift;
+    my $string  = shift;
+
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        centre => '50.675761,-1.296571',
+        bounds => [ 50.574653, -1.591732, 50.767567, -1.062957 ],
+    };
+}
+
+# sub get_geocoder { 'OSM' }
+
+1;
