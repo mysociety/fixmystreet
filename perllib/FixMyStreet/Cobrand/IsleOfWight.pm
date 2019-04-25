effcb0f3043f3684e94db4866d0b434440e8ba53
Struan Donald <struan@exo.org.uk>
2019-04-25 14:02:51 +0100

    [IsleOfWight] Initial cobrand.
    Co-authored-by: Matthew Somerville <matthew@mysociety.org>
    
    Configured to allow:
     - updates only from original reporter
     - close updates from fetch reports
     - use OSM geocoder

---
+++ perllib/FixMyStreet/Cobrand/IsleOfWight.pm
@@ -0,0 +1,70 @@
+package FixMyStreet::Cobrand::IsleOfWight;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+sub council_area_id { 2636 }
+sub council_area { 'Isle of Wight' }
+sub council_name { 'Island Roads' }
+sub council_url { 'isleofwight' }
+sub all_reports_single_body { { name => "Isle of Wight Council" } }
+sub link_to_council_cobrand { "Island Roads" }
+
+sub enter_postcode_text {
+    my ($self) = @_;
+    return 'Enter an ' . $self->council_area . ' postcode, or street name and area';
+}
+
+sub on_map_default_status { 'open' }
+
+sub send_questionnaires { 0 }
+
+sub map_type { 'OSM' }
+
+sub disambiguate_location {
+    my $self    = shift;
+    my $string  = shift;
+
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        centre => '50.675761,-1.296571',
+        bounds => [ 50.574653, -1.591732, 50.767567, -1.062957 ],
+    };
+}
+
+sub updates_disallowed {
+    my ($self, $problem) = @_;
+
+    my $c = $self->{c};
+    return 0 if $c->user_exists && $c->user->id eq $problem->user->id;
+    return 1;
+}
+
+sub get_geocoder { 'OSM' }
+
+sub open311_get_update_munging {
+    my ($self, $comment) = @_;
+
+    # If we've received an update via Open311 that's closed
+    # or fixed the report, also close it to updates.
+    $comment->problem->set_extra_metadata(closed_updates => 1)
+        if !$comment->problem->is_open;
+}
+
+sub open311_config {
+    my ($self, $row, $h, $params) = @_;
+
+    my $extra = $row->get_extra_fields;
+    push @$extra,
+        { name => 'report_url',
+          value => $h->{url} },
+        { name => 'title',
+          value => $row->title },
+        { name => 'description',
+          value => $row->detail };
+
+    $row->set_extra_fields(@$extra);
+}
+
+1;
