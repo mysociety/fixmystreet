08901fb2ffea5c834e994f148ff60502f49003d4
Dave Arter <davea@mysociety.org>
2019-05-03 14:40:47 +0100

    [Hounslow] Display name as ‘Hounslow Highways’ not borough council
    Technically the cobrand isn't for the council but rather the Hounslow
    Highways PFI. So make it clear on the cobranded site where reports
    will be sent.

--- web/cobrands/hounslow/js.js
+++ web/cobrands/hounslow/js.js
@@ -15,4 +15,16 @@ fixmystreet.assets.add($.extend(true, {}, fixmystreet.roadworks.layer_planned, {
     body: body
 }));
 
+if (fixmystreet.cobrand == 'hounslow') {
+    // We want the cobranded site to always display "Hounslow Highways"
+    // as the destination for reports in the "Public details" section.
+    // This is OK because the cobranded site only shows categories which
+    // Hounslow Highways actually handle.
+    // Replacing this function with a no-op stops the changes made
+    // to the cobranded councils_text_all.html from being clobbered and
+    // the 'correct' (according to bodies set up within FMS) body names
+    // being shown.
+    fixmystreet.update_public_councils_text = function() {};
+}
+
 })();
