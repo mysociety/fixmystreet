8c8b62e2b3a8af1b1f6fafe4080296a3826b7c1e
Matthew Somerville <matthew-github@dracos.co.uk>
2019-05-17 10:48:16 +0100

    [Bexley] Stop private property dead animal reporting.

--- web/cobrands/bexley/js.js
+++ web/cobrands/bexley/js.js
@@ -130,5 +130,28 @@ fixmystreet.assets.add(labeled_defaults, {
     asset_item: 'street light'
 });
 
+// We need to trigger the below function on subcategory change also
+$(function(){
+    $("#problem_form").on("change.category", "#form_DALocation", function() {
+        $(fixmystreet).trigger('report_new:category_change', [ $('#form_category') ]);
+    });
+});
+
+fixmystreet.message_controller.register_category({
+    body: defaults.body,
+    category: function() {
+        var cat = $('#form_category').val();
+        if (cat === 'Dead animal') {
+            var where = $('#form_DALocation').val();
+            if (where === 'Garden' || where === 'Other private property') {
+                return true;
+            }
+        }
+        return false;
+    },
+    keep_category_extras: true,
+    message: 'Please follow the link below to pay to remove a dead animal from a private property.'
+});
+
 })();
 
