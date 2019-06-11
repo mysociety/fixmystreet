ea0dd73ecba3252f32830dc9000ef9d56f17aad7
Struan Donald <struan@exo.org.uk>
2019-06-11 13:49:37 +0100

    [IsleOfWight] use Island Roads in place of council name
    fixes mysociety/fixmystreet-commercial#1419

---
+++ .cypress/cypress/integration/isleofwight.js
@@ -0,0 +1,19 @@
+describe('When you look at the Island Roads site', function() {
+
+  beforeEach(function() {
+    cy.server();
+    cy.fixture('roads.xml');
+    cy.route('/report/new/ajax*').as('report-ajax');
+    cy.visit('http://isleofwight.localhost:3001/');
+    cy.contains('Island Roads');
+    cy.get('[name=pc]').type('PO30 5XJ');
+    cy.get('[name=pc]').parents('form').submit();
+  });
+
+  it('uses the correct name', function() {
+    cy.get('#map_box').click();
+    cy.wait('@report-ajax');
+    cy.get('select:eq(4)').select('Potholes');
+    cy.contains('sent to Island Roads');
+  });
+});
