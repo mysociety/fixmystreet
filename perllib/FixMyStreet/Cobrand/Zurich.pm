1292c3955b81ef045d7424924cc1453a04130219
Matthew Somerville <matthew@mysociety.org>
2019-06-24 18:43:46 +0100

    [Zurich] Use Beantwortet on external banner.

--- perllib/FixMyStreet/Cobrand/Zurich.pm
+++ perllib/FixMyStreet/Cobrand/Zurich.pm
@@ -141,7 +141,7 @@ sub problem_as_hashref {
         $hashref->{title} = _('This report is awaiting moderation.');
         $hashref->{banner_id} = 'closed';
     } else {
-        if ( $problem->state eq 'confirmed' || $problem->state eq 'external' ) {
+        if ( $problem->state eq 'confirmed' ) {
             $hashref->{banner_id} = 'closed';
         } elsif ( $problem->is_fixed || $problem->is_closed ) {
             $hashref->{banner_id} = 'fixed';
@@ -152,7 +152,7 @@ sub problem_as_hashref {
         if ( $problem->state eq 'confirmed' ) {
             $hashref->{state} = 'open';
             $hashref->{state_t} = _('Open');
-        } elsif ( $problem->state eq 'wish' ) {
+        } elsif ( $problem->state eq 'wish' || $problem->state eq 'external' ) {
             $hashref->{state_t} = _('Closed');
         } elsif ( $problem->is_fixed ) {
             $hashref->{state} = 'closed';
