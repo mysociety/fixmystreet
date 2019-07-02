dd19a3c76b8991540dd35056cbfc0e49a40f5a6a
Martin Wright <martin@mysociety.org>
2019-07-02 17:29:32 +0100

    [Peterborough] Initial cobrand

---
+++ perllib/FixMyStreet/Cobrand/Peterborough.pm
@@ -0,0 +1,23 @@
+package FixMyStreet::Cobrand::Peterborough;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+sub council_area_id { 2566 }
+sub council_area { 'Peterborough' }
+sub council_name { 'Peterborough City Council' }
+sub council_url { 'peterborough' }
+
+sub disambiguate_location {
+    my $self    = shift;
+    my $string  = shift;
+
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        centre => '52.6085234396978,-0.253091266573947',
+        bounds => [ 52.5060949603654, -0.497663559599628, 52.6752139533306, -0.0127696975457487 ],
+    };
+}
+
+1;
