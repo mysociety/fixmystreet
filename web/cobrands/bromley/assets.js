fc32e5be18693136029e79feafb3ad7c6fbcb4d8
Matthew Somerville <matthew@mysociety.org>
2019-09-04 11:56:04 +0100

    [Bromley] Use live map server.

--- web/cobrands/bromley/assets.js
+++ web/cobrands/bromley/assets.js
@@ -6,7 +6,7 @@ if (!fixmystreet.maps) {
 
 var defaults = {
     http_options: {
-        url: "https://tilma.staging.mysociety.org/mapserver/bromley_wfs",
+        url: "https://tilma.mysociety.org/mapserver/bromley_wfs",
         params: {
             SERVICE: "WFS",
             VERSION: "1.1.0",
