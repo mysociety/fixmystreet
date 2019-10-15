3644641d25c9bbbb0b6ba6e0362ec5914ef601df
Dave Arter <davea@mysociety.org>
2019-10-15 11:07:03 +0100

    [TfL] Add traffic signals & bus stops asset layers
    Connects https://github.com/mysociety/fixmystreet-commercial/issues/1631

---
+++ web/cobrands/tfl/assets.js
@@ -0,0 +1,54 @@
+(function(){
+
+if (!fixmystreet.maps) {
+    return;
+}
+
+var defaults = {
+    http_options: {
+        url: "https://tilma.staging.mysociety.org/mapserver/tfl",
+        params: {
+            SERVICE: "WFS",
+            VERSION: "1.1.0",
+            REQUEST: "GetFeature",
+            SRSNAME: "urn:ogc:def:crs:EPSG::3857"
+        }
+    },
+    asset_type: 'spot',
+    max_resolution: 2.388657133579254,
+    min_resolution: 0.5971642833948135,
+    geometryName: 'msGeometry',
+    srsName: "EPSG:3857",
+    strategy_class: OpenLayers.Strategy.FixMyStreet,
+    body: "TfL"
+};
+
+fixmystreet.assets.add($.extend(true, {}, defaults, {
+    http_options: {
+        params: {
+            TYPENAME: "trafficsignals"
+        }
+    },
+    asset_id_field: 'Site',
+    attributes: {
+        site: 'Site',
+    },
+    asset_group: "Traffic Lights",
+    asset_item: 'traffic signal'
+}));
+
+fixmystreet.assets.add($.extend(true, {}, defaults, {
+    http_options: {
+        params: {
+            TYPENAME: "busstops"
+        }
+    },
+    asset_id_field: 'STOP_CODE',
+    attributes: {
+        stop_code: 'STOP_CODE',
+    },
+    asset_group: "Bus Stops and Shelters",
+    asset_item: 'bus stop'
+}));
+
+})();
