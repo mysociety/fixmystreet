97f6752794a61fd35b41b951b8a944c021a05bdf
Dave Arter <davea@mysociety.org>
2019-10-15 10:33:29 +0100

    [TfL] Add roadworks.org
    Connects https://github.com/mysociety/fixmystreet-commercial/issues/1605

--- web/cobrands/tfl/js.js
+++ web/cobrands/tfl/js.js
@@ -4,6 +4,17 @@ if (!fixmystreet.maps) {
     return;
 }
 
+var org_id = '1250';
+var body = "TfL";
+fixmystreet.assets.add(fixmystreet.roadworks.layer_future, {
+    http_options: { params: { organisation_id: org_id } },
+    body: body
+});
+fixmystreet.assets.add(fixmystreet.roadworks.layer_planned, {
+    http_options: { params: { organisation_id: org_id } },
+    body: body
+});
+
 if (fixmystreet.cobrand == 'tfl') {
     // We want the cobranded site to always display "TfL"
     // as the destination for reports in the "Public details" section.
