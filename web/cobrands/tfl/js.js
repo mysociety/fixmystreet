64d09262cabeac85704067d03a358b86265ef27b
Dave Arter <davea@mysociety.org>
2019-11-05 10:51:00 +0000

    [TfL] Show category group in label above category dropdown on inspector form

---
+++ web/cobrands/tfl/js.js
@@ -0,0 +1,18 @@
+(function(){
+
+$(function() {
+    function update_category_group_label() {
+        var group = $("#report_inspect_form select#category option:selected").closest("optgroup").attr('label');
+        var $label = $("#report_inspect_form select#category").closest("p").find("label");
+        if (group) {
+            $label.text("Category (" + group + ")");
+        } else {
+            $label.text("Category");
+        }
+    }
+    $(document).on('change', "#report_inspect_form select#category", update_category_group_label);
+    $(fixmystreet).on('display:report', update_category_group_label);
+    update_category_group_label();
+});
+
+})();
