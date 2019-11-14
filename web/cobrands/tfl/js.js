116565e90a8ede50fcc3c126a1265ab7ea72db09
Matthew Somerville <matthew@mysociety.org>
2019-11-14 12:57:52 +0000

    [TfL] Put councils_text above category dropdown.

--- web/cobrands/tfl/js.js
+++ web/cobrands/tfl/js.js
@@ -1,5 +1,8 @@
 (function(){
 
+translation_strings.name.validName = 'Please enter your full name, Transport for London needs this information – if you do not wish your name to be shown on the site, untick the box below';
+translation_strings.upload_default_message = 'Drag photo here to upload or <u>browse files</u>';
+
 $(function() {
     function update_category_group_label() {
         var group = $("#report_inspect_form select#category option:selected").closest("optgroup").attr('label');
