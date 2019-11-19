c610f3b3eb942d0595793167a700e7ecfd742d38
Matthew Somerville <matthew@mysociety.org>
2019-11-19 16:48:03 +0000

    [TfL] Link through to .com with correct co-ordinates.

--- web/cobrands/tfl/js.js
+++ web/cobrands/tfl/js.js
@@ -3,6 +3,20 @@
 translation_strings.name.validName = 'Please enter your full name, Transport for London needs this information – if you do not wish your name to be shown on the site, untick the box below';
 translation_strings.upload_default_message = 'Drag photo here to upload or <u>browse files</u>';
 
+fixmystreet.tfl_link_update = function() {
+    var lat = document.getElementById('fixmystreet.latitude');
+    if (!lat) {
+        return;
+    }
+    lat = lat.value;
+    var lon = document.getElementById('fixmystreet.longitude').value;
+    $('.js-not-tfl-link').each(function(){
+        this.search = 'latitude=' + lat + '&longitude=' + lon;
+    });
+};
+$(fixmystreet).on('maps:update_pin', fixmystreet.tfl_link_update);
+$(fixmystreet).on('report_new:category_change', fixmystreet.tfl_link_update);
+
 $(function() {
     function update_category_group_label() {
         var group = $("#report_inspect_form select#category option:selected").closest("optgroup").attr('label');
