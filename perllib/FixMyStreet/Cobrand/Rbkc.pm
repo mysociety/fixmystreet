3b013ac56934a506814750c53a0ebf22d950e0cb
Graeme Porteous <graeme@rgbp.co.uk>
2020-01-13 14:58:48 +0000

    Initial RBKC cobrand based of whitelabel theme

---
+++ perllib/FixMyStreet/Cobrand/Rbkc.pm
@@ -0,0 +1,24 @@
+package FixMyStreet::Cobrand::Rbkc;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+sub council_area_id { 2503 }
+sub council_area { 'Royal Borough of Kensington and Chelsea' }
+sub council_name { 'Kensington and Chelsea Borough Council' }
+sub council_url { 'rbkc' }
+sub admin_user_domain { 'rbkc.gov.uk' }
+
+sub disambiguate_location {
+    my $self    = shift;
+    my $string  = shift;
+
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        centre => '51.501206,-0.192219',
+        bounds => [ 51.477221, -0.228727, 51.530352, -0.149843 ],
+    };
+}
+
+1;
