00fe7b548cee48c9683e4fa99ed148cdf911cc84
Struan Donald <struan@exo.org.uk>
2020-01-16 18:30:10 +0000

    [Oxfordshire] add asset layers
    Trees, street lights, traffic lights, grit bins and gullies

---
+++ web/cobrands/oxfordshire/assets.js
@@ -0,0 +1,138 @@
+(function(){
+
+if (!fixmystreet.maps) {
+    return;
+}
+
+var wfs_host = fixmystreet.staging ? 'tilma.staging.mysociety.org' : 'tilma.mysociety.org';
+var tilma_url = "https://" + wfs_host + "/mapserver/oxfordshire";
+var proxy_base_url = "https://" + wfs_host + "/proxy/occ/";
+
+var defaults = {
+    wfs_url: tilma_url,
+    asset_type: 'spot',
+    max_resolution: 4.777314267158508,
+    geometryName: 'msGeometry',
+    srsName: "EPSG:3857",
+    body: "Oxfordshire County Council",
+    strategy_class: OpenLayers.Strategy.FixMyStreet
+};
+
+var occ_default = $.extend({}, fixmystreet.assets.style_default.defaultStyle, {
+    fillColor: "#007258"
+});
+
+var occ_hover = new OpenLayers.Style({
+    pointRadius: 8,
+    cursor: 'pointer'
+});
+
+var occ_stylemap = new OpenLayers.StyleMap({
+    'default': occ_default,
+    'select': fixmystreet.assets.style_default_select,
+    'hover': occ_hover
+});
+
+fixmystreet.assets.add(defaults, {
+    stylemap: occ_stylemap,
+    wfs_feature: "Trees",
+    asset_id_field: 'Ref',
+    attributes: {
+        feature_id: 'Ref'
+    },
+    asset_category: ["Trees"],
+    asset_item: 'tree'
+});
+
+fixmystreet.assets.add(defaults, {
+    stylemap: occ_stylemap,
+    wfs_feature: "Traffic_Lights",
+    asset_id_field: 'Site',
+    attributes: {
+        feature_id: 'Site'
+    },
+    asset_category: ["Traffic Lights (permanent only)"],
+    asset_item: 'traffic light'
+});
+
+var streetlight_select = $.extend({
+    label: "${UNITNO}",
+    fontColor: "#FFD800",
+    labelOutlineColor: "black",
+    labelOutlineWidth: 3,
+    labelYOffset: 69,
+    fontSize: '18px',
+    fontWeight: 'bold'
+}, fixmystreet.assets.style_default_select.defaultStyle);
+
+var streetlight_stylemap = new OpenLayers.StyleMap({
+  'default': occ_default,
+  'select': new OpenLayers.Style(streetlight_select),
+  'hover': occ_hover
+});
+
+fixmystreet.assets.add(defaults, {
+    select_action: true,
+    stylemap: streetlight_stylemap,
+    wfs_feature: "Street_Lights",
+    asset_id_field: 'UNITID',
+    attributes: {
+        feature_id: 'UNITID',
+        column_no: 'UNITNO'
+    },
+    asset_category: ["Street lighting"],
+    asset_item: 'street light',
+    feature_code: 'UNITNO',
+    actions: {
+        asset_found: fixmystreet.assets.named_select_action_found,
+        asset_not_found: fixmystreet.assets.named_select_action_not_found
+    }
+});
+
+var owned_defaults = $.extend({}, defaults, {
+    stylemap: occ_stylemap,
+    // have to do this by hand rather than using wfs_* options
+    // as the server does not like being POSTed xml with application/xml
+    // as the Content-Type which is what using those options results in.
+    http_options: {
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        params: {
+            SERVICE: "WFS",
+            VERSION: "1.1.0",
+            REQUEST: "GetFeature",
+            SRSNAME: "urn:ogc:def:crs:EPSG::27700",
+            propertyName: 'id,maintained_by,msGeometry'
+        }
+    },
+    srsName: "EPSG:27700",
+    asset_id_field: 'id',
+    attributes: {
+        feature_id: 'id'
+    }
+});
+
+fixmystreet.assets.add(owned_defaults, {
+    http_options: {
+        url: proxy_base_url + 'drains/wfs',
+        params: {
+            TYPENAME: "junctions"
+        }
+    },
+    asset_category: ["Gully and Catchpits"],
+    asset_item: 'drain'
+});
+
+fixmystreet.assets.add(owned_defaults, {
+    http_options: {
+        url: proxy_base_url + 'grit/wfs',
+        params: {
+            TYPENAME: "Grit_bins"
+        }
+    },
+    asset_category: ["Ice/Snow"],
+    asset_item: 'grit bin'
+});
+
+})();
