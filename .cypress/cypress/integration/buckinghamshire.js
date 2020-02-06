152f8f4e608abc5029104e652f4ef37d4cfe02cb
Chris Mytton <chrism@mysociety.org>
2020-02-06 17:26:18 +0000

    [Buckinghamshire] Change "Name" to "Full name" on forms
    Since we require the user to enter their full name Buckinghamshire have
    asked us to change the text to "Full name" to make it clearer that the
    full name is required to complete the form.

--- .cypress/cypress/integration/buckinghamshire.js
+++ .cypress/cypress/integration/buckinghamshire.js
@@ -5,6 +5,7 @@ describe('flytipping', function() {
     cy.fixture('roads.xml');
     cy.route('**mapserver/bucks*Whole_Street*', 'fixture:roads.xml').as('roads-layer');
     cy.route('/report/new/ajax*').as('report-ajax');
+    cy.route('/around/nearby*').as('around-ajax');
     cy.visit('http://buckinghamshire.localhost:3001/');
     cy.contains('Buckinghamshire');
     cy.get('[name=pc]').type('SL9 0NX');
@@ -49,6 +50,19 @@ describe('flytipping', function() {
     cy.wait('@report-ajax');
     cy.get('select:eq(4)').select('Parks');
     cy.get('[name=site_code]').should('have.value', '7300268');
+  });
+
+  it('uses the label "Full name" for the name field', function() {
+    cy.get('#map_box').click(290, 307);
+    cy.wait('@report-ajax');
+    cy.get('select:eq(4)').select('Flytipping');
+    cy.wait('@around-ajax');
+    cy.get('.js-hide-duplicate-suggestions:first').should('be.visible').click();
+
+    cy.get('[name=title]').type('Title');
+    cy.get('[name=detail]').type('Detail');
+    cy.get('.js-new-report-user-show').click();
+    cy.get('label[for=form_name]').should('contain', 'Full name');
   });
 
 });
