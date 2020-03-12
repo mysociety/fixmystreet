1553bf38c1745a8ebde5481df724246e8e28c5e5
Chris Mytton <chrism@mysociety.org>
2020-03-12 06:41:47 +0000

    [Highways England] Add cobrand package

---
+++ perllib/FixMyStreet/Cobrand/HighwaysEngland.pm
@@ -0,0 +1,11 @@
+package FixMyStreet::Cobrand::HighwaysEngland;
+use parent 'FixMyStreet::Cobrand::UK';
+
+use strict;
+use warnings;
+
+sub enter_postcode_text { 'Enter a location, road name or postcode' }
+
+sub allow_photo_upload { 0 }
+
+1;
