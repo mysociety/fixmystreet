7c0329f45bd58b3592db87d3050a767fdcb9106d
Chris Mytton <chrism@mysociety.org>
2020-03-13 16:43:12 +0000

    [Highways England] Example places

--- perllib/FixMyStreet/Cobrand/HighwaysEngland.pm
+++ perllib/FixMyStreet/Cobrand/HighwaysEngland.pm
@@ -1,3 +1,4 @@
+use utf8;
 package FixMyStreet::Cobrand::HighwaysEngland;
 use parent 'FixMyStreet::Cobrand::UK';
 
@@ -5,6 +6,10 @@ use strict;
 use warnings;
 
 sub enter_postcode_text { 'Enter a location, road name or postcode' }
+
+sub example_places {
+    ['M60, Junction 2’, ‘M6 323.5', 'Spaghetti Junction']
+}
 
 sub allow_photo_upload { 0 }
 
