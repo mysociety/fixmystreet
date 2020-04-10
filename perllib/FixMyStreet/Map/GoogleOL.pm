979d767ee8d3391828025cede8f623f25490ee96
Matthew Somerville <matthew@mysociety.org>
2020-04-10 10:10:25 +0100

    Stop double escape in Google Maps URL.

--- perllib/FixMyStreet/Map/GoogleOL.pm
+++ perllib/FixMyStreet/Map/GoogleOL.pm
@@ -16,7 +16,7 @@ sub map_template { 'google-ol' }
 sub map_javascript {
     my $google_maps_url = "https://maps.googleapis.com/maps/api/js?v=3";
     my $key = FixMyStreet->config('GOOGLE_MAPS_API_KEY');
-    $google_maps_url .= "&amp;key=$key" if $key;
+    $google_maps_url .= "&key=$key" if $key;
     [
         $google_maps_url,
         '/vendor/OpenLayers/OpenLayers.google.js',
