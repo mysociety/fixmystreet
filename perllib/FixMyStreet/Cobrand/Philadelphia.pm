160ba4cb09104bb48bb912cb4f31da92fc90efb1
Alex <tuk60559@temple.edu>
2020-04-14 10:08:36 -0400

    baseline perl module for cobrand perllib/FixMyStreet/Cobrand/Philadelphia.pm

---
+++ perllib/FixMyStreet/Cobrand/Philadelphia.pm
@@ -0,0 +1,12 @@
+package FixMyStreet::Cobrand::Philadelphia;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+sub council_area_id { return 2608; }
+sub council_area { return 'Philadelphia'; }
+sub council_name { return 'Borsetshire County Council'; }
+sub council_url { return 'demo'; }
+
+
