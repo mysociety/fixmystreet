10d8de2f08f8eda7273398e643fd871402284960
Alex <tuk60559@temple.edu>
2020-04-14 22:42:24 -0400

    Update general.yml parameters to serve Philly, potentially linked it with the new cobrand module? Need testing

--- perllib/FixMyStreet/Cobrand/Philadelphia.pm
+++ perllib/FixMyStreet/Cobrand/Philadelphia.pm
@@ -13,6 +13,16 @@ sub disable_resend_button { 1 }
 
 sub on_map_default_status { 'open' }
 
+sub pin_colour {
+    my ( $self, $p, $context ) = @_;
+    return 'grey' if $p->is_closed;
+    return 'green' if $p->is_fixed;
+    return 'yellow' if $p->state eq 'confirmed';
+    return 'orange'; # all the other `open_states` like "in progress"
+}
+
 sub path_to_pin_icons {
     return '/cobrands/philadelphia/images/';
 }
+
+sub bypass_password_checks { 1 }
