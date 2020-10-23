e37e3af3656b4545e638494c3ada61e37df7ee0a
M Somerville <matthew-github@dracos.co.uk>
2020-10-23 10:31:52 +0100

    [Borsetshire] Move sign-in button code inline.
    This should fix a race condition on Travis where the button is clicked
    before the JavaScript on the button has registered.

--- web/cobrands/borsetshire/js.js
+++ web/cobrands/borsetshire/js.js
@@ -4,27 +4,8 @@
         return;
     }
 
-    function set_redirect(form) {
-        var e = form.username.value;
-        if (e == 'inspector@example.org') {
-            form.r.value = 'my/planned';
-        } else if (e == 'cs@example.org') {
-            form.r.value = 'reports';
-        } else if (e == 'super@example.org') {
-            form.r.value = 'admin';
-        }
-    }
-
-    $('#demo-user-list button').click(function(){
-        var form = document.forms.general_auth;
-        form.username.value = $(this).data('email');
-        form.password_sign_in.value = 'password';
-        set_redirect(form);
-        form.submit();
-    });
-
     $('form[name=general_auth]').on('submit', function() {
-        set_redirect(this);
+        fixmystreet.borsetshire.set_redirect(this);
     });
 
 })();
