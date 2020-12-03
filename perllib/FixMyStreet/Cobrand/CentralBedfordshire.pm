2f4485894503d56d28d546ca83eae18093f4a841
Dave Arter <davea@mysociety.org>
2020-12-03 15:36:08 +0000

    [Central Beds] Add report reference prompt to front page form

--- perllib/FixMyStreet/Cobrand/CentralBedfordshire.pm
+++ perllib/FixMyStreet/Cobrand/CentralBedfordshire.pm
@@ -24,6 +24,8 @@ sub disambiguate_location {
     };
 }
 
+sub enter_postcode_text { 'Enter a postcode, street name and area, or check an existing report number' }
+
 sub open311_munge_update_params {
     my ($self, $params, $comment, $body) = @_;
 
