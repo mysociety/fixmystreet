22c58a50c49ebfc5776bb4f766ecc142ee7fab37
M Somerville <matthew-github@dracos.co.uk>
2021-01-11 15:58:00 +0000

    [Central Beds] Add trees layer.

--- web/cobrands/centralbedfordshire/assets.js
+++ web/cobrands/centralbedfordshire/assets.js
@@ -4,10 +4,10 @@ if (!fixmystreet.maps) {
     return;
 }
 
-
+var domain = fixmystreet.staging ? "https://tilma.staging.mysociety.org" : "https://tilma.mysociety.org";
 var defaults = {
     http_options: {
-        url: fixmystreet.staging ? "https://tilma.staging.mysociety.org/mapserver/centralbeds" : "https://tilma.mysociety.org/mapserver/centralbeds",
+        url: domain + "/mapserver/centralbeds",
         params: {
             SERVICE: "WFS",
             VERSION: "1.1.0",
@@ -15,12 +15,22 @@ var defaults = {
             SRSNAME: "urn:ogc:def:crs:EPSG::3857"
         }
     },
+    asset_type: 'spot',
     max_resolution: 9.554628534317017,
     geometryName: 'msGeometry',
     srsName: "EPSG:3857",
     body: "Central Bedfordshire Council",
     strategy_class: OpenLayers.Strategy.FixMyStreet
 };
+var proxy_defaults = $.extend(true, {}, defaults, {
+    http_options: {
+        url: domain + "/proxy/cbc/wfs",
+        params: {
+            SRSNAME: "urn:ogc:def:crs:EPSG::27700"
+        }
+    },
+    srsName: "EPSG:27700"
+});
 
 var centralbeds_types = [
     "CBC",
@@ -64,22 +74,21 @@ var streetlight_stylemap = new OpenLayers.StyleMap({
     'default': fixmystreet.assets.style_default,
     'hover': fixmystreet.assets.style_default_hover,
     'select': fixmystreet.assets.construct_named_select_style("${lighting_c}")
-  });
+});
 
-  var labeled_defaults = $.extend(true, {}, defaults, {
-      select_action: true,
-      stylemap: streetlight_stylemap,
-      feature_code: 'lighting_c',
-      asset_type: 'spot',
-      asset_id_field: 'asset',
-      attributes: {
-          asset: 'asset'
-      },
-      actions: {
-          asset_found: fixmystreet.assets.named_select_action_found,
-          asset_not_found: fixmystreet.assets.named_select_action_not_found
-      }
-  });
+var labeled_defaults = $.extend(true, {}, defaults, {
+    select_action: true,
+    stylemap: streetlight_stylemap,
+    feature_code: 'lighting_c',
+    asset_id_field: 'asset',
+    attributes: {
+        asset: 'asset'
+    },
+    actions: {
+        asset_found: fixmystreet.assets.named_select_action_found,
+        asset_not_found: fixmystreet.assets.named_select_action_not_found
+    }
+});
 
 fixmystreet.assets.add(labeled_defaults, {
     http_options: {
@@ -101,6 +110,22 @@ fixmystreet.assets.add(defaults, {
     max_resolution: 1.194328566789627,
     asset_category: ["Surface cover", "Drains/Ditches Blocked"],
     asset_item: 'drain or manhole'
+});
+
+fixmystreet.assets.add(proxy_defaults, {
+    http_options: {
+        params: {
+            propertyName: 'id,msGeometry',
+            TYPENAME: "trees"
+        }
+    },
+    asset_id_field: 'id',
+    attributes: {
+        UnitID: 'id'
+    },
+    max_resolution: 1.194328566789627,
+    asset_category: ["Trees"],
+    asset_item: 'tree'
 });
 
 
