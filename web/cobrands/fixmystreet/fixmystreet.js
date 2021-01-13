265fc2f2ef10533fde6f89bb62782a40dd1e0759
Struan Donald <struan@exo.org.uk>
2021-01-13 17:41:53 +0000

    screen to locate incident on a map

--- web/cobrands/fixmystreet/fixmystreet.js
+++ web/cobrands/fixmystreet/fixmystreet.js
@@ -1408,7 +1408,7 @@ fixmystreet.update_councils_text = function(data) {
 fixmystreet.update_pin = function(lonlat, savePushState) {
     var lonlats = fixmystreet.maps.update_pin(lonlat);
 
-    if ($('body').hasClass('noise')) {
+    if ($('body').hasClass('noise') || $('body').hasClass('claims')) {
         // Do nothing for noise map page
         return;
     }
@@ -1834,7 +1834,7 @@ $(function() {
     // pages stops working (because the replaceState turns the POST into a
     // GET), e.g. clicking back in a multi-page form reloads the page and
     // takes you back to the start, so avoid that on the noise flow.
-    if ($('body').hasClass('noise')) {
+    if ($('body').hasClass('noise') || $('body').hasClass('claims')) {
         return;
     }
 
