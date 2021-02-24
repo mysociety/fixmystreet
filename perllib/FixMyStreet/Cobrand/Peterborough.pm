670943206051de0959aae7942d6d3536ece189e1
Dave Arter <davea@mysociety.org>
2021-02-24 14:43:36 +0000

    [Peterborough] Add iCal future collections support

--- perllib/FixMyStreet/Cobrand/Peterborough.pm
+++ perllib/FixMyStreet/Cobrand/Peterborough.pm
@@ -409,6 +409,22 @@ sub bin_services_for_address {
     return \@out;
 }
 
+sub bin_future_collections {
+    my $self = shift;
+
+    my $bartec = $self->feature('bartec');
+    $bartec = Integrations::Bartec->new(%$bartec);
+
+    my $jobs = $bartec->Jobs_FeatureScheduleDates_Get($self->{c}->stash->{property}{uprn});
+
+    my $events = [];
+    foreach (@$jobs) {
+        my $dt = construct_bin_date($_->{NextDate});
+        push @$events, { date => $dt, desc => '', summary => $_->{JobName} };
+    }
+    return $events;
+}
+
 
 sub waste_munge_request_data {
     my ($self, $id, $data) = @_;
