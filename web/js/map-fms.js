19573f87f6db07684d2a50f5287dcb0baa5436b5
M Somerville <matthew-github@dracos.co.uk>
2021-02-26 19:56:22 +0000

    Rename GB bounds function.

--- web/js/map-fms.js
+++ web/js/map-fms.js
@@ -1,20 +1,20 @@
 fixmystreet.maps.tile_base = '//{S}tilma.mysociety.org/oml';
 
 OpenLayers.Layer.BingUK = OpenLayers.Class(OpenLayers.Layer.Bing, {
-    uk_bounds: [
+    gb_bounds: [
         new OpenLayers.Bounds(-6.6, 49.8, 1.102680, 51),
         new OpenLayers.Bounds(-5.4, 51, 2.28, 54.94),
         new OpenLayers.Bounds(-5.85, 54.94, -1.15, 55.33),
         new OpenLayers.Bounds(-9.35, 55.33, -0.7, 60.98)
     ],
 
-    in_uk: function(c) {
+    in_gb: function(c) {
         c = c.clone();
         c.transform(
             fixmystreet.map.getProjectionObject(),
             new OpenLayers.Projection("EPSG:4326")
         );
-        if ( this.uk_bounds[0].contains(c.lon, c.lat) || this.uk_bounds[1].contains(c.lon, c.lat) || this.uk_bounds[2].contains(c.lon, c.lat) || this.uk_bounds[3].contains(c.lon, c.lat) ) {
+        if ( this.gb_bounds[0].contains(c.lon, c.lat) || this.gb_bounds[1].contains(c.lon, c.lat) || this.gb_bounds[2].contains(c.lon, c.lat) || this.gb_bounds[3].contains(c.lon, c.lat) ) {
             return true;
         }
         return false;
@@ -30,13 +30,13 @@ OpenLayers.Layer.BingUK = OpenLayers.Class(OpenLayers.Layer.Bing, {
         var copyrights;
         var logo = '';
         var c = this.map.getCenter();
-        var in_uk = c ? this.in_uk(c) : true;
+        var in_gb = c ? this.in_gb(c) : true;
         var year = (new Date()).getFullYear();
-        if (z >= 16 && in_uk) {
+        if (z >= 16 && in_gb) {
             copyrights = 'Contains Highways England and Ordnance Survey data &copy; Crown copyright and database right ' + year;
         } else {
             logo = '<a href="https://www.bing.com/maps/"><img border=0 src="//dev.virtualearth.net/Branding/logo_powered_by.png"></a>';
-            if (in_uk) {
+            if (in_gb) {
                 copyrights = '&copy; ' + year + ' <a href="https://www.bing.com/maps/">Microsoft</a>, HERE, Highways England, Ordnance Survey';
             } else {
                 copyrights = '&copy; ' + year + ' <a href="https://www.bing.com/maps/">Microsoft</a>, HERE, Ordnance Survey';
@@ -49,13 +49,13 @@ OpenLayers.Layer.BingUK = OpenLayers.Class(OpenLayers.Layer.Bing, {
 
     get_urls: function(bounds, z) {
         var urls = [], i;
-        var in_uk = this.in_uk(bounds.getCenterLonLat());
-        if (z >= 16 && in_uk) {
+        var in_gb = this.in_gb(bounds.getCenterLonLat());
+        if (z >= 16 && in_gb) {
             urls = [];
             for (i=0; i< this.tile_prefix.length; i++) {
                 urls.push( fixmystreet.maps.tile_base.replace('{S}', this.tile_prefix[i]) + "/${z}/${x}/${y}.png" );
             }
-        } else if (z > 11 && in_uk) {
+        } else if (z > 11 && in_gb) {
             var type = 'g=8702&lbl=l1&productSet=mmOS&key=' + fixmystreet.bing_key;
             var tile_base = "//ecn.t{S}.tiles.virtualearth.net/tiles/r${id}?" + type;
             for (i=0; i<4; i++) {
