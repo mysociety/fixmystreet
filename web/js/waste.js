62d090e273d123a7f2c302479d4c1e698a57f93a
Struan Donald <struan@exo.org.uk>
2021-04-05 17:24:44 +0100

    [Bromley] update subscription costs as values change
    Add some javascript to make the payments costs update as you change the
    number of bins when creating or modifying a subscription.

--- web/js/waste.js
+++ web/js/waste.js
@@ -5,4 +5,34 @@ $(function() {
         $btn.prop("disabled", true);
         $btn.parents('.govuk-form-group').addClass('loading');
     });
+
+    var cost = $('#per_bin_cost').val();
+    function bin_cost() {
+      var total_bins = parseInt($('#current_bins').val()) + parseInt($('#new_bins').val());
+      var total_cost = ( total_bins * cost ) / 100;
+      $('#cost_pa').text(total_cost.toFixed(2));
+      $('#cost_now').text(total_cost.toFixed(2));
+    }
+    $('#new_bins').on('change', bin_cost);
+    $('#current_bins').on('change', bin_cost);
+
+    function modify_cost() {
+      var total_bins = parseInt($('#bin_number').val());
+      var existing_bins = parseInt($('#existing_bins').val());
+      var new_bins = total_bins - existing_bins;
+      var pro_rata_cost = 0;
+      var total_cost = ( total_bins * cost ) / 100;
+      var new_bin_text = new_bins == 1 ? 'bin' : 'bins';
+      $('#new_bin_text').text(new_bin_text);
+
+      if ( new_bins > 0) {
+          $('#new_bin_count').text(new_bins);
+          pro_rata_cost = ( new_bins * parseInt($('#pro_rata_bin_cost').val()) ) / 100;
+      } else {
+          $('#new_bin_count').text(0);
+      }
+      $('#cost_per_year').text(total_cost.toFixed(2));
+      $('#pro_rata_cost').text(pro_rata_cost.toFixed(2));
+    }
+    $('#bin_number').on('change', modify_cost);
 });
