968893cc7a3cbb7d48097404a6775ce1d706f066
Struan Donald <struan@exo.org.uk>
2021-04-09 17:31:58 +0100

    [Bromley] style garden waste renewal pages
    plus add in JS to auto-update costs

--- web/js/waste.js
+++ web/js/waste.js
@@ -8,7 +8,7 @@ $(function() {
 
     var cost = $('#per_bin_cost').val();
     function bin_cost() {
-      var total_bins = parseInt($('#current_bins').val()) + parseInt($('#new_bins').val());
+      var total_bins = parseInt($('#current_bins').val() || 0) + parseInt($('#new_bins').val() || 0);
       var total_cost = ( total_bins * cost ) / 100;
       $('#cost_pa').text(total_cost.toFixed(2));
       $('#cost_now').text(total_cost.toFixed(2));
@@ -17,8 +17,8 @@ $(function() {
     $('#current_bins').on('change', bin_cost);
 
     function modify_cost() {
-      var total_bins = parseInt($('#bin_number').val());
-      var existing_bins = parseInt($('#existing_bins').val());
+      var total_bins = parseInt($('#bin_number').val() || 0);
+      var existing_bins = parseInt($('#existing_bins').val() || 0);
       var new_bins = total_bins - existing_bins;
       var pro_rata_cost = 0;
       var total_cost = ( total_bins * cost ) / 100;
