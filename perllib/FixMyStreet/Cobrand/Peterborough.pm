a0f3a685653595722e0cbe38763ccc84bd6a7194
Dave Arter <davea@mysociety.org>
2021-05-21 15:14:18 +0100

    [Peterborough] Skip empty page on bin enquiry form
    For https://3.basecamp.com/4020879/buckets/19439943/todos/3568720742

--- perllib/FixMyStreet/Cobrand/Peterborough.pm
+++ perllib/FixMyStreet/Cobrand/Peterborough.pm
@@ -573,6 +573,7 @@ sub waste_munge_enquiry_data {
 
 
     my $bin = $container_ids{$data->{service_id}};
+    $data->{category} = $self->{c}->get_param('category');
     $data->{title} = "$bin $data->{category}";
     $data->{detail} = $self->{c}->stash->{property}->{address};
 }
