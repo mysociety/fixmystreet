70a3fb83a046a6c53b020ec62f5f2850f97f339f
M Somerville <matthew-github@dracos.co.uk>
2021-06-18 14:24:38 +0100

    [Bucks] no photo hashes in description.

--- perllib/FixMyStreet/App/Form/Claims.pm
+++ perllib/FixMyStreet/App/Form/Claims.pm
@@ -1098,9 +1098,12 @@ sub format_for_display {
         }
     } elsif ( $field->{type} eq 'FileIdUpload' ) {
         if ( ref $value eq 'HASH' ) {
-           return join( ',', @{ $value->{filenames} } );
+           return join( ', ', @{ $value->{filenames} } );
         }
         return "";
+    } elsif ( $field->{type} eq 'FileIdPhoto' ) {
+        my $num = split /,/, $value;
+        return sprintf(mySociety::Locale::nget("%d photo", "%d photos", $num), $num);
     }
 
     return $value;
