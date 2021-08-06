17c5925f7bac22e86d8a141de9d4727ec5c410c2
Ludo Tolhurst-Cleaver <ludo@mysociety.org>
2021-08-06 11:59:19 +0100

    [Oxfordshire] New Defect Types
    New defect category, types and detail added, along with a
    requirement for more complex behaviour to correctly match
    each detail with the correct type and category.
    
    Fixes mysociety/societyworks/issues/2482
    and https://mysocietysupport.freshdesk.com/a/tickets/1275

--- .cypress/cypress/integration/oxfordshire.js
+++ .cypress/cypress/integration/oxfordshire.js
@@ -28,6 +28,178 @@ describe("Oxfordshire cobrand", function() {
     cy.get('#raise_defect_yes').click({force: true});
     cy.get('#defect_item_category').should('be.visible');
 
+    // test defect dropdowns' interaction
+    cy.get('#defect_item_category').as('dicat');
+    cy.get('#defect_item_type').as('ditype');
+    cy.get('#defect_item_detail').as('didetail');
+
+    // all the --s
+    cy.get('@dicat').select('-- Pick a category --');
+    cy.get('@ditype').should('have.value', '');
+    cy.get('@didetail').should('have.value', '');
+
+    function testDefectDropdowns(catval, righttypes, wrongtypes) {
+        // select cat
+        cy.get('@dicat').select(catval);
+
+        // check types
+        righttypes.forEach( function(rtype) {
+            cy.get('@ditype').contains(rtype.typename);
+
+            // select type
+            cy.get('@ditype').select(rtype.typename);
+
+            // check details
+            rtype.right_details.forEach( function(rdetail) {
+                cy.get('@didetail').contains(rdetail);
+            });
+            rtype.wrong_details.forEach( function(wdetail) {
+                cy.get('@didetail').should('not.have.text', wdetail);
+            });
+        });
+        wrongtypes.forEach( function(wtype) {
+            cy.get('@ditype').should('not.have.text', wtype);
+        });
+    }
+
+    var sweepfill = {
+        typename: 'Sweep & Fill',
+        right_details: [ 'Pothole Sweep & Fill 0-1m²', 'Pothole Cluster Sweep & Fill 1-2m²' ],
+        wrong_details: [
+             '0-1m²',
+             '1-2m²',
+             'Pothole Cluster',
+             '1 kerb unit or I liner length',
+             'Greater than 1 kerb unit or I liner length',
+             'Blockage raised as a defect'
+        ]
+    };
+    var pothole = {
+        typename: 'Pothole (Permanent)',
+        right_details: [ '0-1m²', '1-2m²' ],
+        wrong_details: [
+            'Pothole Sweep & Fill 0-1m²',
+            'Pothole Cluster Sweep & Fill 1-2m²',
+            'Pothole Cluster',
+            '1 kerb unit or I liner length',
+            'Greater than 1 kerb unit or I liner length',
+            'Blockage raised as a defect'
+        ]
+    };
+    var pothole_cluster = {
+        typename: 'Pothole Cluster (Permanent)',
+        right_details: [ 'Pothole Cluster' ],
+        wrong_details: [
+            '0-1m²',
+            '1-2m²',
+            'Pothole Sweep & Fill 0-1m²',
+            'Pothole Cluster Sweep & Fill 1-2m²',
+            '1 kerb unit or I liner length',
+            'Greater than 1 kerb unit or I liner length',
+            'Blockage raised as a defect'
+        ]
+    };
+    var damaged = {
+        typename: 'Damaged',
+        right_details: [
+            '1 kerb unit or I liner length',
+            'Greater than 1 kerb unit or I liner length',
+        ],
+        wrong_details: [
+            '0-1m²',
+            '1-2m²',
+            'Pothole Sweep & Fill 0-1m²',
+            'Pothole Cluster Sweep & Fill 1-2m²',
+            'Pothole Cluster',
+            'Blockage raised as a defect'
+        ]
+    };
+    var loose = {
+        typename: 'Loose',
+        right_details: [
+            '1 kerb unit or I liner length',
+            'Greater than 1 kerb unit or I liner length'
+        ],
+        wrong_details: [
+            '0-1m²',
+            '1-2m²',
+            'Pothole Sweep & Fill 0-1m²',
+            'Pothole Cluster Sweep & Fill 1-2m²',
+            'Pothole Cluster',
+            'Blockage raised as a defect'
+        ]
+    };
+    var misaligned = {
+        typename: 'Misaligned Single Units or Uneven Run of Units',
+        right_details: [
+            '1 kerb unit or I liner length',
+            'Greater than 1 kerb unit or I liner length'
+        ],
+        wrong_details: [
+            '0-1m²',
+            '1-2m²',
+            'Pothole Sweep & Fill 0-1m²',
+            'Pothole Cluster Sweep & Fill 1-2m²',
+            'Pothole Cluster',
+            'Blockage raised as a defect'
+        ]
+    };
+    var missing = {
+        typename: 'Missing',
+        right_details: [
+            '1 kerb unit or I liner length',
+            'Greater than 1 kerb unit or I liner length'
+        ],
+        wrong_details: [
+            '0-1m²',
+            '1-2m²',
+            'Pothole Sweep & Fill 0-1m²',
+            'Pothole Cluster Sweep & Fill 1-2m²',
+            'Pothole Cluster',
+            'Blockage raised as a defect'
+        ]
+    };
+    var blockage = {
+        typename: 'Blockage',
+        right_details: [
+            '1 kerb unit or I liner length',
+            'Greater than 1 kerb unit or I liner length'
+        ],
+        wrong_details: [
+            '0-1m²',
+            '1-2m²',
+            'Pothole Sweep & Fill 0-1m²',
+            'Pothole Cluster Sweep & Fill 1-2m²',
+            'Pothole Cluster',
+            'Blockage raised as a defect'
+        ]
+    };
+
+    (function() {
+        var right_types = [ sweepfill, pothole, pothole_cluster ];
+        var wrong_types = [ damaged, loose, misaligned, missing, blockage ];
+
+        testDefectDropdowns('Minor Carriageway', right_types, wrong_types);
+        testDefectDropdowns('Footway/ Cycleway', right_types, wrong_types);
+        cy.log('Minor Carriageway and Footway/ Cycleway category types & details are correct');
+    }());
+
+    (function() {
+        var right_types = [ damaged, loose, misaligned, missing ];
+        var wrong_types = [ sweepfill, pothole, pothole_cluster, blockage ];
+
+        testDefectDropdowns('Kerbing', right_types, wrong_types);
+        cy.log('Kerbing category types & details are correct');
+    }());
+
+    (function() {
+        var right_types = [ blockage ];
+        var wrong_types = [ sweepfill, pothole, pothole_cluster, damaged, loose, misaligned, missing ];
+
+        testDefectDropdowns('Drainage', right_types, wrong_types);
+        cy.log('Drainage category types & details are correct');
+    }());
+
     cy.get('#report_inspect_form select[name=state]').select('No further action');
     cy.get('#js-inspect-action-scheduled').should('not.be.visible');
     cy.get('#raise_defect_yes').should('not.have.attr', 'required');
