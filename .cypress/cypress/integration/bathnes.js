689d7e955c8fcec9bc66339e65b4dfb6fe5068f1
M Somerville <matthew-github@dracos.co.uk>
2021-08-17 15:36:22 +0100

    Stop error if category picked before data arrives.

--- .cypress/cypress/integration/bathnes.js
+++ .cypress/cypress/integration/bathnes.js
@@ -41,8 +41,10 @@ it('uses the Curo Group housing layer correctly', function() {
 });
 
 it('handles code names with spaces without error', function() {
+    cy.server();
+    cy.route('/report/new/ajax*').as('report-ajax');
     cy.visit('http://bathnes.localhost:3001/report/new?longitude=-2.359276&latitude=51.379009');
-    cy.contains('Bath & North East Somerset Council');
+    cy.wait('@report-ajax');
     cy.get('input[value="Abandoned vehicles"]').click();
     cy.get('input[value="Blocked drain"]').click();
     cy.get('input[value="Abandoned vehicles"]').click();
