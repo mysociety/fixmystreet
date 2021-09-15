09f76916c0948379d7dac0c84a43e0062874596f
Ludo Tolhurst-Cleaver <ludo@mysociety.org>
2021-09-15 11:26:38 +0100

    Revert "[Fixup] switch to using user->remove_from_user_planned_reports"
    Need to use non-standard way of removing planned reports so as to
    return the shortlisted problem object for code that handles
    setting already unassigned problems as unassigned.
    
    This reverts commit d22f0f3e1541fd197b7da39f01542a07753f5176.

--- perllib/FixMyStreet/App/Controller/Report.pm
+++ perllib/FixMyStreet/App/Controller/Report.pm
@@ -525,7 +525,7 @@ sub inspect : Private {
         my $assigned = ($c->get_param('assignment'));
         if ($assigned && $assigned eq 'unassigned') {
             # take off shortlist...
-            my $shortlisted = $problem->user->remove_from_planned_reports($problem);
+            my $shortlisted = $problem->user_planned_reports->search({ removed => undef })->first;
             # ...(catching already unassigned)
             if ($shortlisted) {
                 $shortlisted->removed( \'current_timestamp' );
