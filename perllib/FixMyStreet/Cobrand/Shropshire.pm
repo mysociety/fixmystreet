65bd817dd89e03e67329d1dc21de838a60abde2a
Moray Jones <moray@mysociety.org>
2021-10-06 16:01:07 +0100

    [Shropshire] Initial styles and templates.

---
+++ perllib/FixMyStreet/Cobrand/Shropshire.pm
@@ -0,0 +1,36 @@
+package FixMyStreet::Cobrand::Shropshire;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+use Moo;
+with 'FixMyStreet::Roles::ConfirmOpen311';
+with 'FixMyStreet::Roles::ConfirmValidation';
+
+sub council_area_id { return 2238; } # https://mapit.mysociety.org/area/2238.html
+sub council_area { return 'Shropshire'; }
+sub council_name { return 'Shropshire Council'; }
+sub council_url { return 'shropshire'; }
+
+sub admin_user_domain {
+    'shropshire.gov.uk'
+}
+
+sub default_map_zoom { 6 }
+
+sub send_questionnaires { 0 }
+
+sub disambiguate_location {
+    my $self    = shift;
+    my $string  = shift;
+
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        centre => '52.6354074681479,-2.73414274873688',
+        span   => '0.692130766645555,1.00264228991404',
+        bounds => [ 52.3062638566609, -3.23554076944319, 52.9983946233065, -2.23289847952914 ],
+    };
+}
+
+1;
