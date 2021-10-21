3ed54f593d31f81755bea1dc9101c97518562c8a
Ludo Tolhurst-Cleaver <ludo@mysociety.org>
2021-10-21 16:59:49 +0100

    [Merton] WIP: USRN and service attributes
    - Updated Merton cobrand class with lookup_site_code_config()
    - new assets.js for USRN layer
    
    fixes https://github.com/mysociety/societyworks/issues/2673

--- perllib/FixMyStreet/Cobrand/Merton.pm
+++ perllib/FixMyStreet/Cobrand/Merton.pm
@@ -59,4 +59,12 @@ sub open311_extra_data_include {
     return $open311_only;
 }
 
+sub lookup_site_code_config { {
+    buffer => 50, # metres
+    url => "https://tilma.mysociety.org/mapserver/merton",
+    typename => 'openusrn',
+    property => "usrn",
+    accept_feature => sub { 1 },
+} }
+
 1;
---
+++ web/cobrands/merton/assets.js
@@ -0,0 +1,69 @@
+(function(){
+
+if (!fixmystreet.maps) {
+    return;
+}
+
+var format = new OpenLayers.Format.QueryStringFilter();
+OpenLayers.Protocol.Merton = OpenLayers.Class(OpenLayers.Protocol.HTTP, {
+    filterToParams: function(filter, params) {
+        params = format.write(filter, params);
+        params.geometry = params.bbox;
+        delete params.bbox;
+        return params;
+    },
+    CLASS_NAME: "OpenLayers.Protocol.Merton"
+});
+
+var defaults = {
+    max_resolution: 4.777314267158508,
+    srsName: "EPSG:27700",
+    body: "Merton Council",
+    strategy_class: OpenLayers.Strategy.FixMyStreet
+};
+
+var tilma_defaults = $.extend(true, {}, defaults, {
+    http_options: {
+        url: fixmystreet.staging ? "https://tilma.staging.mysociety.org/mapserver/merton" : "https://tilma.mysociety.org/mapserver/merton",
+        params: {
+            SERVICE: "WFS",
+            VERSION: "1.1.0",
+            REQUEST: "GetFeature",
+            SRSNAME: "urn:ogc:def:crs:EPSG::27700"
+        }
+    },
+    geometryName: 'msGeometry'
+});
+
+fixmystreet.assets.add(tilma_defaults, {
+    http_options: {
+        params: {
+            TYPENAME: "USRN"
+        }
+    },
+    nearest_radius: 2,
+    stylemap: fixmystreet.assets.stylemap_invisible,
+    non_interactive: true,
+    always_visible: true,
+    usrn: {
+        attribute: 'Usrn',
+        field: 'site_code'
+    },
+    name: "USRN"
+});
+
+// This is required so that the found/not found actions are fired on category
+// select and pin move rather than just on asset select/not select.
+OpenLayers.Layer.MertonVectorAsset = OpenLayers.Class(OpenLayers.Layer.VectorAsset, {
+    initialize: function(name, options) {
+        OpenLayers.Layer.VectorAsset.prototype.initialize.apply(this, arguments);
+        $(fixmystreet).on('maps:update_pin', this.checkSelected.bind(this));
+        $(fixmystreet).on('report_new:category_change', this.checkSelected.bind(this));
+    },
+
+    CLASS_NAME: 'OpenLayers.Layer.MertonVectorAsset'
+});
+
+var url_base = 'https://tilma.mysociety.org/resource-proxy/proxy.php?https://merton.assets/';
+
+})();
