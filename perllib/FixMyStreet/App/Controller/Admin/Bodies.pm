3fc7923dae9b8deeea641bce46b9d40f83faca44
Chris Mytton <chrism@mysociety.org>
2022-01-31 10:57:16 +0000

    Add prefer_if_multiple setting to contacts
    This is for the situation where multiple bodies covering the same area
    have a contact with the same name. Checking this box indicates that if
    multiple contacts are found with the same name then the one(s) with this
    box checked should be preferred. If no contacts are found with this flag
    then fall back to the default behaviour of sending to all matching
    contacts.

--- perllib/FixMyStreet/App/Controller/Admin/Bodies.pm
+++ perllib/FixMyStreet/App/Controller/Admin/Bodies.pm
@@ -276,7 +276,7 @@ sub update_contact : Private {
     $contact->send_method( $c->get_param('send_method') );
 
     # Set flags in extra to the appropriate values
-    foreach (qw(photo_required open311_protect updates_disallowed reopening_disallowed assigned_users_only anonymous_allowed)) {
+    foreach (qw(photo_required open311_protect updates_disallowed reopening_disallowed assigned_users_only anonymous_allowed prefer_if_multiple)) {
         if ( $c->get_param($_) ) {
             $contact->set_extra_metadata( $_ => 1 );
         } else {
