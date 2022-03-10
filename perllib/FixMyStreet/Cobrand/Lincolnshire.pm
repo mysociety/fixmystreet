95c38dcea5f874789bc673419843d2fb7d5910f0
M Somerville <matthew-github@dracos.co.uk>
2022-03-10 21:36:28 +0000

    [Lincolnshire] Update district categories list

--- perllib/FixMyStreet/Cobrand/Lincolnshire.pm
+++ perllib/FixMyStreet/Cobrand/Lincolnshire.pm
@@ -61,8 +61,9 @@ sub categories_restriction {
 
         # District categories:
         'me.category' => { -in => [
+            'Litter',
             'Street nameplates',
-            'Bench/cycle rack/litter bin/planter',
+            'Bench', 'Cycle rack', 'Litter bin', 'Planter',
         ] },
     ] } );
 }
