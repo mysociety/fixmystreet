684e205b839535e4c0d7a2eff11fabd0e46842c5
Moray Jones <moray@mysociety.org>
2022-03-22 11:11:55 +0000

    [Thamesmead] Add Thamesmead cobrand file
    Adds preliminary Cobrand file

---
+++ perllib/FixMyStreet/Cobrand/Thamesmead.pm
@@ -0,0 +1,51 @@
+package FixMyStreet::Cobrand::Thamesmead;
+use parent 'FixMyStreet::Cobrand::UK';
+
+use strict;
+use warnings;
+
+sub council_area { return 'Thamesmead'; }
+sub council_name { return 'Thamesmead'; }
+sub council_url { return 'thamesmead'; }
+
+sub admin_user_domain {
+    'thamesmeadnow.org.uk'
+}
+
+sub updates_disallowed {
+    my $self = shift;
+    my $problem = shift;
+    my $c = $self->{c};
+
+    my $staff = $c->user_exists && $c->user->from_body && $c->user->from_body->name eq $self->council_name;
+    my $superuser = $c->user_exists && $c->user->is_superuser;
+    my $reporter = $c->user_exists && $c->user->id == $problem->user->id;
+    my $closed_to_updates = $self->SUPER::updates_disallowed($problem);
+
+    if (($staff || $superuser || $reporter) && !$closed_to_updates) {
+        return;
+    } else {
+        return 1;
+    }
+}
+
+sub reopening_disallowed {
+    my $self = shift;
+    my $problem = shift;
+    my $c = $self->{c};
+
+    my $staff = $c->user_exists && $c->user->from_body && $c->user->from_body->name eq $self->council_name;
+    my $superuser = $c->user_exists && $c->user->is_superuser;
+    my $reporter = $c->user_exists && $c->user->id == $problem->user->id;
+    my $reopening_disallowed = $self->SUPER::reopening_disallowed($problem);
+
+    if (($staff || $superuser || $reporter) && !$reopening_disallowed) {
+        return;
+    } else {
+        return 1;
+    }
+}
+
+sub default_map_zoom { 6 }
+
+1;
