7686176b863a043c36d6cfffa5b693fdd1f7e969
Moray Jones <moray@mysociety.org>
2022-04-27 16:54:35 +0100

    [Thamesmead] Add admin permissions
    Adds admin permission based on UK Councils
    
    Adds body based on Highways England
    
    Adds users_restrictions imported from UK Councils

--- perllib/FixMyStreet/Cobrand/Thamesmead.pm
+++ perllib/FixMyStreet/Cobrand/Thamesmead.pm
@@ -8,9 +8,7 @@ sub council_area { return 'Thamesmead'; }
 sub council_name { return 'Thamesmead'; }
 sub council_url { return 'thamesmead'; }
 
-sub admin_user_domain {
-    'thamesmeadnow.org.uk'
-}
+sub admin_user_domain { ( 'thamesmeadnow.org.uk', 'peabody.org.uk' ) }
 
 sub updates_disallowed {
     my $self = shift;
@@ -45,6 +43,26 @@ sub reopening_disallowed {
         return 1;
     }
 }
+
+sub body {
+    my $self = shift;
+    my $body = FixMyStreet::DB->resultset('Body')->search({ name => 'Thamesmead' })->first;
+    return $body;
+}
+
+sub admin_allow_user {
+    my ( $self, $user ) = @_;
+    return 1 if $user->is_superuser;
+    return undef unless defined $user->from_body;
+    return $user->from_body->name eq 'Thamesmead';
+}
+
+sub cut_off_date { '2022-04-25' }
+sub problems_restriction { FixMyStreet::Cobrand::UKCouncils::problems_restriction($_[0], $_[1]) }
+sub problems_on_map_restriction { $_[0]->problems_restriction($_[1]) }
+sub problems_sql_restriction { FixMyStreet::Cobrand::UKCouncils::problems_sql_restriction($_[0], $_[1]) }
+sub users_restriction { FixMyStreet::Cobrand::UKCouncils::users_restriction($_[0], $_[1]) }
+sub updates_restriction { FixMyStreet::Cobrand::UKCouncils::updates_restriction($_[0], $_[1]) }
 
 sub default_map_zoom { 6 }
 
