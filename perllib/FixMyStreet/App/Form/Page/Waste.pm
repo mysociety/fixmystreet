b5108cf856533fb894ca24bc9eb4864fa47f47cf
M Somerville <matthew-github@dracos.co.uk>
2022-05-20 17:13:15 +0100

    [Kingston] Different garden service name.

--- perllib/FixMyStreet/App/Form/Page/Waste.pm
+++ perllib/FixMyStreet/App/Form/Page/Waste.pm
@@ -3,7 +3,15 @@ use Moose;
 extends 'FixMyStreet::App::Form::Page::Simple';
 
 # Title to use for this page
-has title => ( is => 'ro', isa => 'Str' );
+has title => ( is => 'ro', 'isa' => 'Str', lazy => 1, builder => '_build_title' );
+# So we can insert cobrand specific service name
+has title_ggw => ( is => 'ro', isa => 'Str' );
+
+sub _build_title {
+    my $self = shift;
+    my $name = $self->form->{c}->cobrand->garden_service_name;
+    return sprintf($self->title_ggw, $name);
+}
 
 # Special template to use in preference to the default
 has template => ( is => 'ro', isa => 'Str' );
