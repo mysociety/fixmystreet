22ed7ee78f8112403333f57d3640f49b16313702
Moray Jones <moray@mysociety.org>
2022-06-06 18:48:34 +0100

    [Thamesmead] Text requesting full user name should be from Peabody
    When filling in the form to make a report, a user is asked for their full name if they only enter one name.
    
    This text usually says that councils require this text. We want it to say that Peabody requires this.

---
+++ web/cobrands/thamesmead/js.js
@@ -0,0 +1,4 @@
+(function(){
+translation_strings.name.validName = 'Please enter your full name, Peabody needs this information – if you do not wish your name to be shown on the site, untick the box below';
+})();
+
