313f63ebfb2ab5e8e4bc34a780e623a711e8511b
Chris Mytton <chrism@mysociety.org>
2022-06-15 15:27:53 +0100

    [Buckinghamshire] Make it clear when reports are sent to the parish
    Rather than the text during the reporting process saying "This report
    will be sent to Bucks and the parish" we instead want it to just say the
    parish name. This is because parish categories have the
    prefer_if_multiple flag, so the reports will only go to those contacts.

--- web/cobrands/buckinghamshire/js.js
+++ web/cobrands/buckinghamshire/js.js
@@ -4,27 +4,46 @@
       return;
   }
 
+  function getBodies() {
+    var bucks, parish;
+    $.each(fixmystreet.bodies, function(i, body) {
+      if (body === 'Buckinghamshire Council') {
+        bucks = body;
+      } else {
+        parish = body;
+      }
+    });
+
+    return {
+      bucks: bucks,
+      parish: parish
+    };
+  }
+
   $(function() {
+
     $(document).on('change', '#form_speed_limit_greater_than_30', function(e) {
-      var bucks, parish;
-      $.each(fixmystreet.bodies, function(i, body) {
-        if (body === 'Buckinghamshire Council') {
-          bucks = body;
-        } else {
-          parish = body;
-        }
-      });
-      if (!bucks || !parish) {
+      var bodies = getBodies();
+      if (!bodies.bucks || !bodies.parish) {
         return;
       }
 
       if ($(this).val() === 'no') {
         // Report for the parish
-        fixmystreet.update_public_councils_text($('#js-councils_text').html(), [parish]);
+        fixmystreet.update_public_councils_text($('#js-councils_text').html(), [bodies.parish]);
       } else {
         // Report for the council
-        fixmystreet.update_public_councils_text($('#js-councils_text').html(), [bucks]);
+        fixmystreet.update_public_councils_text($('#js-councils_text').html(), [bodies.bucks]);
       }
+    });
+
+    $(fixmystreet).on('report_new:category_change', function(e) {
+      var bodies = getBodies();
+      if (!bodies.parish) {
+        return;
+      }
+
+      fixmystreet.update_public_councils_text($('#js-councils_text').html(), [bodies.parish]);
     });
   });
 
