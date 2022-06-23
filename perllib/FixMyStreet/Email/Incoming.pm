1aa85ab99b6b2df4bfd47b46160fe575dd003ff4
M Somerville <matthew-github@dracos.co.uk>
2022-06-23 20:17:21 +0100

    Refactor handlemail script for testing.

---
+++ perllib/FixMyStreet/Email/Incoming.pm
@@ -0,0 +1,247 @@
+package FixMyStreet::Email::Incoming;
+
+use Moo;
+use Path::Tiny;
+use FixMyStreet;
+use FixMyStreet::Cobrand;
+use FixMyStreet::DB;
+use FixMyStreet::Email;
+use mySociety::HandleMail;
+use mySociety::SystemMisc qw(print_log);
+
+has cobrand => ( is => 'ro' );
+has bouncemgr => ( is => 'ro' );
+
+has data => ( is => 'lazy' );
+has lines => ( is => 'lazy' );
+has token => ( is => 'lazy' );
+has token_parts => ( is => 'lazy' );
+has type => ( is => 'lazy' );
+has object => ( is => 'lazy' );
+
+sub _build_data {
+    my %data = mySociety::HandleMail::get_message();
+    return \%data;
+}
+
+sub _build_lines { $_[0]->data->{lines} }
+
+sub process {
+    my $self = shift;
+
+    if ($self->data->{is_bounce_message}) {
+        if ($self->object) {
+            $self->handle_bounce_to_verp_address();
+        } else {
+            print_log('info', "bounce received for don't-care email");
+        }
+    } else {
+        # This is not a bounce message. If it's to a VERP address, pass it on to
+        # the message sender; otherwise send an auto-reply
+        if ($self->object) {
+            $self->handle_non_bounce_to_verp_address();
+        } else {
+            $self->handle_non_bounce_to_null_address();
+        }
+    }
+}
+
+sub _build_token {
+    my $self = shift;
+    my $m = $self->data->{message};
+
+    # If we have a special suffix header for the local part suffix, use that.
+    # This is set by our exim so we have access to it through the domain name
+    # forwarding and routers.
+    my $suffix = $m->head()->get("X-Delivered-Suffix");
+    if ($suffix) {
+        chomp $suffix;
+        return substr($suffix, 1);
+    }
+
+    # Otherwise, fall back to To header
+    my $a = mySociety::HandleMail::get_bounce_recipient($m);
+
+    my $token = mySociety::HandleMail::get_token($a,
+        'fms-', FixMyStreet->config('EMAIL_DOMAIN')
+    );
+    exit 0 unless $token; # Don't care unless we have a token
+
+    return $token;
+}
+
+sub _build_token_parts {
+    my $self = shift;
+
+    my $verp = $self->token !~ /DO-NOT-REPLY/i;
+    if (!$verp) {
+        return { verp => 0 };
+    }
+
+    my ($type, $id) = FixMyStreet::Email::check_verp_token($self->token);
+    exit 0 unless $type;
+
+    return { verp => 1, type => $type, id => $id };
+}
+
+sub _build_type { $_[0]->token_parts->{type} }
+
+sub _build_object {
+    my $self = shift;
+
+    my $token_parts = $self->token_parts;
+    return unless $token_parts->{verp};
+
+    my $rs;
+    if ($self->type eq 'report') {
+        $rs = FixMyStreet::DB->resultset('Problem');
+    } elsif ($self->type eq 'alert') {
+        $rs = FixMyStreet::DB->resultset('Alert');
+    }
+
+    my $id = $token_parts->{id};
+    my $object = $rs->find({ id => $id });
+    exit(0) unless $object;
+
+    return $object;
+}
+
+sub handle_permanent_bounce {
+    my $self = shift;
+    if ($self->type eq 'alert') {
+        print_log('info', "Received bounce for alert " . $self->object->id . ", unsubscribing");
+        $self->object->disable();
+    } elsif ($self->type eq 'report') {
+        print_log('info', "Received bounce for report " . $self->object->id . ", forwarding to support");
+        $self->forward_on_to($self->bouncemgr);
+    }
+}
+
+sub is_out_of_office {
+    my $self = shift;
+    my (%attributes) = @_;
+    return 1 if $attributes{problem} && $attributes{problem} == mySociety::HandleMail::ERR_OUT_OF_OFFICE;
+    my $head = $self->data->{message}->head();
+    return 1 if $head->get('X-Autoreply') || $head->get('X-Autorespond');
+    my $mc = $head->get('X-POST-MessageClass') || '';
+    return 1 if $mc eq '9; Autoresponder';
+    my $auto_submitted = $head->get("Auto-Submitted") || '';
+    return 1 if $auto_submitted && $auto_submitted !~ /no/;
+    my $precedence = $head->get("Precedence") || '';
+    return 1 if $precedence =~ /auto_reply/;
+    my $subject = $head->get("Subject");
+    return 1 if $subject =~ /Auto(matic|mated)?[ -_]?(reply|response|responder)|Thank[ _]you[ _]for[ _](your[ _]email|contacting)|Out of (the )?Office|away from the office|This office is closed until|^Re: (Problem Report|New updates)|^Auto: |^E-Mail Response$|^Message Received:|have received your email|Acknowledgement of your email|away from my desk|We got your email/i;
+    return 0;
+}
+
+sub handle_bounce_to_verp_address {
+    my $self = shift;
+    my %attributes = mySociety::HandleMail::parse_bounce($self->lines);
+    my $info = '';
+    if ($attributes{is_dsn}) {
+        # If permanent failure, but not mailbox full
+        return $self->handle_permanent_bounce() if $attributes{status} =~ /^5\./ && $attributes{status} ne '5.2.2';
+        $info = ", Status $attributes{status}";
+    } elsif ($attributes{problem}) {
+        my $err_type = mySociety::HandleMail::error_type($attributes{problem});
+        return $self->handle_permanent_bounce() if $err_type == mySociety::HandleMail::ERR_TYPE_PERMANENT;
+        $info = ", Bounce type $attributes{problem}";
+    }
+
+    # Check if the Subject looks like an auto-reply rather than a delivery bounce.
+    # If so, treat as if it were a normal email
+    my $type = $self->type;
+    if ($self->is_out_of_office(%attributes)) {
+        print_log('info', "Treating bounce for $type " . $self->object->id . " as auto-reply to sender");
+        $self->handle_non_bounce_to_verp_address();
+    } elsif (!$info) {
+        print_log('info', "Unparsed bounce received for $type " . $self->object->id . ", forwarding to support");
+        $self->forward_on_to($self->bouncemgr);
+    } else {
+        print_log('info', "Ignoring bounce received for $type " . $self->object->id . $info);
+    }
+}
+
+sub handle_non_bounce_to_verp_address {
+    my $self = shift;
+    if ($self->type eq 'alert' && !$self->is_out_of_office()) {
+        print_log('info', "Received non-bounce for alert " . $self->object->id . ", forwarding to support");
+        $self->forward_on_to($self->bouncemgr);
+    } elsif ($self->type eq 'report') {
+        my $contributed_as = $self->object->get_extra_metadata('contributed_as') || '';
+        if ($contributed_as eq 'body' || $contributed_as eq 'anonymous_user') {
+            print_log('info', "Received non-bounce for report " . $self->object->id . " to anon report, dropping");
+        } else {
+            print_log('info', "Received non-bounce for report " . $self->object->id . ", forwarding to report creator");
+            $self->forward_on_to($self->object->user->email);
+        }
+    }
+}
+
+sub handle_non_bounce_to_null_address {
+    my $self = shift;
+    # Don't send a reply to out of office replies...
+    if ($self->is_out_of_office()) {
+        print_log('info', "Received non-bounce auto-reply to null address, ignoring");
+        return;
+    }
+
+    # Send an automatic response
+    print_log('info', "Received non-bounce to null address, auto-replying");
+
+    my ( $cobrand, $from_addr, $from_name ) = $self->get_config_for_autoresponse();
+
+    my $template = path(FixMyStreet->path_to("templates", "email", $cobrand, 'reply-autoresponse'))->slurp_utf8;
+
+    # We generate this as a bounce.
+    my ($rp) = $self->data->{return_path} =~ /^\s*<(.*)>\s*$/;
+    my $mail = FixMyStreet::Email::construct_email({
+        'Auto-Submitted' => 'auto-replied',
+        From => [ $from_addr, $from_name ],
+        To => $rp,
+        _body_ => $template,
+    });
+    send_mail($mail, $rp);
+}
+
+# Based on the address the incoming message was sent to, we might want to
+# use a cobrand's own reply-autoresponse template.
+sub get_config_for_autoresponse {
+    my $self = shift;
+    # cobrand might have been set from command line, so prefer that if so.
+    if ( $self->cobrand ) {
+        return ( $self->cobrand, FixMyStreet->config('CONTACT_EMAIL'), FixMyStreet->config('CONTACT_NAME') );
+    }
+
+    # Try and find a matching email address in the COBRAND_FEATURES config
+    my $recipient = mySociety::HandleMail::get_bounce_recipient($self->data->{message})->address;
+    my $features = FixMyStreet->config('COBRAND_FEATURES') || {};
+    my $cobrands = $features->{do_not_reply_email} || {};
+    for my $moniker ( keys %$cobrands ) {
+        if ( $cobrands->{$moniker} eq $recipient ) {
+            my $cb = FixMyStreet::Cobrand->get_class_for_moniker($moniker)->new();
+            return ( $moniker, $cb->contact_email, $cb->contact_name );
+        }
+    }
+
+    # No match found, so use default cobrand
+    return ( "default", FixMyStreet->config('CONTACT_EMAIL'), FixMyStreet->config('CONTACT_NAME') );
+}
+
+sub forward_on_to {
+    my $self = shift;
+    my $recipient = shift;
+    my $text = join("\n", @{$self->lines}) . "\n";
+    send_mail($text, $recipient);
+}
+
+sub send_mail {
+    my ($email, $recipient) = @_;
+    unless (FixMyStreet::Email::Sender->try_to_send(
+        $email, { from => '<>', to => $recipient }
+    )) {
+        exit(75);
+    }
+}
+
+1;
---
+++ t/email/incoming.t
@@ -0,0 +1,223 @@
+use Test::Trap;
+use FixMyStreet::TestMech;
+use_ok 'FixMyStreet::Email::Incoming';
+use FixMyStreet::Email;
+
+my $mech = FixMyStreet::TestMech->new;
+
+my $body = $mech->create_body_ok(2217, 'Buckinghamshire Council');
+my ($p) = $mech->create_problems_for_body(1, $body->id, 'Title');
+my $alert = FixMyStreet::DB->resultset("Alert")->create({
+    alert_type => 'new_updates',
+    user_id => $p->user_id,
+});
+my $id = $p->id;
+my $alert_id = $alert->id;
+my $token_report = FixMyStreet::Email::generate_verp_token('report', $id);
+my $token_alert = FixMyStreet::Email::generate_verp_token('alert', $alert_id);
+
+# For testing, want to turn this back on and see what we get
+mySociety::SystemMisc::log_to_stderr(1);
+
+sub email_from_template {
+    my %params = @_;
+    $params{RETURNPATH} = $params{RETURNPATH} ? 'from@council.example.net': '';
+    my $email = <<'EOF';
+Return-Path: <RETURNPATH>
+From: someone@example.org
+To: fms-TOKEN@example.com
+Subject: SUBJECT Re: Problem report
+Message-ID: <ABCDEF@GHIJKL>
+HEADERS
+
+This is the contents of the message.
+EOF
+    foreach (keys %params) {
+        $email =~ s/$_/$params{$_}/g;
+    }
+    return $email;
+}
+
+sub process {
+    my $email = shift;
+    open my $stdin, '<', \$email;
+    local *STDIN = $stdin;
+    my $e = FixMyStreet::Email::Incoming->new( bouncemgr => 'contact@example.org' );
+    trap { $e->process };
+}
+
+FixMyStreet::override_config {
+    EMAIL_DOMAIN => 'example.com',
+    ALLOWED_COBRANDS => ['buckinghamshire', 'fixmystreet', 'tfl'],
+    COBRAND_FEATURES => {
+        do_not_reply_email =>  {
+            tfl => 'fms-tfl-do-not-reply@example.com'
+        },
+    },
+}, sub {
+    subtest 'A bad token email' => sub {
+        my $email = email_from_template(TOKEN => "bad");
+        process($email);
+        is $trap->exit, 0, 'exited with 0';
+    };
+
+    subtest 'A bounce to the do-not-reply address' => sub {
+        my $email = email_from_template(RETURNPATH => 0, TOKEN => "DO-NOT-REPLY");
+        process($email);
+        is $trap->stderr, "incoming.t: bounce received for don't-care email\n";
+    };
+
+    subtest 'An email to the do-not-reply address' => sub {
+        my $email = email_from_template(RETURNPATH => 1, HEADERS => "X-Delivered-Suffix: -DO-NOT-REPLY");
+        process($email);
+        is $trap->stderr, "incoming.t: Received non-bounce to null address, auto-replying\n";
+        like $mech->get_text_body_from_email, qr/This is an automatic response/;
+    };
+
+    subtest 'An email to the cobrand specific do-not-reply address' => sub {
+        my $email = email_from_template(RETURNPATH => 1, TOKEN => 'tfl-do-not-reply');
+        process($email);
+        is $trap->stderr, "incoming.t: Received non-bounce to null address, auto-replying\n";
+        like $mech->get_text_body_from_email, qr/from TfL Street Care/;
+    };
+
+    subtest 'An OOO email to the do-not-reply address' => sub {
+        my $email = email_from_template(RETURNPATH => 1, TOKEN => "DO-NOT-REPLY", SUBJECT => "Out of Office");
+        process($email);
+        is $trap->stderr, "incoming.t: Received non-bounce auto-reply to null address, ignoring\n";
+        $mech->email_count_is(0);
+    };
+
+    subtest 'A bounce to a VERP address' => sub {
+        my $email = email_from_template(RETURNPATH => 0, TOKEN => $token_report);
+        process($email);
+        is $trap->stderr, "incoming.t: Unparsed bounce received for report $id, forwarding to support\n";
+        my $env = $mech->get_email_envelope;
+        is $env->{to}[0], 'contact@example.org';
+        $email = $mech->get_email;
+        like $email->as_string, qr/This is the contents/;
+        $mech->clear_emails_ok;
+    };
+
+    subtest 'An OOO bounce to a VERP address' => sub {
+        my $email = email_from_template(RETURNPATH => 0, TOKEN => $token_report, HEADERS => "X-Autoreply: true");
+        process($email);
+        is $trap->stderr, "incoming.t: Treating bounce for report $id as auto-reply to sender\nincoming.t: Received non-bounce for report $id, forwarding to report creator\n";
+        my $env = $mech->get_email_envelope;
+        is $env->{to}[0], $p->user->email;
+        $email = $mech->get_email;
+        like $email->as_string, qr/This is the contents/;
+        $mech->clear_emails_ok;
+    };
+
+    subtest 'An email to a VERP address' => sub {
+        my $email = email_from_template(RETURNPATH => 1, TOKEN => $token_report);
+        process($email);
+        is $trap->stderr, "incoming.t: Received non-bounce for report $id, forwarding to report creator\n";
+        my $env = $mech->get_email_envelope;
+        is $env->{to}[0], $p->user->email;
+        $email = $mech->get_email;
+        like $email->as_string, qr/This is the contents/;
+        $mech->clear_emails_ok;
+    };
+
+    subtest 'An email to a VERP address, report made as body' => sub {
+        $p->set_extra_metadata(contributed_as => 'body');
+        $p->update;
+        my $email = email_from_template(RETURNPATH => 1, TOKEN => $token_report);
+        process($email);
+        is $trap->stderr, "incoming.t: Received non-bounce for report $id to anon report, dropping\n";
+        $mech->email_count_is(0);
+        $p->unset_extra_metadata('contributed_as');
+        $p->update;
+    };
+
+    subtest 'An email to an alert VERP address' => sub {
+        my $email = email_from_template(RETURNPATH => 1, TOKEN => $token_alert);
+        process($email);
+        is $trap->stderr, "incoming.t: Received non-bounce for alert $alert_id, forwarding to support\n";
+        my $env = $mech->get_email_envelope;
+        is $env->{to}[0], 'contact@example.org';
+        $email = $mech->get_email;
+        like $email->as_string, qr/This is the contents/;
+        $mech->clear_emails_ok;
+    };
+
+    subtest 'An OOO email to an alert VERP address' => sub {
+        my $email = email_from_template(RETURNPATH => 1, TOKEN => $token_alert, HEADERS => "Auto-Submitted: yes");
+        process($email);
+        is $trap->stderr, '';
+        $mech->email_count_is(0);
+    };
+
+    subtest 'A DSN' => sub {
+        my $email = <<EOF;
+Return-path: <>
+Date: Thu, 11 Sep 2008 05:00:33 -0500
+From: someone\@example.org
+Message-Id: <message-id\@somewhere.example.org>
+To: fms-$token_report\@example.com
+Content-Type: multipart/report; report-type=delivery-status;
+	boundary="1221127233-13870"
+Subject: Returned mail: User unknown
+
+This is a MIME-encapsulated message
+
+--1221127233-13870
+Content-type: text/plain; charset=US-ASCII
+
+The original message was received Thu, 11 Sep 2008 05:00:32 -0500
+from -
+
+   ----- The following address(es) had permanent fatal errors -----
+<anon>; originally to anon (unrecoverable error)
+  	The recipient 'anon' is unknown
+
+--1221127233-13870
+Content-type: message/delivery-status
+
+Arrival-Date: Thu, 11 Sep 2008 05:00:32 -0500
+
+Original-Recipient: anon
+Final-Recipient: anon
+Action: failed
+Status: 5.0.0
+--1221127233-13870
+Content-type: message/rfc822
+
+
+-- Message body has been omitted --
+
+--1221127233-13870--
+EOF
+        process($email);
+        is $trap->stderr, "incoming.t: Received bounce for report $id, forwarding to support\n";
+        my $env = $mech->get_email_envelope;
+        is $env->{to}[0], 'contact@example.org';
+        $email = $mech->get_email;
+        $mech->clear_emails_ok;
+    };
+
+    subtest 'A remote host bounce to an alert VERP address' => sub {
+        my $email = <<EOF;
+Return-path: <>
+Date: Thu, 11 Sep 2008 05:00:33 -0500
+From: someone\@example.org
+Message-Id: <message-id\@somewhere.example.org>
+To: fms-$token_alert\@example.com
+Subject: Returned mail: User unknown
+
+This server does not like recipient.
+
+Remote host said: 500 User does not exist - <@>
+
+EOF
+        process($email);
+        is $trap->stderr, "incoming.t: Received bounce for alert $alert_id, unsubscribing\n";
+        $alert->discard_changes;
+        isnt $alert->whendisabled, undef;
+    };
+
+};
+
+done_testing;
