f74adf482bc8fd9b4756741023e23eb7f35f90cd
Dave Arter <davea@mysociety.org>
2022-10-25 16:00:34 +0100

    [Camden] Remove Bristol-specific stuff

--- perllib/FixMyStreet/Cobrand/Camden.pm
+++ perllib/FixMyStreet/Cobrand/Camden.pm
@@ -4,77 +4,30 @@ use parent 'FixMyStreet::Cobrand::Whitelabel';
 use strict;
 use warnings;
 
-sub council_area_id { return 2561; }
+sub council_area_id { return 2505; }
 sub council_area { return 'Camden'; }
-sub council_name { return 'Camden County Council'; }
-sub council_url { return 'bristol'; }
-
-sub map_type {
-    'Bristol';
-}
+sub council_name { return 'Camden Council'; }
+sub council_url { return 'camden'; }
 
 sub disambiguate_location {
     my $self    = shift;
     my $string  = shift;
 
-    my $town = 'Bristol';
+    my $town = 'Camden';
 
     return {
         %{ $self->SUPER::disambiguate_location() },
         town   => $town,
-        centre => '51.4526044866206,-2.7706173308649',
-        span   => '0.202810508012753,0.60740886659825',
-        bounds => [ 51.3415749466466, -3.11785543094126, 51.5443854546593, -2.51044656434301 ],
+        centre => '51.546390811297,-0.157422262955539',
+        span   => '0.0603011959324533,0.108195286339115',
+        bounds => [ 51.5126591342049, -0.213511484504216, 51.5729603301373, -0.105316198165101 ],
     };
-}
-
-sub get_geocoder {
-    return 'OSM'; # use OSM geocoder
-}
-
-sub pin_colour {
-    my ( $self, $p, $context ) = @_;
-    return 'grey' if $p->state eq 'not responsible';
-    return 'green' if $p->is_fixed || $p->is_closed;
-    return 'red' if $p->state eq 'confirmed';
-    return 'yellow';
 }
 
 sub send_questionnaires {
     return 0;
 }
 
-sub categories_restriction {
-    my ($self, $rs) = @_;
-    # Categories covering the Bristol area have a mixture of Open311 and Email
-    # send methods. Bristol only want Open311 categories to be visible on their
-    # cobrand, not the email categories from FMS.com. We've set up the
-    # Email categories with a devolved send_method, so can identify Open311
-    # categories as those which have a blank send_method.
-    # Also National Highways categories have a blank send_method.
-    return $rs->search( { -or => [
-        'me.send_method' => undef, # Open311 categories
-        'me.send_method' => '', # Open311 categories that have been edited in the admin
-    ] } );
-}
-
-sub open311_config {
-    my ($self, $row, $h, $params) = @_;
-
-    $params->{always_send_email} = 1;
-}
-
-sub open311_contact_meta_override {
-    my ($self, $service, $contact, $meta) = @_;
-
-    my %server_set = (easting => 1, northing => 1);
-    my %hidden_field = (usrn => 1, asset_id => 1);
-    foreach (@$meta) {
-        $_->{automated} = 'server_set' if $server_set{$_->{code}};
-        $_->{automated} = 'hidden_field' if $hidden_field{$_->{code}};
-    }
-}
-
-sub admin_user_domain { 'bristol.gov.uk' }
+sub admin_user_domain { 'camden.gov.uk' }
 
 1;
