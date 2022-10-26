cbdf1eef375dc56aac01585eb3c514d1cc5c2010
Lucas <hello@lucascm.dev>
2022-10-26 05:55:42 +0100

    [Brent] Add initial cobrand design.

---
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -0,0 +1,11 @@
+package FixMyStreet::Cobrand::Brent;
+use parent 'FixMyStreet::Cobrand::UKCouncils';
+
+sub council_area_id { return 2488; }
+sub council_area { return 'Brent'; }
+sub council_name { return 'Brent Council'; }
+sub council_url { return 'brent'; }
+
+sub admin_user_domain { 'brent.gov.uk' }
+
+1;
