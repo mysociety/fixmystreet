a71bc13a395d14783507ae0998dc3720721d344e
Matthew Somerville <matthew@mysociety.org>
2022-11-11 17:50:22 +0000

    [Brent] Initial cobrand configuration.
    Including anonymous reporting, privacy URL, OS maps, zoom level,
    location disambiguation, reopening of reports, social auth.

--- perllib/FixMyStreet/Cobrand/Brent.pm
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -12,4 +12,28 @@ sub path_to_pin_icons {
 
 sub admin_user_domain { 'brent.gov.uk' }
 
+sub allow_anonymous_reports { 'button' }
+
+sub default_map_zoom { 6 }
+
+sub privacy_policy_url {
+    'https://www.brent.gov.uk/the-council-and-democracy/access-to-information/data-protection-and-privacy/brent-privacy-policy'
+}
+
+sub get_geocoder { 'OSM' }
+
+sub reopening_disallowed { 1 }
+
+sub disambiguate_location { {
+    centre => '51.5585509362304,-0.26781886445231',
+    span   => '0.0727325098393763,0.144085171830317',
+    bounds => [ 51.52763684136, -0.335577710963202, 51.6003693511994, -0.191492539132886 ],
+} }
+
+sub social_auth_enabled {
+    my $self = shift;
+
+    return $self->feature('oidc_login') ? 1 : 0;
+}
+
 1;
