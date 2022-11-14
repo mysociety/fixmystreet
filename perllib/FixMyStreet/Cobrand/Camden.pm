b2d1bccc7a0d33c2141f175e9971d8b3705f88ba
Chris Mytton <chrism@mysociety.org>
2022-11-14 10:55:15 +0000

    [Camden] Add NSGRef to reports if missing
    Reports from the app don't run the asset.js, so do the lookup
    server-side as well as client-side.
    
    The client side asset layer is configured in the asset_layers config.

--- perllib/FixMyStreet/Cobrand/Camden.pm
+++ perllib/FixMyStreet/Cobrand/Camden.pm
@@ -37,4 +37,34 @@ sub privacy_policy_url {
 
 sub admin_user_domain { 'camden.gov.uk' }
 
+sub lookup_site_code_config {
+    my ($self, $property) = @_;
+
+    # uncoverable subroutine
+    # uncoverable statement
+    {
+        buffer => 1000, # metres
+        url => "https://tilma.mysociety.org/mapserver/camden",
+        srsname => "urn:ogc:def:crs:EPSG::27700",
+        typename => "Streets",
+        property => $property,
+        accept_feature => sub { 1 },
+    }
+}
+
+sub open311_extra_data_include {
+    my ($self, $row, $h, $contact) = @_;
+
+    # Reports made via the app probably won't have a NSGRef because we don't
+    # display the road layer. Instead we'll look up the closest asset from the
+    # WFS service at the point we're sending the report over Open311.
+    if (!$row->get_extra_field_value('NSGRef')) {
+        if (my $ref = $self->lookup_site_code($row, 'NSG_REF')) {
+            $row->update_extra_field({ name => 'NSGRef', description => 'NSG Ref', value => $ref });
+        }
+    }
+
+    return [];
+}
+
 1;
