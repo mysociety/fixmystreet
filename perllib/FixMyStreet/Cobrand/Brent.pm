34895a78701886be6b75ee94eebfacae5b07e391
Moray Jones <moray@mysociety.org>
2022-11-14 08:36:07 +0000

    [Brent] Configuration
    * Adds privacy url
    * OS maps option
    * Zoom
    * Disambiguate location

--- perllib/FixMyStreet/Cobrand/Brent.pm
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -10,4 +10,18 @@ sub admin_user_domain { 'brent.gov.uk' }
 
 sub allow_anonymous_reports { 'button' }
 
+sub default_map_zoom { 6 }
+
+sub privacy_policy_url {
+    'https://www.brent.gov.uk/the-council-and-democracy/access-to-information/data-protection-and-privacy/brent-privacy-policy'
+}
+
+sub get_geocoder { 'OSM' }
+
+sub disambiguate_location { {
+    centre => '51.5585509362304,-0.26781886445231',
+    span   => '0.0727325098393763,0.144085171830317',
+    bounds => [ 51.52763684136, -0.335577710963202, 51.6003693511994, -0.191492539132886 ],
+} }
+
 1;
