1a7ecfbe23b3091ed2d2c32709e5f7038bc21694
Matthew Somerville <matthew@mysociety.org>
2022-11-15 13:38:28 +0000

    [Brent] Open311 configuration.
    Multiple photos, send through ID with updates,
    and server-side road check.

--- perllib/FixMyStreet/Cobrand/Brent.pm
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -1,6 +1,12 @@
 package FixMyStreet::Cobrand::Brent;
 use parent 'FixMyStreet::Cobrand::UKCouncils';
 
+use strict;
+use warnings;
+use Moo;
+with 'FixMyStreet::Roles::Open311Multi';
+with 'FixMyStreet::Roles::CobrandOpenUSRN';
+
 sub council_area_id { return 2488; }
 sub council_area { return 'Brent'; }
 sub council_name { return 'Brent Council'; }
@@ -23,5 +29,33 @@ sub disambiguate_location { {
     span   => '0.0727325098393763,0.144085171830317',
     bounds => [ 51.52763684136, -0.335577710963202, 51.6003693511994, -0.191492539132886 ],
 } }
+
+sub open311_config {
+    my ($self, $row, $h, $params) = @_;
+    $params->{multi_photos} = 1;
+}
+
+sub open311_munge_update_params {
+    my ($self, $params, $comment, $body) = @_;
+    $params->{service_request_id_ext} = $comment->problem->id;
+}
+
+sub open311_extra_data_include {
+    my ($self, $row, $h, $contact) = @_;
+
+    my $open311_only;
+    if ($contact->email =~ /^Symology/) {
+        # Reports made via the app probably won't have a NSGRef because we don't
+        # display the road layer. Instead we'll look up the closest asset from the
+        # WFS service at the point we're sending the report over Open311.
+        if (!$row->get_extra_field_value('NSGRef')) {
+            if (my $ref = $self->lookup_site_code($row, 'usrn')) {
+                $row->update_extra_field({ name => 'NSGRef', description => 'NSG Ref', value => $ref });
+            }
+        }
+    }
+
+    return $open311_only;
+}
 
 1;
