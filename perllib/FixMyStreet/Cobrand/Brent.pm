fd065375f589668a005873034d8b300c2925806e
Matthew Somerville <matthew@mysociety.org>
2022-11-15 08:54:35 +0000

    fixup! [Brent] Configuration

--- perllib/FixMyStreet/Cobrand/Brent.pm
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -18,10 +18,10 @@ sub privacy_policy_url {
 
 sub get_geocoder { 'OSM' }
 
-sub disambiguate_location {
+sub disambiguate_location { {
     centre => '51.5585509362304,-0.26781886445231',
     span   => '0.0727325098393763,0.144085171830317',
     bounds => [ 51.52763684136, -0.335577710963202, 51.6003693511994, -0.191492539132886 ],
-}
+} }
 
 1;
