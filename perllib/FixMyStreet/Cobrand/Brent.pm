a10bba5d640a8f7f3796b248c87b2ca955655dc3
Lucas <hello@lucascm.dev>
2022-12-16 11:22:52 +0000

    [Brent] Add custom map pin colours.

--- perllib/FixMyStreet/Cobrand/Brent.pm
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -6,6 +6,10 @@ sub council_area { return 'Brent'; }
 sub council_name { return 'Brent Council'; }
 sub council_url { return 'brent'; }
 
+sub path_to_pin_icons {
+    return '/cobrands/brent/images/';
+}
+
 sub admin_user_domain { 'brent.gov.uk' }
 
 1;
