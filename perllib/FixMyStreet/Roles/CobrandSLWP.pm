5b9e1524f297fa714efd1158c5c95d91e927cf55
Matthew Somerville <matthew@mysociety.org>
2023-01-06 16:30:52 +0000

    [SLWP] Cope when no data from NLPG.

--- perllib/FixMyStreet/Roles/CobrandSLWP.pm
+++ perllib/FixMyStreet/Roles/CobrandSLWP.pm
@@ -172,8 +172,8 @@ around look_up_property => sub {
     my $cfg = $self->feature('echo');
     if ($cfg->{nlpg} && $data->{uprn}) {
         my $uprn_data = get(sprintf($cfg->{nlpg}, $data->{uprn}));
-        $uprn_data = JSON::MaybeXS->new->decode($uprn_data);
-        if ($uprn_data->{results}[0]{LPI}{LOCAL_CUSTODIAN_CODE_DESCRIPTION} ne $self->lpi_value) {
+        $uprn_data = JSON::MaybeXS->new->decode($uprn_data) if $uprn_data;
+        if (!$uprn_data || $uprn_data->{results}[0]{LPI}{LOCAL_CUSTODIAN_CODE_DESCRIPTION} ne $self->lpi_value) {
             $self->{c}->stash->{template} = 'waste/missing.html';
             $self->{c}->detach;
         }
