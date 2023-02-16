3dd8eb3e5374b7e4d9f85bf9145de23a41ee1e23
Moray Jones <moray@mysociety.org>
2023-02-16 12:55:35 +0000

    [Brent] Adds half price GGW service for late subscription
    The GGW service ends at the end of March. Subscribing in
    Oct/Nov/Dec is half price.
    
    This does not apply to Jan/Feb/Mar as those subscriptions
    will only begin in April.

--- perllib/FixMyStreet/Cobrand/Brent.pm
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -854,7 +854,14 @@ sub garden_waste_cost_pa {
 
     $bin_count ||= 1;
 
-    return $self->feature('payment_gateway')->{ggw_cost} * $bin_count;
+    my $cost = $self->feature('payment_gateway')->{ggw_cost} * $bin_count;
+    my $now = DateTime->now( time_zone => FixMyStreet->local_time_zone );
+
+    if ($now->month =~ /^(10|11|12)$/ ) {
+        $cost = $cost/2;
+    }
+
+    return $cost;
 }
 
 sub garden_waste_new_bin_admin_fee { 0 }
