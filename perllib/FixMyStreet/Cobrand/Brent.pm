df780f63bc9ab09e748ef2246e021f2d15c58d57
Matthew Somerville <matthew@mysociety.org>
2023-02-17 12:10:08 +0000

    [Brent] Add paye.net integration.

--- perllib/FixMyStreet/Cobrand/Brent.pm
+++ perllib/FixMyStreet/Cobrand/Brent.pm
@@ -37,6 +37,7 @@ with 'FixMyStreet::Roles::Open311Multi';
 with 'FixMyStreet::Roles::CobrandOpenUSRN';
 with 'FixMyStreet::Roles::CobrandEcho';
 with 'FixMyStreet::Roles::SCP';
+use Integrations::Paye;
 
 sub council_area_id { return 2488; }
 sub council_area { return 'Brent'; }
@@ -359,19 +360,111 @@ sub admin_templates_external_status_code_hook {
     return $code;
 }
 
+around waste_cc_get_redirect_url => sub {
+    my ($orig, $self, $c, $back) = @_;
+
+    if ($c->stash->{is_staff}) {
+        my $payment = Integrations::Paye->new({
+            config => $self->feature('payment_gateway')
+        });
+
+        my $p = $c->stash->{report};
+        my $uprn = $p->get_extra_field_value('uprn');
+
+        my $amount = $p->get_extra_field_value( 'pro_rata' );
+        unless ($amount) {
+            $amount = $p->get_extra_field_value( 'payment' );
+        }
+        my $admin_fee = $p->get_extra_field_value('admin_fee');
+
+        my $redirect_id = mySociety::AuthToken::random_token();
+        $p->set_extra_metadata('redirect_id', $redirect_id);
+        $p->update;
+
+        my $backUrl = $c->uri_for_action("/waste/$back", [ $c->stash->{property}{id} ]) . '';
+        my $address = $c->stash->{property}{address};
+        my @parts = split ',', $address;
+
+        my @items = ({
+            amount => $amount,
+            reference => $payment->config->{customer_ref},
+            description => $p->title,
+            lineId => $self->waste_cc_payment_line_item_ref($p),
+        });
+        if ($admin_fee) {
+            push @items, {
+                amount => $admin_fee,
+                reference => $payment->config->{customer_ref_admin_fee},
+                description => 'Admin fee',
+                lineId => $self->waste_cc_payment_admin_fee_line_item_ref($p),
+            };
+        }
+        my $result = $payment->pay({
+            returnUrl => $c->uri_for('pay_complete', $p->id, $redirect_id ) . '',
+            backUrl => $backUrl,
+            ref => $self->waste_cc_payment_sale_ref($p),
+            request_id => $p->id,
+            description => $p->title,
+            name => $p->name,
+            email => $p->user->email,
+            uprn => $uprn,
+            address1 => shift @parts,
+            address2 => shift @parts,
+            country => 'UK',
+            postcode => pop @parts,
+            items => \@items,
+            staff => $c->stash->{staff_payments_allowed} eq 'cnp',
+        });
+
+        if ( $result ) {
+            $c->stash->{xml} = $result;
+
+            # GET back
+            # requestId - should match above
+            # apnReference - transaction Ref, used later for query
+            # transactionState - InProgress
+            # invokeResult/status - Success/...
+            # invokeResult/redirectUrl - what is says
+            # invokeResult/errorDetails - what it says
+            if ( $result->{transactionState} eq 'InProgress' &&
+                 $result->{invokeResult}->{status} eq 'Success' ) {
+
+                 $p->set_extra_metadata('apnReference', $result->{apnReference});
+                 $p->update;
+
+                 my $redirect = $result->{invokeResult}->{redirectUrl};
+                 if ($redirect =~ /#/) {
+                     $redirect =~ s/#/?apnReference=$result->{apnReference}#/;
+                 } else {
+                     $redirect .= "?apnReference=$result->{apnReference}";
+                 }
+                 return $redirect;
+             } else {
+                 # XXX - should this do more?
+                my $error = $result->{invokeResult}->{status};
+                $c->stash->{error} = $error;
+                return undef;
+             }
+         } else {
+            return undef;
+        }
+        return;
+    }
+
+    return $self->$orig($c, $back);
+};
+
 =head2 waste_check_staff_payment_permissions
 
-Staff can make payments via entering a PAYE code.
+Staff can make payments via a PAYE endpoint.
 
 =cut
 
 sub waste_check_staff_payment_permissions {
     my $self = shift;
     my $c = $self->{c};
-
     return unless $c->stash->{is_staff};
-
-    $c->stash->{staff_payments_allowed} = 'paye';
+    $c->stash->{staff_payments_allowed} = 'paye-api';
 }
 
 =head2 waste_event_state_map
