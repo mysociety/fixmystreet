cb66309542247e4eae234b42ef6cbf882b7e738d
Nik Gupta <vngupta77@gmail.com>
2023-07-09 12:33:16 +0100

    Add support for multivaluelist category extra questions using checkboxes.

--- perllib/FixMyStreet/App/Controller/Report/New.pm
+++ perllib/FixMyStreet/App/Controller/Report/New.pm
@@ -1341,10 +1341,18 @@ sub set_report_extras : Private {
                     $c->stash->{field_errors}->{ 'x' . $field->{code} } = _('This information is required');
                 }
             }
+
+            my $value;
+            if ($field->{datatype} eq 'multivaluelist') {
+                $value = $c->req->params->{$param_prefix . $field->{code}} // [];
+            } else {
+                $value = $c->get_param($param_prefix . $field->{code}) // '';
+            }
+
             push @extra, {
                 name => $field->{code},
                 description => $field->{description},
-                value => $c->get_param($param_prefix . $field->{code}) // '',
+                value => $value,
             };
         }
     }
