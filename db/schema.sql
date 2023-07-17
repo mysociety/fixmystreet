de9ceabcc68d2659f6f5e7be5f7223382ce558f7
Matthew Somerville <matthew@mysociety.org>
2023-07-17 10:16:50 +0100

    Update main schema to match migration.

--- db/schema.sql
+++ db/schema.sql
@@ -260,7 +260,7 @@ create index problem_state_latitude_longitude_idx on problem(state, latitude, lo
 create index problem_user_id_idx on problem ( user_id );
 create index problem_external_id_idx on problem(external_id);
 create index problem_external_body_idx on problem(lower(external_body));
-create index problem_send_state_state_idx on problem(send_state, state);
+create index problem_send_state_state_idx on problem(send_state, state) WHERE state NOT IN ('unconfirmed', 'hidden', 'partial');
 create index problem_radians_latitude_longitude_idx on problem(radians(latitude), radians(longitude));
 create index problem_bodies_str_array_idx on problem USING gin(regexp_split_to_array(bodies_str, ','));
 create index problem_fulltext_idx on problem USING GIN(
