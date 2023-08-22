685830f03f4f11dc73425d5f222c0b7a4a7c9cb2
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2023-08-22 15:28:24 +0100

    [SLWP] Check bulky eligibility for properties.

--- perllib/FixMyStreet/Roles/CobrandEcho.pm
+++ perllib/FixMyStreet/Roles/CobrandEcho.pm
@@ -55,6 +55,7 @@ sub look_up_property {
         address => FixMyStreet::Template::title($result->{Description}),
         latitude => $result->{Coordinates}{GeoPoint}{Latitude},
         longitude => $result->{Coordinates}{GeoPoint}{Longitude},
+        type_id => $result->{PointAddressType}{Id},
     };
 }
 
