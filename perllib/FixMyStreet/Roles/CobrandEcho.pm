c48f62838d6631e8988ac40d5ff39803ac701fbe
Moray Jones <moray@mysociety.org>
2023-08-23 11:51:30 +0100

    [SLWP] Link notes and photos to extra data fields

--- perllib/FixMyStreet/Roles/CobrandEcho.pm
+++ perllib/FixMyStreet/Roles/CobrandEcho.pm
@@ -568,17 +568,19 @@ sub waste_munge_bulky_data {
 
     my @notes;
     my @ids;
+    my @photos;
 
     my $max = $self->bulky_items_maximum;
     for (1..$max) {
         if (my $item = $data->{"item_$_"}) {
-            push @notes, $item; # XXX this should be per-item note field from user, not item description
+            push @notes, $data->{"item_notes_$_"};
             push @ids, $items{$item};
+            push @photos, $data->{"item_photos_$_"};
         };
     }
     $data->{extra_Bulky_Collection_Notes} = join("::", @notes);
     $data->{extra_Bulky_Collection_Bulky_Items} = join("::", @ids);
-
+    $data->{extra_Image} = join("::", @photos);
     $data->{extra_Payment_Details_Payment_Amount} = $self->bulky_total_cost($data);
 }
 
