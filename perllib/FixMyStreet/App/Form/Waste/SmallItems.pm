f7d29d865f40617a223fabebb8bf3ccd28e2c39d
Moray Jones <moray@mysociety.org>
2023-09-06 11:41:46 +0100

    [Brent][WW] Making a booking
    Adds count of how many small WEEE items have been selected
    and how many different 'types' of item have been selected.
    
    Add error if more that 4 small WEEE or more than 3 types.
    
    Added text to clarify the limits allowed

--- perllib/FixMyStreet/App/Form/Waste/SmallItems.pm
+++ perllib/FixMyStreet/App/Form/Waste/SmallItems.pm
@@ -274,6 +274,26 @@ sub validate {
                 $self->add_form_error("Too many of item: $_");
             }
         }
+        if ($self->{c}->cobrand->moniker eq 'brent') {
+            my %category_count;
+            for my $name (keys %given) {
+                for my $list_item (@{ $self->items_master_list }) {
+                    if ($list_item->{'name'} eq $name) {
+                        if ($list_item->{'category'}) {
+                            $category_count{$list_item->{'category'}} += $given{$name};
+                        } else {
+                            $category_count{$list_item->{'name'}} += $given{$name};
+                        }
+                    }
+                }
+            }
+            if (scalar keys %category_count > 3) {
+                $self->add_form_error("Too many categories: maximum of 3 types");
+            }
+            if (scalar $category_count{'Small WEEE'} && scalar $category_count{'Small WEEE'} > 4) {
+                $self->add_form_error("Too many Small WEEE items: maximum 4");
+            }
+        }
     }
 }
 
