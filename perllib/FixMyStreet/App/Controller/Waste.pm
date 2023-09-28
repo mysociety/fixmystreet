789dfd0f6d3eacddb2c1687816393e62e1360fa6
Chris Mytton <chrism@mysociety.org>
2023-09-28 13:51:39 +0100

    [SLWP] Send bulky logged email after payment.
    Co-authored-by: Moray Jones <moray@mysociety.org>
    Co-authored-by: Matthew Somerville <matthew@mysociety.org>

--- perllib/FixMyStreet/App/Controller/Waste.pm
+++ perllib/FixMyStreet/App/Controller/Waste.pm
@@ -255,6 +255,11 @@ sub pay_complete : Path('pay_complete') : Args(2) {
         $c->stash->{reference} = $ref;
         $c->stash->{action} = $p->title eq 'Garden Subscription - Amend' ? 'add_containers' : 'new_subscription';
         $c->forward( 'confirm_subscription', [ $ref ] );
+        if ($c->cobrand->suppress_report_sent_email($p)) {
+            # Send bulky confirmation email after payment confirmation (see
+            # the suppress_report_sent_email in CobrandSLWP.pm)
+            $p->send_logged_email({ %{$c->stash} }, 0, $c->cobrand);
+        }
     } else {
         $c->stash->{template} = 'waste/pay_error.html';
         $c->detach;
