0d106f85750bc039b8a48171a7ebc13b65f55d43
Chris Mytton <chrism@mysociety.org>
2024-01-19 18:08:40 +0000

    [Bexley] [Whitespace] WIP: Code for showing list of services

--- perllib/FixMyStreet/Cobrand/Bexley/Waste.pm
+++ perllib/FixMyStreet/Cobrand/Bexley/Waste.pm
@@ -5,7 +5,6 @@ use Moo::Role;
 use Integrations::Whitespace;
 use FixMyStreet::Template;
 use Sort::Key::Natural qw(natkeysort_inplace);
-use DateTime::Format::ISO8601;
 
 has 'whitespace' => (
     is => 'lazy',
@@ -27,6 +26,48 @@ sub bin_addresses_for_postcode {
     natkeysort_inplace { $_->{label} } @$data;
 
     return $data;
+}
+
+sub look_up_property {
+    my ($self, $id) = @_;
+
+    my $site = $self->whitespace->GetSiteInfo($id);
+
+    return {
+        id => $site->{AccountSiteID},
+        uprn => $site->{AccountSiteUPRN},
+        address => FixMyStreet::Template::title($site->{Site}->{SiteShortAddress}),
+        latitude => $site->{Site}->{SiteLatitude},
+        longitude => $site->{Site}->{SiteLongitude},
+    };
+}
+
+sub bin_services_for_address {
+    my $self = shift;
+    my $property = shift;
+
+    $self->{c}->stash->{containers} = {
+        # TODO
+    };
+
+    my $site_services = $self->whitespace->GetSiteCollections($property->{uprn});
+
+    # TODO: Filter out services with SiteServiceValidTo in the past or SiteServiceValidFrom in the future
+    $site_services = [ grep { $_->{NextCollectionDate} } @$site_services ];
+
+    my $services = [ map {
+        {
+            id => $_->{SiteServiceID},
+            service_name => $_->{ServiceItemDescription},
+            next => {
+                date => $_->{NextCollectionDate},
+                ordinal => 'th', # TODO!
+                changed => 0,
+            },
+        }
+    } @$site_services ];
+
+    return $services;
 }
 
 1;
