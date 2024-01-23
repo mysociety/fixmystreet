0dfead5877fe08146e10410e671ce15c7ff2cbe9
Moray Jones <moray@mysociety.org>
2024-01-23 10:29:02 +0000

    [TfL] Users with TfL email addresses shouldn't be prompted to change password
    TfL Staff will be logging in by OIDC only so shouldn't be
    forced to update their FMS password

--- perllib/FixMyStreet/Cobrand/TfL.pm
+++ perllib/FixMyStreet/Cobrand/TfL.pm
@@ -159,7 +159,14 @@ sub inactive_reports_filter {
 }
 
 sub password_expiry {
+    my ($self) = @_;
+
     return if FixMyStreet->test_mode;
+
+    my $email = $self->{c}->user->email;
+    my $domain_email = $self->admin_user_domain;
+    return if $email =~ /$domain_email$/;
+
     # uncoverable statement
     86400 * 365
 }
