168166fdd0231bddac7227403c04921d1f1489a8
Dave Arter <davea@mysociety.org>
2024-05-01 12:37:17 +0100

    [Surrey] Display town from geocoder on front page recent problems

--- perllib/FixMyStreet/Cobrand/Surrey.pm
+++ perllib/FixMyStreet/Cobrand/Surrey.pm
@@ -4,6 +4,8 @@ use parent 'FixMyStreet::Cobrand::Whitelabel';
 use strict;
 use warnings;
 
+use FixMyStreet::Geocode::Address;
+
 sub council_area_id { 2242 }
 sub council_area { 'Surrey' }
 sub council_name { 'Surrey County Council' }
@@ -23,5 +25,23 @@ sub disambiguate_location {
 }
 
 sub report_sent_confirmation_email { 'id' }
+
+
+=head2 get_town
+
+Returns the name of the town from the problem's geocode information, if present.
+
+=cut
+
+sub get_town {
+    my ($self, $p) = @_;
+
+    return unless $p->geocode;
+    my $geocode = FixMyStreet::Geocode::Address->new($p->geocode);
+    my $address = $geocode->{LPI} || $geocode->{address} || ($geocode->can('address') ? $geocode->address : '');
+    return unless $address;
+    my $town = $address->{town} || $address->{city} || $address->{TOWN_NAME} || $address->{locality} || $address->{village} || $address->{suburb};
+    return $town;
+}
 
 1;
