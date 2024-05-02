5a5d7157d85f93b42cf9c62b739ce40d653f575c
Dave Arter <davea@mysociety.org>
2024-05-02 14:09:17 +0100

    [Merton] Send updates to Crimson

--- perllib/FixMyStreet/Script/Merton/SendWaste.pm
+++ perllib/FixMyStreet/Script/Merton/SendWaste.pm
@@ -4,6 +4,7 @@ use Moo;
 use FixMyStreet::DB;
 use FixMyStreet::Queue::Item::Report;
 use FixMyStreet::SendReport::Open311;
+use Open311;
 
 has body => (
     is => 'ro',
@@ -11,7 +12,7 @@ has body => (
 );
 
 sub send_reports {
-    my ($self, $cobrand) = @_;
+    my ($self) = @_;
 
     my $problems = $self->_problems;
 
@@ -51,6 +52,55 @@ sub _problems {
         cobrand_data => 'waste',
         cobrand => 'merton',
         -not => { extra => { '\?' => 'sent_to_crimson' } },
+    });
+}
+
+sub send_comments {
+    my ($self) = @_;
+    my $comments = $self->_comments;
+
+    while (my $row = $comments->next) {
+        my $problem = $row->problem;
+
+        my $cobrand = $problem->get_cobrand_logged;
+        FixMyStreet::DB->schema->cobrand($cobrand);
+        $cobrand->set_lang_and_domain($problem->lang, 1);
+
+        my $cfg = $cobrand->feature('echo');
+        my %o311_cfg = (
+            jurisdiction => '',
+            endpoint => $cfg->{open311_endpoint},
+            api_key => $cfg->{open311_api_key},
+            extended_statuses => $self->body->send_extended_statuses,
+            fixmystreet_body => $self->body,
+            use_customer_reference => 1,
+        );
+        my $o = Open311->new(%o311_cfg);
+        $problem->set_extra_metadata( customer_reference => $problem->get_extra_metadata('crimson_external_id') );
+        my $id = $o->post_service_request_update( $row );
+        if ( $id ) {
+            $row->set_extra_metadata( sent_to_crimson => 1 );
+            $row->set_extra_metadata( crimson_external_id => $id );
+            $row->update;
+        } else {
+            STDERR->print("Failed to post over Open311\n\n" . $o->error . "\n");
+        }
+    }
+}
+
+sub _comments {
+    my $self = shift;
+    FixMyStreet::DB->resultset('Comment')->to_body($self->body->id)->search({
+        'problem.cobrand_data' => 'waste',
+        'problem.cobrand' => 'merton',
+        'problem.extra' => { '\?' => 'sent_to_crimson' },
+        'me.external_id' => \'is not null',
+        -or => [
+            'me.extra' => undef,
+            -not => { 'me.extra' => { '\?' => 'sent_to_crimson' } },
+        ],
+    }, {
+        prefetch => 'problem',
     });
 }
 
