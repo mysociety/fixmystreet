1369bd2b73d004fac563b8747faa8762d6d27e87
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2024-06-25 12:24:54 +0100

    [Bucks] Show custom photo tip when vehicle littering selected

--- web/cobrands/fixmystreet-uk-councils/council_category_hide_element_rules.js
+++
@@ -1,8 +0,0 @@
-hide_element_rules = {
-    'Central Bedfordshire Council': {
-        'Fly Tipping': [
-            "#js-description-tips",
-            "#js-photo-tips-avoid-personal-info"
-        ]
-    }
-};
