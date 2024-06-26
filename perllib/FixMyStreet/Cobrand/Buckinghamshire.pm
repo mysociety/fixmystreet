d26a4339607ded87fc27dc024e80965764db7a69
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2024-06-26 11:28:35 +0100

    [Bucks] Auto-set vehicle littering reports to 'investigating'

--- perllib/FixMyStreet/Cobrand/Buckinghamshire.pm
+++ perllib/FixMyStreet/Cobrand/Buckinghamshire.pm
@@ -34,6 +34,8 @@ use URI;
 use Try::Tiny;
 use Utils;
 
+use constant VEHICLE_LITTERING_CATEGORY => 'Littering From Vehicles';
+
 sub council_area_id { return 163793; }
 sub council_area { return 'Buckinghamshire'; }
 sub council_name { return 'Buckinghamshire Council'; }
@@ -143,6 +145,35 @@ sub category_change_force_resend { 1 }
 
 sub send_questionnaires {
     return 0;
+}
+
+=head2 post_report_sent
+
+Bucks have a special 'Littering From Vehicles' category; any reports made in
+that category are automatically set to 'Investigating'.
+
+=cut
+
+sub post_report_sent {
+    my ( $self, $problem ) = @_;
+
+    if ( $problem->category eq VEHICLE_LITTERING_CATEGORY ) {
+        my $new_state = 'investigating';
+        $problem->update( { state => $new_state } );
+
+        my $template = $problem->response_template_for( $new_state, 'dummy' );
+
+        my $user = $self->body->comment_user;
+
+        $problem->add_to_comments(
+            {   user          => $user,
+                send_state    => 'processed',
+                text          => $template->text,
+                problem_state => $new_state,
+                state         => 'confirmed',
+            }
+        );
+    }
 }
 
 sub open311_extra_data_exclude { [ 'road-placement' ] }
