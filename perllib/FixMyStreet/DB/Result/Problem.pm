4af11fa68853115f4479c26f77dc0bd9bcd18848
Dave Arter <davea@mysociety.org>
2024-07-03 14:46:27 +0100

    [Surrey] Remove FMS ID from display on report page

--- perllib/FixMyStreet/DB/Result/Problem.pm
+++ perllib/FixMyStreet/DB/Result/Problem.pm
@@ -971,7 +971,7 @@ Note: this only makes sense when called on a problem that has been sent!
 
 sub can_display_external_id {
     my $self = shift;
-    if ( $self->external_id && $self->to_body_named('Lincolnshire|Isle of Wight|East Sussex|Central Bedfordshire|Shropshire|Merton|Surrey') ) {
+    if ( $self->external_id && $self->to_body_named('Lincolnshire|Isle of Wight|East Sussex|Central Bedfordshire|Shropshire|Merton|Surrey County') ) {
         return 1;
     }
     return 0;
