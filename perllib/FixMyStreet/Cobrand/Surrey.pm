a9bc5461bde6a8dedcb0ea43706059fc3a5e5162
Dave Arter <davea@mysociety.org>
2024-07-31 13:40:05 +0100

    [Surrey] Do not send questionnaire emails

--- perllib/FixMyStreet/Cobrand/Surrey.pm
+++ perllib/FixMyStreet/Cobrand/Surrey.pm
@@ -33,6 +33,12 @@ sub disambiguate_location {
 
 sub suppress_reporter_alerts { 1 }
 
+=item * We do not send questionnaires.
+
+=cut
+
+sub send_questionnaires { 0 }
+
 sub enter_postcode_text { 'Enter a nearby UK postcode, or street name and area' }
 
 =item * The privacy policy is held on Surrey's own site
