3d28fce34447552222c56961bf5d58f7430d4c6c
Matthew Somerville <matthew@mysociety.org>
2024-08-14 18:48:08 +0100

    [Merton] Fallback approved payment check.

--- perllib/FixMyStreet/Script/Merton/SendWaste.pm
+++ perllib/FixMyStreet/Script/Merton/SendWaste.pm
@@ -20,6 +20,26 @@ sub echo_available {
     return 1;
 }
 
+sub check_payments {
+    my ($self) = @_;
+    my $problems = FixMyStreet::DB->resultset('Problem')->to_body($self->body->id)->search({
+        -or => [
+            { state => 'unconfirmed', category => 'Garden Subscription' },
+            { state => 'confirmed', category => 'Bulky collection' },
+        ],
+        -not => { extra => { '\?' => 'payment_reference' } },
+        created => [ -and => { '<', \"current_timestamp - '15 minutes'::interval" }, { '>=', \"current_timestamp - '1 hour'::interval" } ],
+    });
+    while (my $row = $problems->next) {
+        my $cobrand = $row->get_cobrand_logged;
+        my $query_id = $row->get_extra_metadata('scpReference') or next; # Problem fetching unique ID from payment provider
+        my ($error, $reference) = $cobrand->cc_check_payment_and_update($query_id, $row);
+        if ($reference) {
+            $row->waste_confirm_payment($reference);
+        }
+    }
+}
+
 sub send_reports {
     my ($self) = @_;
 
