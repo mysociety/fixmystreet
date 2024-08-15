5490ae9e3f9d86e8e2228a6cb14c4a7510a51687
Moray Jones <moray@mysociety.org>
2024-08-15 16:26:09 +0100

    [Surrey] Allow some categories to be reported off road
    Request for two categories to be reportable not on highway
    https://3.basecamp.com/4020879/buckets/36546415/todos/7696151989

---
+++ .cypress/cypress/integration/surrey.js
@@ -0,0 +1,27 @@
+describe('Surrey cobrand', function() {
+    it('contains Surrey configuration', function() {
+        cy.viewport(480, 800);
+        cy.visit('http://surrey.localhost:3001/');
+        cy.contains('Surrey');
+        cy.contains('Report, view, or discuss local problems');
+    });
+});
+
+describe('Reporting not on a road', function() {
+    it('Can report certain categories not on a road', function() {
+        cy.server();
+        cy.route('/report/new/ajax*').as('report-ajax');
+        cy.visit('http://surrey.localhost:3001/report/new?longitude=-0.441269&latitude=51.293415');
+        cy.wait('@report-ajax');
+        cy.pickCategory('Abandoned vehicles');
+        cy.contains('You cannot send Surrey County Council a report');
+        cy.get('#map_sidebar').scrollTo('bottom');
+        cy.get('.js-reporting-page--next:visible').should('be.disabled');
+        cy.pickCategory('Flooding inside a building');
+        cy.contains('You cannot send Surrey County Council a report').should('not.be.visible');
+        cy.get('#map_sidebar').scrollTo('bottom');
+        cy.get('.js-reporting-page--next:visible').should('not.be.disabled');
+    });
+});
+
+
