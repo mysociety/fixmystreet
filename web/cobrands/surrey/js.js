dfa77a95dad7e5fa84fe922a98b33ae9d11443db
Dave Arter <davea@mysociety.org>
2024-09-30 13:25:03 +0100

    [Surrey] Disallow email addresses in problem title/detail

---
+++ web/cobrands/surrey/js.js
@@ -0,0 +1,10 @@
+(function(){
+    if (!window.translation_strings) {
+        return;
+    }
+
+    // Surrey want custom error messages when an email address is present
+    translation_strings.detail.notEmail = 'Report cannot contain an email address or personal information';
+    translation_strings.title.notEmail = 'Report cannot contain an email address or personal information';
+
+})();
