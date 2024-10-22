454f279772027a6b97fcdc30d6333a982400bc90
Matthew Somerville <matthew@mysociety.org>
2024-10-22 09:54:22 +0100

    Add body_handler method to Problem.

--- perllib/FixMyStreet/DB/Result/Problem.pm
+++ perllib/FixMyStreet/DB/Result/Problem.pm
@@ -419,11 +419,7 @@ sub visible_states_remove {
 sub public_asset_id {
     my $self = shift;
 
-    my $current_cobrand = $self->result_source->schema->cobrand;
-
-    my $cobrand_for_problem = $current_cobrand->call_hook(
-        get_body_handler_for_problem => $self );
-
+    my $cobrand_for_problem = $self->body_handler;
     return unless $cobrand_for_problem;
 
     # Should be of the form:
@@ -1043,8 +1039,7 @@ sub updates_sent_to_body {
     return unless $self->send_method_used && $self->send_method_used =~ /Open311/;
 
     # Some bodies only send updates *to* FMS, they don't receive updates.
-    my $cobrand = $self->get_cobrand_logged;
-    my $handler = $cobrand->call_hook(get_body_handler_for_problem => $self);
+    my $handler = $self->body_handler;
     my $ret = $handler && $handler->call_hook(updates_sent_to_body => $self);
     return $ret if defined $ret;
 
@@ -1219,6 +1214,17 @@ has get_cobrand_logged => (
         my $self = shift;
         my $cobrand_class = FixMyStreet::Cobrand->get_class_for_moniker( $self->cobrand );
         return $cobrand_class->new;
+    },
+);
+
+has body_handler => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $cobrand = $self->get_cobrand_logged;
+        my $handler = $cobrand->call_hook(get_body_handler_for_problem => $self);
+        return $handler;
     },
 );
 
