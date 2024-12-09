dfc05047edf37691b86f8620cf9e24b6ad4e8cb3
Lucas <hello@lucascm.dev>
2024-12-09 10:56:37 +0000

    [Gloucester] Initial cobrand setup
    Co-authored-by: Chris Mytton <chrism@mysociety.org>

---
+++ perllib/FixMyStreet/Cobrand/Gloucester.pm
@@ -0,0 +1,102 @@
+=head1 NAME
+
+FixMyStreet::Cobrand::Gloucester - code specific to the Gloucester cobrand
+
+=head1 SYNOPSIS
+
+We integrate with Gloucester's Alloy back end.
+
+=head1 DESCRIPTION
+
+=cut
+
+package FixMyStreet::Cobrand::Gloucester;
+use parent 'FixMyStreet::Cobrand::Whitelabel';
+
+use strict;
+use warnings;
+
+use Moo;
+
+=head2 Defaults
+
+=over 4
+
+=cut
+
+sub council_area_id { '2325' }
+sub council_area { 'Gloucester' }
+sub council_name { 'Gloucester City Council' }
+sub council_url { 'gloucester' }
+
+=item * Users with a gloucester.gov.uk email can always be found in the admin.
+
+=cut
+
+sub admin_user_domain { 'gloucester.gov.uk' }
+
+=item * Allows anonymous reporting
+
+=cut
+
+sub allow_anonymous_reports { 'button' }
+
+=item * Gloucester use their own privacy policy
+
+=cut
+
+sub privacy_policy_url {
+    'https://www.gloucester.gov.uk/about-the-council/data-protection-and-freedom-of-information/data-protection/'
+}
+
+=item * Doesn't allow the reopening of reports
+
+=cut
+
+sub reopening_disallowed { 1 }
+
+=item * We do not send questionnaires.
+
+=cut
+
+sub send_questionnaires { 0 }
+
+=item * Override the default text for entering a postcode or street name
+
+=cut
+
+sub enter_postcode_text {
+    return 'Enter a Gloucester postcode or street name';
+}
+
+=item * TODO: Don't show reports before the go-live date
+
+=cut
+
+# sub cut_off_date { '2024-03-31' }
+
+=pod
+
+=back
+
+=cut
+
+sub disambiguate_location {
+    my $self = shift;
+    my $string = shift;
+
+    my $town = 'Gloucester';
+
+    return {
+        %{ $self->SUPER::disambiguate_location() },
+        town   => $town,
+        centre => '51.8493825813624,-2.24025312382298',
+        span   => '0.0776436939868574,0.12409536555503',
+        bounds => [
+            51.8075803711933, -2.30135343437398,
+            51.8852240651802, -2.17725806881895
+        ],
+    };
+}
+
+1;
