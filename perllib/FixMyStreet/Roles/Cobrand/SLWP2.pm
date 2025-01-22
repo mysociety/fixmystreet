8feb3db67f0c546e8629be22e1cdbf8ca5e30a48
Matthew Somerville <matthew@mysociety.org>
2025-01-22 22:16:49 +0000

    [SLWP] No need to check property location.
    As it is now in a separate Echo instance,
    only relevant results will be returned.

--- perllib/FixMyStreet/Roles/Cobrand/SLWP2.pm
+++ perllib/FixMyStreet/Roles/Cobrand/SLWP2.pm
@@ -19,22 +19,6 @@ use LWP::Simple;
 use MIME::Base64;
 use WasteWorks::Costs;
 
-around look_up_property => sub {
-    my ($orig, $self, $id) = @_;
-    my $data = $orig->($self, $id);
-
-    my $cfg = $self->feature('echo');
-    if ($cfg->{nlpg} && $data->{uprn}) {
-        my $uprn_data = get(sprintf($cfg->{nlpg}, $data->{uprn}));
-        $uprn_data = JSON::MaybeXS->new->decode($uprn_data) if $uprn_data;
-        if (!$uprn_data || $uprn_data->{results}[0]{LPI}{LOCAL_CUSTODIAN_CODE_DESCRIPTION} ne $self->lpi_value) {
-            $self->{c}->stash->{template} = 'waste/missing.html';
-            $self->{c}->detach;
-        }
-    }
-    return $data;
-};
-
 sub waste_staff_choose_payment_method { 1 }
 around waste_cheque_payments => sub {
     my ($orig, $self) = @_;
