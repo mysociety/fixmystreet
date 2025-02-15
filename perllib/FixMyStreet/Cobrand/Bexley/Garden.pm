950cf35b098bf0e4a1f1b7942d488a360bac6b7f
Chris Mytton <chrism@mysociety.org>
2025-02-15 11:47:01 +0000

    [Bexley] Internal Direct Debit collection method for GGW
    - Add support for internal Direct Debit processing in Bexley
    - Implement new direct_debit_internal method in Waste controller
    - Add direct_debit_collection_method method to Default and Bexley cobrands
    - Update garden subscription form to handle internal Direct Debit flow
    - Add bank details form validation and formatting
    - Add tests for Direct Debit submission process

--- perllib/FixMyStreet/Cobrand/Bexley/Garden.pm
+++ perllib/FixMyStreet/Cobrand/Bexley/Garden.pm
@@ -8,7 +8,10 @@ package FixMyStreet::Cobrand::Bexley::Garden;
 
 use Moo::Role;
 with 'FixMyStreet::Roles::Cobrand::SCP',
-     'FixMyStreet::Roles::Cobrand::Paye';
+     'FixMyStreet::Roles::Cobrand::Paye',
+     'FixMyStreet::Roles::Cobrand::AccessPaySuite';
+
+use FixMyStreet::App::Form::Waste::Garden::Bexley;
 
 sub garden_service_name { 'garden waste collection service' }
 
@@ -35,6 +38,85 @@ sub waste_cc_payment_sale_ref {
 sub waste_cc_payment_line_item_ref {
     my ($self, $p) = @_;
     return $p->id;
+}
+
+sub waste_payment_ref_council_code { "BEX" }
+
+sub direct_debit_collection_method { 'internal' }
+
+sub waste_setup_direct_debit {
+    my ($self) = @_;
+    my $c = $self->{c};
+
+    my $report = $c->stash->{report};
+    my $email = $report->user->email;
+
+    my $data = $c->stash->{form_data};
+
+    # Lookup existing customer and contract
+    my $i = $self->get_dd_integration;
+    my $customer = $i->get_customer_by_customer_ref($email);
+
+    if (!$customer) {
+        my $customer_data = {
+            customerRef => $email, # Use email as customer reference
+            email => $email,
+            title => $data->{name_title},
+            firstName => $data->{first_name},
+            surname => $data->{surname},
+            postCode => $data->{post_code},
+            accountNumber => $data->{account_number},
+            bankSortCode => $data->{sort_code},
+            accountHolderName => $data->{account_holder},
+            line1 => $data->{address1},
+            line2 => $data->{address2},
+            line3 => $data->{address3},
+            line4 => $data->{address4},
+        };
+        $customer = $i->create_customer($customer_data);
+    } else {
+        # XXX do we need to check that the existing customer's details (name/address/bank/etc)
+        # match what they've provided to us? If they don't match, what should we do?
+    }
+
+    my $contract_data = {
+        scheduleId => $c->stash->{payment_details}->{dd_schedule_id},
+        start => $c->stash->{payment_date}->strftime('%Y-%m-%dT%H:%M:%S.000'),
+        isGiftAid => 0,
+        terminationType => "Until further notice",
+        atTheEnd => "Switch to further notice",
+        paymentMonthInYear => $c->stash->{payment_date}->month,
+        paymentDayInMonth => 28, # Always the 28th for Bexley
+        amount => $c->stash->{amount},
+        additionalReference => $c->stash->{reference},
+    };
+
+    if ($customer->{error}) {
+        $c->stash->{error} = $customer->{error};
+        return 0;
+    }
+
+    my $contract = $i->create_contract($customer->{Id}, $contract_data);
+
+    if ($contract->{error}) {
+        $c->stash->{error} = $contract->{error};
+        return 0;
+    }
+
+    # Store the customer and contract IDs for future reference
+    $report->set_extra_metadata('direct_debit_customer_id', $customer->{Id});
+    $report->set_extra_metadata('direct_debit_contract_id', $contract->{Id});
+    $report->set_extra_metadata('direct_debit_reference', $contract->{DirectDebitRef});
+    $report->update;
+
+    return 1;
+}
+
+sub waste_garden_subscribe_form_setup {
+    my ($self) = @_;
+
+    # Use a custom form class that includes fields for bank details
+    $self->{c}->stash->{form_class} = 'FixMyStreet::App::Form::Waste::Garden::Bexley';
 }
 
 1;
