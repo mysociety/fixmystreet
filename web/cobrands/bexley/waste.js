b8c925a868aa32d76694abca74c8bf14036f526d
Nik Gupta <vngupta77@gmail.com>
2025-02-16 20:11:19 +0000

    [Bexley][WW] Update displayed GGW bin costs and total depdending on if DD or not.

---
+++ web/cobrands/bexley/waste.js
@@ -0,0 +1,17 @@
+window.garden_waste_first_bin_discount_applies = function() {
+    return $('input[name="payment_method"]:checked').val() === 'direct_debit';
+};
+
+$(function() {
+    // If there's an error when submitting the subscribe form,
+    // the first bin discount won't be displayed even when it should apply.
+    // This is due to how the form is set-up on the server.
+    // We work around this by just checking if the discount should apply
+    // when the page loads and running the cost calculation function if so.
+    window.onload = function() {
+        if (window.garden_waste_first_bin_discount_applies()) {
+            window.garden_waste_bin_cost_new();
+        }
+    };
+    $('#subscribe_details input[name="payment_method"]').on('change', window.garden_waste_bin_cost_new);
+});
