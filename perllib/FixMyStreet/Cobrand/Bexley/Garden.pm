e2680507e02d2e0ebbbba594ad677d0a181ae21e
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2025-02-17 13:22:31 +0000

    [Bexley][WW] Pass 'total_containers' (=bins_wanted) to O311 for GGW subs

--- perllib/FixMyStreet/Cobrand/Bexley/Garden.pm
+++ perllib/FixMyStreet/Cobrand/Bexley/Garden.pm
@@ -177,6 +177,10 @@ sub waste_garden_sub_params {
         $c->set_param( 'customer_external_ref', $srv->{customer_external_ref} );
         $c->set_param( 'due_date', $due_date_str );
         $c->set_param( 'reason', $reason );
+
+    } elsif ( $data->{category} eq 'Garden Subscription' ) {
+        $c->set_param( 'total_containers', $data->{bins_wanted} );
+
     }
 }
 
