a1f6b6aba17d871e275873fb85f4416823c67373
Matthew Somerville <matthew@mysociety.org>
2025-05-14 21:15:17 +0100

    [Buckinghamshire] Move parish ID list to database.

--- perllib/FixMyStreet/Cobrand/FixMyStreet.pm
+++ perllib/FixMyStreet/Cobrand/FixMyStreet.pm
@@ -640,6 +640,7 @@ sub add_extra_areas_for_admin {
     my $extra = $self->feature('extra_parishes') || [];
     push @extra, @$extra;
     my $ids_string = join ",", @extra;
+    return $areas unless $ids_string;
     my $extra_areas = mySociety::MaPit::call('areas', [ $ids_string ]);
     my %all_areas = ( %$areas, %$extra_areas );
     return \%all_areas;
