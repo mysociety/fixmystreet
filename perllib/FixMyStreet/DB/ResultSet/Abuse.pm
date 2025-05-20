5f5813ff0397465057299d6cf8213e9d1044b671
Matthew Somerville <matthew@mysociety.org>
2025-05-20 16:02:52 +0100

    Add option for checking against UserCheck.

--- perllib/FixMyStreet/DB/ResultSet/Abuse.pm
+++ perllib/FixMyStreet/DB/ResultSet/Abuse.pm
@@ -15,6 +15,9 @@ use base 'FixMyStreet::DB::ResultSet';
 
 use strict;
 use warnings;
+use JSON::MaybeXS;
+use LWP::UserAgent;
+use Try::Tiny;
 
 =head2 unsafe
 
@@ -40,15 +43,48 @@ sub check {
     my ($rs, $email, $phone) = @_;
 
     my @check;
+    my $domain;
     if ($email) {
-        my ($domain) = $email =~ m{ @ (.*) \z }x;
+        ($domain) = $email =~ m{ @ (.*) \z }x;
         push @check, $email, $domain;
     }
     if ($phone) {
         push @check, $phone;
     }
     my $existing = $rs->search( { email => \@check } )->first;
-    return $existing ? !$existing->safe : 0;
+    return !$existing->safe if $existing;
+
+    my $check = usercheck($domain);
+    if ($check eq 'bad') {
+        $rs->create({ email => $domain, safe => 0 });
+        return 1;
+    } elsif ($check eq 'good') {
+        $rs->create({ email => $domain, safe => 1 });
+    }
+    return 0;
+}
+
+=head2 usercheck
+
+Returns true if we should check UserCheck and the check
+comes back positive for a domain that is disposable/blocked.
+
+=cut
+
+sub usercheck {
+    my $domain = shift;
+
+    my $api_key = FixMyStreet->config('CHECK_USERCHECK');
+    return "off" unless $api_key;
+
+    my $url = "https://api.usercheck.com/domain/$domain";
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout(2);
+    $ua->default_header(User_Agent => 'FixMyStreet/1.0', Authorization => "Bearer $api_key");
+    my $response = $ua->get($url);
+    my $check = try { decode_json($response->decoded_content) };
+    return "fail" if !$check || $check->{status} != 200;
+    return $check->{disposable} || $check->{blocklisted} ? "bad" : "good";
 }
 
 1;
