51ead3fcb61e70d47f7bfae936a486f27f93b6c0
Matthew Somerville <matthew@mysociety.org>
2025-05-20 16:02:30 +0100

    Add safe/created columns to abuse table.

--- perllib/FixMyStreet/DB/ResultSet/Abuse.pm
+++ perllib/FixMyStreet/DB/ResultSet/Abuse.pm
@@ -16,6 +16,18 @@ use base 'FixMyStreet::DB::ResultSet';
 use strict;
 use warnings;
 
+=head2 unsafe
+
+Returns those entries with safe set to false.
+
+=cut
+
+sub unsafe {
+    my $rs = shift;
+    $rs = $rs->search({ safe => 0 });
+    return $rs;
+}
+
 =head2 check
 
 Given an email and/or phone number, query the abuse table to see
@@ -36,7 +48,7 @@ sub check {
         push @check, $phone;
     }
     my $existing = $rs->search( { email => \@check } )->first;
-    return $existing ? 1 : 0;
+    return $existing ? !$existing->safe : 0;
 }
 
 1;
