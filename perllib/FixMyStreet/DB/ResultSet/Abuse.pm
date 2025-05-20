04d2017a59b3094fae6cc3ed0eefffa7e9626714
Matthew Somerville <matthew@mysociety.org>
2025-05-20 14:58:28 +0100

    Refactor abuse table checking function.

---
+++ perllib/FixMyStreet/DB/ResultSet/Abuse.pm
@@ -0,0 +1,42 @@
+=head1 NAME
+
+FixMyStreet::DB::ResultSet::Abuse
+
+=head1 DESCRIPTION
+
+Helper functions for querying the abuse table in the database
+
+=head1 METHODS
+
+=cut
+
+package FixMyStreet::DB::ResultSet::Abuse;
+use base 'FixMyStreet::DB::ResultSet';
+
+use strict;
+use warnings;
+
+=head2 check
+
+Given an email and/or phone number, query the abuse table to see
+if they are present (plus the email's domain if email was given)
+and return true if they are.
+
+=cut
+
+sub check {
+    my ($rs, $email, $phone) = @_;
+
+    my @check;
+    if ($email) {
+        my ($domain) = $email =~ m{ @ (.*) \z }x;
+        push @check, $email, $domain;
+    }
+    if ($phone) {
+        push @check, $phone;
+    }
+    my $existing = $rs->search( { email => \@check } )->first;
+    return $existing ? 1 : 0;
+}
+
+1;
--- perllib/FixMyStreet/Roles/DB/Abuser.pm
+++ perllib/FixMyStreet/Roles/DB/Abuser.pm
@@ -6,26 +6,14 @@ use Moo::Role;
 
     $bool = $alert->is_from_abuser(  );
 
-Returns true if the user's email or its domain is listed in the 'abuse' table.
+Returns true if the user's email/domain/phone is listed in the 'abuse' table.
 
 =cut
 
 sub is_from_abuser {
     my $self = shift;
-
-    my $email = $self->user->email;
-    my $domain;
-    ($domain) = $email =~ m{ @ (.*) \z }x if $email;
-    my $phone = $self->user->phone;
-
-    # search for an entry in the abuse table
     my $abuse_rs = $self->result_source->schema->resultset('Abuse');
-
-    return
-         $abuse_rs->find( { email => $email } )
-      || $abuse_rs->find( { email => $domain } )
-      || $abuse_rs->find( { email => $phone } )
-      || undef;
+    return $abuse_rs->check($self->user->email, $self->user->phone);
 }
 
 1;
