e6582d048659905c6131009baf31e798b3ca2cbb
Matthew Somerville <matthew@mysociety.org>
2025-05-29 07:34:31 +0100

    [Waste] Different small items maximum from bulky.

--- perllib/FixMyStreet/App/Controller/Admin/Waste.pm
+++ perllib/FixMyStreet/App/Controller/Admin/Waste.pm
@@ -80,6 +80,7 @@ sub edit : Chained('body') : PathPart('') : Args(0) {
                 base_price => 'int',
                 daily_slots => 'int',
                 items_per_collection_max => 'int',
+                small_items_per_collection_max => 'int',
                 band1_price => 'int',
                 band1_max => 'int',
                 free_mode => 'bool',
@@ -94,7 +95,7 @@ sub edit : Chained('body') : PathPart('') : Args(0) {
                 } elsif ($keys{$_} eq 'int') {
                     if ($val && $val ne $val+0) {
                         $c->stash->{errors}->{site_wide} = "Not an integer";
-                    } elsif ($_ eq 'items_per_collection_max' && $val > 200) {
+                    } elsif ($_ =~ /items_per_collection_max/ && $val && $val > 200) {
                         $c->stash->{errors}->{site_wide} = "Maximum items per collection cannot be more than 200";
                     }
                     $new_cfg->{$_} = $val;
@@ -220,7 +221,7 @@ sub stash_body_config_json : Private {
     } else {
         $c->stash->{body_config_json} = JSON->new->utf8(1)->pretty->canonical->encode($cfg);
     }
-    foreach (qw(free_mode per_item_costs per_item_min_collection_price base_price daily_slots items_per_collection_max food_bags_disabled show_location_page band1_price band1_max show_individual_notes)) {
+    foreach (qw(free_mode per_item_costs per_item_min_collection_price base_price daily_slots small_items_per_collection_max items_per_collection_max food_bags_disabled show_location_page band1_price band1_max show_individual_notes)) {
         $c->stash->{$_} = $c->get_param($_) || $cfg->{$_};
     }
 }
