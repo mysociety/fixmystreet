4ea11493bfe5fc11e350247ffebe1111c00001af
Dave Arter <davea@mysociety.org>
2025-06-13 10:27:19 +0100

    [Aberdeenshire] Switch to OSM geocoder

--- perllib/FixMyStreet/Cobrand/Aberdeenshire.pm
+++ perllib/FixMyStreet/Cobrand/Aberdeenshire.pm
@@ -149,8 +149,11 @@ sub disambiguate_location {
     my $self = shift;
     my $string = shift;
 
+    my $town = 'Aberdeenshire';
+
     return {
         %{ $self->SUPER::disambiguate_location() },
+        town   => $town,
         centre => '57.24185467,-2.62923456',
         span   => '0.95461349,2.03725374',
         bounds => [
@@ -158,6 +161,25 @@ sub disambiguate_location {
             57.70174199, -1.76439269,
         ],
     };
+}
+
+=item * Uses the OSM geocoder
+
+=cut
+
+sub get_geocoder { 'OSM' }
+
+
+=item * Make a few improvements to the display of geocoder results
+
+Remove 'Aberdeenshire' and 'Alba / Scotland', skip any that don't mention Aberdeenshire at all
+
+=cut
+
+sub geocoder_munge_results {
+    my ($self, $result) = @_;
+    $result->{display_name} = '' unless $result->{display_name} =~ /Aberdeenshire/;
+    $result->{display_name} =~ s#, Aberdeenshire, Alba / Scotland##;
 }
 
 1;
