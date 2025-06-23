8f441c8b855e49bd262959e280b03218f22fa09f
Matthew Somerville <matthew@mysociety.org>
2025-06-23 18:08:13 +0100

    [Bexley] Specific bulky terms & conditions

--- perllib/FixMyStreet/App/Form/Waste/Bulky/Cancel.pm
+++ perllib/FixMyStreet/App/Form/Waste/Bulky/Cancel.pm
@@ -38,8 +38,17 @@ has_field confirm => (
         my $text = 'I confirm I wish to cancel my bulky collection';
         if ($cobrand->moniker eq 'kingston' || $cobrand->moniker eq 'sutton') {
             $text = 'I acknowledge that the collection fee is non-refundable and would like to cancel my bulky collection';
+        } elsif ($cobrand->moniker eq 'bexley') {
+            my $report = $_[0]->form->{c}->stash->{cancelling_booking};
+            my $refund = sprintf( '%.2f',
+                $report->get_extra_field_value('payment') / 100 );
+            $text = <<"HERE";
+I confirm I wish to cancel my bulky waste collection and receive a full refund of £$refund.
+<br><br>
+Funds will be returned to the card you used to pay for the booking. Please note that refunds can take up to 5 working days to process.
+HERE
         }
-        return $text;
+        return FixMyStreet::Template::SafeString->new($text);
     },
 );
 
