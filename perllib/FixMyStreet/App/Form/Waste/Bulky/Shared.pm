5899e2fc8c14ab5bd06fe38b894b5a8d926f483b
Matthew Somerville <matthew@mysociety.org>
2025-06-23 18:08:13 +0100

    [Bexley] Specific bulky terms (wording TBC).

--- perllib/FixMyStreet/App/Form/Waste/Bulky/Shared.pm
+++ perllib/FixMyStreet/App/Form/Waste/Bulky/Shared.pm
@@ -273,6 +273,12 @@ has_field tandc => (
 <br>&bull; I confirm I have read the information for the <a href="' . $link . '" target="_blank">bulky waste service</a>';
         } elsif ($c->cobrand->moniker eq 'brent') {
             $label = 'I have read and agree to the <a href="' . $link . '" target="_blank">terms and conditions</a> and understand any additional items presented that do not meet the terms and conditions will not be collected';
+        } elsif ($c->cobrand->moniker eq 'bexley') {
+            $label = '&bull; I confirm that the submitted information is current and correct, and any misrepresentations could lead to a cancellation of the arranged service without refund.
+<br>&bull; I understand that collections can take place any time after 6am.
+<br>&bull; I understand that the arranged service cannot be edited or cancelled once payment has been submitted and refunds are not provided.
+<br>&bull; I understand that only the items added to the booking will be taken. Items must be accessible on the collection day and left in a neat and safe manner. Items cannot be left for collection on the public highway.
+<br>&bull; I understand I may be subject to further charges if proof of pension cannot be provided when requested.';
         } else {
             $label = 'I have read the <a href="' . $link . '" target="_blank">bulky waste collection</a> page on the council’s website';
         }
