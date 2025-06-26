a77deead8ee27a14858482be1368a4b3ba14a60a
Matthew Somerville <matthew@mysociety.org>
2025-06-26 18:29:40 +0100

    [Bexley] Add pension and disability questions.

---
+++ perllib/FixMyStreet/App/Form/Waste/Bulky/Bexley.pm
@@ -0,0 +1,47 @@
+package FixMyStreet::App::Form::Waste::Bulky::Bexley;
+
+use utf8;
+use HTML::FormHandler::Moose;
+extends 'FixMyStreet::App::Form::Waste::Bulky';
+
+has_page intro => (
+    intro => 'bulky/intro.html',
+    fields => ['continue'],
+    update_field_list => sub {
+        my $form = shift;
+        my $data = $form->saved_data;
+        $data->{_pension_check} = 1;
+        return {};
+    },
+    next => 'about_you',
+);
+
+has_page pension_check => (
+    fields => ['pension', 'disability', 'continue'],
+    next => 'choose_date_earlier',
+);
+
+has_field pension => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    required => 1,
+    label => 'Does anyone at the property receive a state pension?',
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+has_field disability => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    required => 1,
+    label => 'Do you have a physical disability?',
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+1;
+
