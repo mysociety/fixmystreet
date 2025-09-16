753709f3c45d7ec4f343bbb15a62ae3c8f438557
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2025-09-16 11:15:19 +0100

    [Bexley][WW] Add initial customer reference page for GGW

--- perllib/FixMyStreet/App/Form/Waste/Garden/Renew/Bexley.pm
+++ perllib/FixMyStreet/App/Form/Waste/Garden/Renew/Bexley.pm
@@ -6,7 +6,10 @@ use HTML::FormHandler::Moose;
 extends 'FixMyStreet::App::Form::Waste::Garden::Renew';
 
 with 'FixMyStreet::App::Form::Waste::AccessPaySuiteBankDetails';
-with 'FixMyStreet::App::Form::Waste::Garden::AboutYou::Bexley';
+with 'FixMyStreet::App::Form::Waste::Garden::Verify::Bexley';
+
+has_page customer_reference =>
+    ( customer_reference( continue_field => 'continue_choice' ) );
 
 has_page about_you =>
     ( about_you( continue_field => 'continue_choice', next_page => 'intro' ) );
