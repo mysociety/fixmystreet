21302a0024d10f83b19fc77c67b9012b4ac641d5
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2025-09-18 16:59:09 +0100

    [Bexley][WW] Split 'name' field into first & last names for GGW

---
+++ perllib/FixMyStreet/App/Form/Waste/AboutYou/Shared.pm
@@ -0,0 +1,50 @@
+package FixMyStreet::App::Form::Waste::AboutYou::Shared;
+
+use utf8;
+
+use FixMyStreet::SMS;
+use HTML::FormHandler::Moose::Role;
+
+has non_staff_user => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $c = $self->{c};
+        return $c->user if $c->user_exists && !($c->user->from_body || $c->user->is_superuser);
+    },
+);
+
+has_field phone => (
+    type => 'Text',
+    label => 'Telephone number',
+    validate_method => sub {
+        my $self = shift;
+        my $parsed = FixMyStreet::SMS->parse_username($self->value);
+        $self->add_error('Please provide a valid phone number')
+            unless $parsed->{phone};
+    }
+);
+
+sub default_phone {
+    my $self = shift;
+    if (my $user = $self->non_staff_user) {
+        return $user->phone;
+    }
+}
+
+has_field email => (
+    type => 'Email',
+    tags => {
+        hint => 'Provide an email address so we can send you order status updates'
+    },
+);
+
+sub default_email {
+    my $self = shift;
+    if (my $user = $self->non_staff_user) {
+        return $user->email;
+    }
+}
+
+1;
