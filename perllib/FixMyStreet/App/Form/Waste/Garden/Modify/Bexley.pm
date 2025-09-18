3d359e5eb66753d5cb72770e1ceaf2fd966e1905
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2025-09-18 11:44:18 +0100

    [Bexley][WW] Refactor to have 'Shared' GGW forms

--- perllib/FixMyStreet/App/Form/Waste/Garden/Modify/Bexley.pm
+++ perllib/FixMyStreet/App/Form/Waste/Garden/Modify/Bexley.pm
@@ -3,9 +3,14 @@ package FixMyStreet::App::Form::Waste::Garden::Modify::Bexley;
 use utf8;
 
 use HTML::FormHandler::Moose;
-extends 'FixMyStreet::App::Form::Waste::Garden::Modify';
+extends 'FixMyStreet::App::Form::Waste::Garden::Modify::Shared';
 
 with 'FixMyStreet::App::Form::Waste::Garden::Verify::Bexley';
+
+# Inherits pages from Shared:
+# intro
+# summary
+# done
 
 has_page customer_reference =>
     ( customer_reference( continue_field => 'continue' ) );
@@ -13,7 +18,7 @@ has_page customer_reference =>
 has_page about_you =>
     ( about_you( continue_field => 'continue', next_page => 'alter' ) );
 
-has_page alter => remove_about_you_fields(
-    FixMyStreet::App::Form::Waste::Garden::Modify::alter() );
+has_page alter =>
+    FixMyStreet::App::Form::Waste::Garden::Modify::Shared::alter();
 
 1;
