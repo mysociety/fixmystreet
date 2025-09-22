374e70fb93aaf4ead79a2bbba2a308d91aff089d
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2025-09-22 22:40:46 +0100

    [Bexley][WW] Check customer ref and name against Agile data

--- perllib/FixMyStreet/App/Form/Waste/Garden/Modify/Bexley.pm
+++ perllib/FixMyStreet/App/Form/Waste/Garden/Modify/Bexley.pm
@@ -12,11 +12,17 @@ with 'FixMyStreet::App::Form::Waste::Garden::Verify::Bexley';
 # summary
 # done
 
-has_page customer_reference =>
-    ( customer_reference( continue_field => 'continue' ) );
+has_page customer_reference => (
+    customer_reference(
+        continue_field        => 'continue',
+        next_page_if_verified => 'alter',
+    )
+);
 
 has_page about_you =>
     ( about_you( continue_field => 'continue', next_page => 'alter' ) );
+
+has_page verify_failed => ( verify_failed() );
 
 has_page alter =>
     FixMyStreet::App::Form::Waste::Garden::Modify::Shared::alter();
