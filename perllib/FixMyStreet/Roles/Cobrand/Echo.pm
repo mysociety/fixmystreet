ef57f0377e7db0b3c3aa812d632916b22a46c4dc
Matthew Somerville <matthew@mysociety.org>
2025-09-30 15:36:14 +0100

    [Sutton] Always store fetched events.
    It is possible a property could have an open container request, but no
    previous instance of collection.

--- perllib/FixMyStreet/Roles/Cobrand/Echo.pm
+++ perllib/FixMyStreet/Roles/Cobrand/Echo.pm
@@ -304,17 +304,18 @@ sub bin_services_for_address {
             end_date => $schedules->{end_date},
             $self->moniker eq 'brent' ? (timeband => $_->{timeband}) : (),
         };
+
+        if ($self->moniker eq 'sutton') {
+            # Sutton needs to know about events from before the last collection in case
+            # there have been missed container escalations that are still relevant
+            $row->{all_events} = $events->filter({ service => $service_id });
+        }
+
         if ($row->{last}) {
             my $ref = join(',', @{$row->{last}{ref}});
             $task_ref_to_row{$ref} = $row;
 
             $row->{report_allowed} = $self->within_working_days($row->{last}{date}, 2);
-
-            if ($self->moniker eq 'sutton') {
-                # Sutton needs to know about events from before the last collection in case there
-                # have been missed container escalations that are still relevant
-                $row->{all_events} = $events->filter({ service => $service_id });
-            }
 
             my $events_unit = $self->_parse_events($calls->{"GetEventsForObject ServiceUnit $_->{Id}"});
             $row->{events} = $events->combine($events_unit)->filter({ service => $service_id, since => $row->{last}{date} });
