106708f7e65736a10adaf0522493752626068c3b
Dave Arter <davea@mysociety.org>
2025-12-19 12:25:03 +0000

    [Dumfries] Enable myAccount SSO
    Refactors Aberdeenshire code to new role, making it much simpler for
    future cobrands

--- perllib/FixMyStreet/Cobrand/Aberdeenshire.pm
+++ perllib/FixMyStreet/Cobrand/Aberdeenshire.pm
@@ -31,6 +31,7 @@ certain fields. These roles implement that behaviour.
 
 with 'FixMyStreet::Roles::ConfirmOpen311';
 with 'FixMyStreet::Roles::ConfirmValidation';
+with 'FixMyStreet::Roles::MyGovScotOIDC';
 
 =head2 Defaults
 
@@ -60,51 +61,6 @@ sub admin_user_domain { 'aberdeenshire.gov.uk' }
 =cut
 
 sub privacy_policy_url { 'https://publications.aberdeenshire.gov.uk/acblobstorage/168aac73-5139-4622-a980-7a9436c3e0a3/cusersspellascdocumentsroads-pn.pdf' }
-
-=item * Single sign on is enabled from the cobrand feature 'oidc_login'
-
-=cut
-
-sub social_auth_enabled {
-    my $self = shift;
-
-    return $self->feature('oidc_login') ? 1 : 0;
-}
-
-=item * Extract the user's details from the OIDC token
-
-=cut
-
-sub user_from_oidc {
-    my ($self, $payload, $access_token) = @_;
-
-    my $name = '';
-    my $email = '';
-
-    # Payload doesn't include user's name so fetch it from
-    # the OIDC userinfo endpoint.
-    my $cfg = $self->feature('oidc_login');
-    if ($access_token && $cfg->{userinfo_uri}) {
-        my $ua = LWP::UserAgent->new;
-        my $response = $ua->get(
-            $cfg->{userinfo_uri},
-            Authorization => 'Bearer ' . $access_token,
-        );
-        my $user = decode_json($response->decoded_content);
-        if ($user->{fname} && $user->{lname}) {
-            $name = join(" ", $user->{fname}, $user->{lname});
-        }
-        if ($user->{emailaddress}) {
-            $email = $user->{emailaddress};
-        }
-    }
-
-    # In case we didn't get email from the claims above, default to value
-    # present in payload. NB name is not available in this manner.
-    $email ||= $payload->{sub} ? lc($payload->{sub}) : '';
-
-    return ($name, $email);
-}
 
 sub abuse_reports_only { 1 }
 
