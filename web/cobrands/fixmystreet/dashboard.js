2e1d6336b2676a0c8dd1a0afab587a2d7ee1b3c4
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2026-01-02 17:00:48 +0000

    Hide categories with zero reports on dashboard page

--- web/cobrands/fixmystreet/dashboard.js
+++ web/cobrands/fixmystreet/dashboard.js
@@ -1,23 +1,41 @@
 $(function(){
 
-console.log('new dboard JS');
+    // hide/show categories with zero reports
+    var $table_with_zeroes = $('table tr.is-zero').closest('table');
+
+    if ($table_with_zeroes.length == 1) {
+        var $toggle_zeroes_btn = $("<input type='submit' class='btn' value='Show categories with zero reports' id='toggle-zeroes-btn' style='margin:1em 0;'/>");
+        $table_with_zeroes.before($toggle_zeroes_btn);
+        $toggle_zeroes_btn.on('click', function(e){
+            e.preventDefault();
+            var $cols = $table_with_zeroes.find('tr.is-zero');
+            if ($cols.first().is(':visible')) {
+                $cols.hide();
+                $(this).prop("value", 'Show categories with zero reports');
+            } else {
+                $cols.show();
+                $(this).prop("value", 'Hide categories with zero reports');
+            }
+        });
+    }
 
     // hide/show deleted contact categories
-    var $table_with_deleted_contacts = $('table tr.is-deleted th.contact-category').closest('table');
-console.log($table_with_deleted_contacts);
+    var $table_with_deleted_contacts = $('table tr.is-deleted').closest('table');
+
     if ($table_with_deleted_contacts.length == 1) {
-        var $toggle_deleted_btn = $("<input type='submit' class='btn' value='Show deleted contacts' id='toggle-deleted-contacts-btn' style='margin:1em 0;'/>");
+        var $toggle_deleted_btn = $("<input type='submit' class='btn' value='Show deleted categories' id='toggle-deleted-contacts-btn' style='margin:1em 0;'/>");
         $table_with_deleted_contacts.before($toggle_deleted_btn);
         $toggle_deleted_btn.on('click', function(e){
             e.preventDefault();
             var $cols = $table_with_deleted_contacts.find('tr.is-deleted');
             if ($cols.first().is(':visible')) {
                 $cols.hide();
-                $(this).prop("value", 'Show deleted contacts');
+                $(this).prop("value", 'Show deleted categories');
             } else {
                 $cols.show();
-                $(this).prop("value", 'Hide deleted contacts');
+                $(this).prop("value", 'Hide deleted categories');
             }
         });
     }
+
 });
