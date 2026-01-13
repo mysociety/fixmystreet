b9c6b619bd79c365246ff1e973e21c4071ccf016
Chris Mytton <chrism@mysociety.org>
2026-01-13 13:48:23 +0000

    [TfL] Add scaffold licence application form
    Add foundation for TfL's digital licence application system, replacing
    their current process.
    
    - Licence controller that discovers and routes to specific licence form
      types (scaffold, etc.)
    - Base Licence form role providing shared functionality for all licence
      forms (file uploads, summary display, field formatting)
    - Scaffold licence form as the first example
    - Form submission creates non-public Problem records with licence data
      stored in extra metadata

---
+++ perllib/FixMyStreet/App/Form/Licence.pm
@@ -0,0 +1,29 @@
+package FixMyStreet::App::Form::Licence;
+
+use strict;
+use warnings;
+
+# Discover and load all form classes in FixMyStreet::App::Form::Licence::*
+use Module::Pluggable
+    sub_name    => '_forms',
+    search_path => 'FixMyStreet::App::Form::Licence',
+    require     => 1;
+
+my @ALL_FORM_CLASSES = __PACKAGE__->_forms;
+
+# Build licence types from discovered form classes
+sub licence_types {
+    my $self = shift;
+    my %types;
+    for my $class (@ALL_FORM_CLASSES) {
+        next unless $class->can('type') && $class->can('name');
+        my $type = $class->type;
+        $types{$type} = {
+            class => $class,
+            name  => $class->name,
+        };
+    }
+    return \%types;
+}
+
+1;
