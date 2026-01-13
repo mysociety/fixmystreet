97a7034dbb58d77b03103291cab1cd789d56540e
Chris Mytton <chrism@mysociety.org>
2026-01-13 13:48:23 +0000

    [TfL] Add scaffold licence application form
    Add foundation for TfL's digital licence application system, replacing
    their current process.
    
    - Licence controller that discovers and routes to specific licence form
      types (scaffold, etc.)
    - Base Licence form role providing shared functionality for all licence
      forms (file uploads, summary display, field formatting)
    - Scaffold licence form as the first example
    - Form submission creates non-public Problem records with licence data
      stored in extra metadata

---
+++ perllib/FixMyStreet/App/Form/Licence/Fields/Dates.pm
@@ -0,0 +1,90 @@
+package FixMyStreet::App::Form::Licence::Fields::Dates;
+
+use utf8;
+use HTML::FormHandler::Moose::Role;
+
+=head1 NAME
+
+FixMyStreet::App::Form::Licence::Fields::Dates - Date fields for licence forms
+
+=head1 DESCRIPTION
+
+Provides standard date fields used by all TfL licence forms:
+proposed_start_date, proposed_duration
+
+Includes validation:
+- Start date must be at least 4 weeks from today
+
+=cut
+
+has_field proposed_start_date => (
+    type => 'DateTime',
+    label => 'Proposed start date',
+    required => 1,
+    tags => { hint => 'Working dates must be set in four‑weekly periods. For example, 1/1/2026 to 29/1/2026.' },
+    messages => {
+        datetime_invalid => 'Please enter a valid date',
+    },
+    validate_method => sub {
+        my $field = shift;
+        return if $field->has_errors; # Called even if already failed
+
+        my $dt = $field->value or return;
+        my $today = DateTime->today(time_zone => FixMyStreet->local_time_zone);
+        my $min_date = $today->clone->add(weeks => 4);
+
+        if ($dt < $min_date) {
+            $field->add_error('Start date must be at least 4 weeks from today');
+        }
+    },
+);
+
+has_field 'proposed_start_date.day' => ( type => 'MonthDay' );
+has_field 'proposed_start_date.month' => ( type => 'Month' );
+has_field 'proposed_start_date.year' => ( type => 'Year' );
+
+has_field proposed_duration => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    label => 'Number of weeks required',
+    required => 1,
+    options => [
+        { label => '2 weeks (mobile scaffold only)', value => 2 },
+        { label => '4 weeks', value => 4 },
+        { label => '8 weeks', value => 8 },
+        { label => '12 weeks', value => 12 },
+        { label => '16 weeks', value => 16 },
+        { label => '20 weeks', value => 20 },
+        { label => '24 weeks', value => 24 },
+        { label => '28 weeks', value => 28 },
+        { label => '32 weeks', value => 32 },
+        { label => '36 weeks', value => 36 },
+        { label => '40 weeks', value => 40 },
+        { label => '44 weeks', value => 44 },
+        { label => '48 weeks', value => 48 },
+        { label => '52 weeks', value => 52 },
+    ],
+
+    validate_method => sub {
+        my $field = shift;
+        return if $field->has_errors; # Called even if already failed
+
+        my $saved_data = $field->form->saved_data;
+        my $weeks = $field->value;
+
+        my $start_date = $field->form->field('proposed_start_date');
+        if (!$start_date->has_errors) {
+            my $end = $start_date->value->clone->add(weeks => $weeks);
+            $saved_data->{proposed_end_date} = $end;
+        }
+
+        # Make sure if weeks is set to 2, we set the type to mobile tower
+        if ($weeks == 2) {
+            $saved_data->{scaffold_type} = 'Scaffold (Mobile Tower)';
+        }
+    },
+);
+
+has_field year_warning => ( type => 'Notice', label => '<span id="js-proposed_end_date"></span> All licences are limited to a duration of one year.', required => 0, widget => 'NoRender' );
+
+1;
