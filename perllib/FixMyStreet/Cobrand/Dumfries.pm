5632dd6d7f917d0fbb3f249166c3174959882c3f
Chris Mytton <chrism@mysociety.org>
2026-01-28 09:48:36 +0000

    [Dumfries] Add custom postcode text with reference number hint
    Add a custom enter_postcode_text method that includes guidance
    for users to search by reference number in addition to postcode
    and street name/area.

--- perllib/FixMyStreet/Cobrand/Dumfries.pm
+++ perllib/FixMyStreet/Cobrand/Dumfries.pm
@@ -24,4 +24,12 @@ sub council_area { return 'Dumfries and Galloway'; }
 sub council_name { return 'Dumfries and Galloway Council'; }
 sub council_url { return 'dumfries'; }
 
+=item * Custom postcode text which includes hint about reference number search
+
+=cut
+
+sub enter_postcode_text {
+    'Enter a Dumfries and Galloway post code or street name and area, or a reference number of a problem previously reported'
+}
+
 1;
