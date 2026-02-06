c7e5645735442b101100161cc0f723d605d76e11
Matthew Somerville <matthew@mysociety.org>
2026-02-06 19:03:21 +0000

    [TfL] PDF licence generation.
    Link at end of process, and include as attachment in email.

--- perllib/FixMyStreet/App/Form/Licence.pm
+++ perllib/FixMyStreet/App/Form/Licence.pm
@@ -27,4 +27,73 @@ sub licence_types {
     return \%types;
 }
 
+sub generate_pdf {
+    my ($self, $report) = @_;
+
+    my $type = $report->get_extra_metadata('licence_type') or return;
+    my $licence_config = $self->licence_types->{lc $type} or return;
+
+    my $form = $licence_config->{class}->new(
+        page_name => 'intro',
+        saved_data => $report->extra,
+        no_preload => 1,
+    );
+
+    require FixMyStreet::PDF;
+    my $pdf = FixMyStreet::PDF->new(
+        title => $report->title . ', FMS' . $report->id,
+        font => 'Johnston100',
+    );
+
+    # Simplest solution, have a template that has the HTML in,
+    # and let it place it all. This works, but has orphans.
+    #my $input = $c->render_fragment('licence/summary_pdf.html');
+    #$pdf->plot_line(undef, 'black', $input);
+
+    # So instead, generate the PDF line by line and check for
+    # orphan headings as we go
+
+    my ($rc, $next_y);
+    ($rc, $next_y) = $pdf->plot_line($next_y, 'black', '<h1>' . $form->title . '</h1>');
+    ($rc, $next_y) = $pdf->plot_line($next_y, 'black', '<p>Application reference: FMS' . $report->id . '</p>');
+    ($rc, $next_y) = $pdf->plot_line($next_y, 'black', '<p>Date of application: ' . $report->created->strftime('%d/%m/%Y') . '</p>');
+
+    foreach my $page (@{$form->fields_for_display}) {
+        next if $page->{hide};
+        next if $page->{stage} eq 'intro' || $page->{stage} eq 'done';
+
+        my $page_title = "<h2>$page->{title}</h2>";
+        my ($rc, $post_title_y) = $pdf->plot_line($next_y, 'white', $page_title);
+        if ($rc) {
+            # Want to start heading on new page
+            $next_y = undef;
+        }
+
+        my $first_field = 1;
+        foreach my $field (@{$page->{fields}}) {
+            next if $field->{hide};
+            my $line = "<p style='margin-top:6pt'><strong>$field->{desc}";
+            $line .= ':' unless $field->{desc} =~ /[?:.]$/;
+            $line .= "</strong> $field->{pretty}</p>";
+            $line =~ s/<br>/<\/p><p>/g; # Term checkboxes
+
+            if ($first_field) {
+                $first_field = 0;
+                if ($post_title_y) {
+                    # Can we fit the line of text in after the heading?
+                    ($rc) = $pdf->plot_line($post_title_y, 'white', $line);
+                    if ($rc) {
+                        # Heading would be an orphan, want to start it on new page
+                        $next_y = undef;
+                    }
+                }
+                ($rc, $next_y) = $pdf->plot_line($next_y, '#0019A8', $page_title);
+            }
+            ($rc, $next_y) = $pdf->plot_line($next_y, 'black', $line);
+        }
+    }
+
+    return $pdf->to_string;
+}
+
 1;
