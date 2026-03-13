be58f3f4a8f27f27bea77f7040a3120534e7bf67
Matthew Somerville <matthew@mysociety.org>
2026-03-13 15:26:45 +0000

    [TfL] New licence custom emails.
    Attach all attachments to submit email.
    Make page name sections of detail text bold.

--- perllib/FixMyStreet/Cobrand/TfL.pm
+++ perllib/FixMyStreet/Cobrand/TfL.pm
@@ -27,7 +27,9 @@ are assigned an FMS account and user role when logging in.
 
 with 'FixMyStreet::Roles::BoroughEmails';
 
+use MIME::Types;
 use POSIX qw(strcoll);
+use Path::Tiny;
 
 use FixMyStreet::MapIt;
 use mySociety::ArrayUtils;
@@ -704,7 +706,46 @@ around 'munge_sendreport_params' => sub {
 
     $params->{From} = [ $self->do_not_reply_email, $self->contact_name ];
     delete $params->{'Reply-To'} if $params->{'Reply-To'};
+
+    if ($row->cobrand_data eq 'licence') {
+        my @attachments;
+        my $subdir = 'tfl-licence-' . $row->get_extra_metadata('licence_type');
+        # Attach documents
+        my $mime_types = MIME::Types->new;
+        # Any field beginning upload_ is perhaps a file
+        my $extra = $row->get_extra_metadata;
+        my @files = grep { $_ } map { $extra->{$_} } grep { /^upload_/ } keys %$extra;
+        foreach (@files) {
+            my $filename = $_->{filenames}[0];
+            my $id = $_->{files};
+            my $dir = FixMyStreet->config('PHOTO_STORAGE_OPTIONS')->{UPLOAD_DIR};
+            $dir = path($dir, $subdir)->absolute(FixMyStreet->path_to());
+            my $data = path($dir, $id)->slurp_raw;
+            my $type = $mime_types->mimeTypeOf($filename);
+            $type = 'application/vnd.ms-outlook' if $filename =~ /\.msg$/i; # Special case not handled
+            $type ||= 'application/octet-stream';
+            push @attachments, {
+                body => $data,
+                attributes => {
+                    filename => $filename,
+                    content_type => $type,
+                    encoding => 'base64', # quoted-printable ends up with newlines corrupting binary data
+                    name => $filename,
+                },
+            };
+        }
+
+        $params->{_attachments_} = \@attachments;
+    }
 };
+
+# We write the detail as [page name]\nfields\n\n[page name]..
+# Change those page names to bold in email
+sub licence_detail {
+    my ($self, $text) = @_;
+    $text =~ s/(<p style="margin: 0 0 0.8em 0;">\s*)\[(.*?)\]/$1<strong>$2<\/strong>/g;
+    return FixMyStreet::Template::SafeString->new($text);
+}
 
 sub is_hardcoded_category {
     my ($self, $category) = @_;
