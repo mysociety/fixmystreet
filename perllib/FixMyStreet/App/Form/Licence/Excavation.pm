6387b6125a555952fdb9c5d8776ddd45bd2d5887
Matthew Somerville <matthew@mysociety.org>
2026-03-16 16:23:02 +0000

    [TfL] fixup confirmation text change

--- perllib/FixMyStreet/App/Form/Licence/Excavation.pm
+++ perllib/FixMyStreet/App/Form/Licence/Excavation.pm
@@ -174,7 +174,7 @@ has_page terms => (
         'terms_accepted',
         'continue'
     ],
-    title => 'Terms and conditions',
+    title => 'Terms and conditions confirmation',
     next => 'uploads',
 );
 
