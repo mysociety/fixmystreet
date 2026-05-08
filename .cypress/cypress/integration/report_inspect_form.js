bbca65d266a6258a724fa20c51644ebe9bb06393
Matthew Somerville <matthew@mysociety.org>
2026-05-08 11:02:18 +0100

    fixup! Handle new 'group__category' selection for JS for report inspect

--- .cypress/cypress/integration/report_inspect_form.js
+++ .cypress/cypress/integration/report_inspect_form.js
@@ -17,9 +17,8 @@ describe('Changing category or group on report inspect form', function() {
         cy.get('[name=category]').select('Abandoned vehicles');
         // For some reason, two state dropdowns are found
         // and the first is not visible (?).
-        // Similarly for response_template.
         cy.get('[name=state]').last().select('Not responsible');
-        cy.get('[name=response_template]').last().should('have.value', 'This report is not the responsibility of the council and will be passed to the relevant organisation.');
+        cy.get('[name=public_update]').should('have.value', 'This report is not the responsibility of the council and will be passed to the relevant organisation.');
         cy.get('[name=priority]').should('contain', 'Priority 1');
         cy.get('[name=priority]').should('contain', 'Priority 2');
         cy.get('[name=priority]').should('contain', 'Priority 3');
@@ -28,7 +27,7 @@ describe('Changing category or group on report inspect form', function() {
         // Category with single group
         cy.get('[name=category]').select('Skips');
         cy.get('[name=state]').last().select('Not responsible');
-        cy.get('[name=response_template]').last().should('have.value', 'This report with a category under one group is not the responsibility of the council.');
+        cy.get('[name=public_update]').last().should('have.value', 'This report with a category under one group is not the responsibility of the council.');
         cy.get('[name=priority]').should('not.contain', 'Priority 1');
         cy.get('[name=priority]').should('contain', 'Priority 2');
         cy.get('[name=priority]').should('contain', 'Priority 3');
@@ -37,7 +36,7 @@ describe('Changing category or group on report inspect form', function() {
         // Category with multiple groups
         cy.get('[name=category]').select('Streets__Litter');
         cy.get('[name=state]').last().select('Not responsible');
-        cy.get('[name=response_template]').last().should('have.value', 'This report with a category under multiple groups is not the responsibility of the council.');
+        cy.get('[name=public_update]').last().should('have.value', 'This report with a category under multiple groups is not the responsibility of the council.');
         cy.get('[name=priority]').should('not.contain', 'Priority 1');
         cy.get('[name=priority]').should('contain', 'Priority 2');
         cy.get('[name=priority]').should('contain', 'Priority 3');
