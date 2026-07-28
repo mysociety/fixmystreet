ea3eda087869c2519d5c70e61f20a5f4afb33410
Struan Donald <struan@exo.org.uk>
2026-07-28 17:27:39 +0100

    [Kingston/Sutton] open bulky missed reporting after 6pm on day
    If the collection is still open at 6pm on the day of collection the user
    should be able to report it as missed. Also tightens up detecting when
    it's before 6pm on the day for providing messages

--- perllib/FixMyStreet/Roles/Cobrand/SLWP2.pm
+++ perllib/FixMyStreet/Roles/Cobrand/SLWP2.pm
@@ -1005,4 +1005,25 @@ sub bulky_location_photo_prompt {
     'Help us find your items by attaching a photo of where the items will be left for collection.';
 }
 
+=item bulky_open_overdue
+
+Returns true if the booking is open and after 6pm on the day of the collection.
+
+=cut
+
+sub bulky_open_overdue {
+    my ($self, $event) = @_;
+
+    if ($event->{state} eq 'open' && $self->_bulky_collection_overdue($event)) {
+        return 1;
+    }
+}
+
+sub _bulky_collection_overdue {
+    my $collection_due_date = $_[1]->{date};
+    $collection_due_date->truncate(to => 'day')->set_hour(18);
+    my $today = DateTime->now->set_time_zone(FixMyStreet->local_time_zone);
+    return $today > $collection_due_date;
+}
+
 1;
