6ca7a5837fa19a5528aa39ffb5d55143fea7bbb6
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2026-08-14 15:10:41 +0100

    [CRT] Enforce selection of tunnel (or portal)
    And make sure tunnel messaging disappears if another category
    selected.

--- web/cobrands/fixmystreet-uk-councils/assets.js
+++ web/cobrands/fixmystreet-uk-councils/assets.js
@@ -728,14 +728,14 @@ fixmystreet.assets.canalrivertrust.tunnel_found = function(layer, feature) {
 };
 
 fixmystreet.assets.canalrivertrust.tunnel_not_found = function(layer) {
-    fixmystreet.message_controller.road_not_found(layer);
+    // 'return true' criterion forces trigger of responsibility_on()
+    fixmystreet.message_controller.road_not_found(layer, function() {return true;});
+
     if ( fixmystreet.assets.selectedFeature() ) {
         // This may never be reached, as selecting a spot asset implies
         // a road has also been found
         return;
     }
-
-    fixmystreet.assets.named_select_action_not_found.call(layer);
 };
 
 /* Central Bedfordshire */
