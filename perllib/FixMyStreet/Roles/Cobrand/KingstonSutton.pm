51ac745a2b1982c5b37fea363cf18625d247266e
Struan Donald <struan@exo.org.uk>
2026-08-19 17:29:38 +0100

    [Kingston/Sutton] text for closed disputes on enquiry problem page

--- perllib/FixMyStreet/Roles/Cobrand/KingstonSutton.pm
+++ perllib/FixMyStreet/Roles/Cobrand/KingstonSutton.pm
@@ -456,7 +456,11 @@ sub _setup_missed_collection_disputes_for_service {
             $row->{dispute}{too_late} = 1;
         }
     } elsif ($missed_event && $dispute_event) {
-        $row->{dispute}{open} = $dispute_event;
+        if ($dispute_event->{closed}) {
+            $row->{dispute}{closed} = $dispute_event;
+        } else {
+            $row->{dispute}{open} = $dispute_event;
+        }
     }
 }
 
@@ -586,7 +590,11 @@ sub _setup_scheduled_collection_disputes_for_service {
             }
         }
     } else {
-        $row->{dispute}{open} = 1;
+        if ($dispute_event->{closed}) {
+            $row->{dispute}{closed} = $dispute_event;
+        } else {
+            $row->{dispute}{open} = $dispute_event;
+        }
     }
 }
 
