c3401f561b35916b5a4f9853fc8d89b100da2d19
Dave Arter <davea@mysociety.org>
2026-08-20 12:06:30 +0100

    fixup! [Merton] Add DVLA lookup for abandoned vehicles
    Add ability to store actual MOT/Tax status value from API

--- web/cobrands/fixmystreet-uk-councils/dvla.js
+++ web/cobrands/fixmystreet-uk-councils/dvla.js
@@ -45,6 +45,8 @@ const FIELDS = {
         'reg': 'vehicle_registration_number',
         'make': 'vehicle_make_model', // DVLA doesn't give us model
         'colour': 'vehicle_colour',
+        'motStatus': 'vehicle_mot_status',
+        'taxStatus': 'vehicle_tax_status',
         'trailer': ' If the vehicle is parked wrongly, you may be able to <a href="https://www.merton.gov.uk/streets-parking-transport/parking/report">report it as a parking offence</a>.'
     }
 };
@@ -229,7 +231,7 @@ function dvla_lookup(e) {
             const height = wrapper.getBoundingClientRect().height;
             document.querySelector('.js-reporting-page--active').style.paddingBottom = height + 'px';
         } else {
-            ['make', 'colour', 'reg', 'make_and_colour'].forEach(name => {
+            ['make', 'colour', 'reg', 'make_and_colour', 'motStatus', 'taxStatus'].forEach(name => {
                 if (fields[name] && data[name]) {
                     let field = document.querySelector('input[name*="' + fields[name] + '"]');
                     if (field) {
