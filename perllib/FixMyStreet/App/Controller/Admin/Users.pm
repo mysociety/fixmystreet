3a47370cb0318ebe78a777796dc31ba65827592b
Chris Mytton <chrism@mysociety.org>
2026-08-25 17:31:53 +0100

    Restrict superuser role admin to the cobrand's body
    When being viewed by a superuser, change the roles list, the role filter
    on the user list, and the body field on the role form to use
    $c->cobrand->body where there is one.
    
    This fixes the confusing long list of all roles when viewing on a
    cobrand as a superuser.
    
    Means that roles belonging to other bodies are no longer editable from a
    cobrand's admin.

--- perllib/FixMyStreet/App/Controller/Admin/Users.pm
+++ perllib/FixMyStreet/App/Controller/Admin/Users.pm
@@ -104,11 +104,15 @@ sub index :Path : Args(0) {
 
     my $rs;
     if ($c->user->is_superuser) {
-        $rs = $c->model('DB::Role')->search_rs({}, { join => 'body', order_by => ['body.name', 'me.name'] });
+        # On a cobrand associated with a body, only show that body's roles
+        my $body = $c->cobrand->body;
+        my $search = $body ? { 'me.body_id' => $body->id } : {};
+        $rs = $c->model('DB::Role')->search_rs($search, { join => 'body', order_by => ['body.name', 'me.name'] });
         $rs = $rs->search(undef, {
             '+columns' => { 'body.msgstr' => \'COALESCE(translation_name.msgstr, body.name)' },
             join => { body => 'translation_name' },
         });
+        $c->stash->{group_roles_by_body} = !$body;
     } elsif ($c->user->from_body) {
         $rs = $c->user->from_body->roles->search_rs({}, { order_by => 'name' });
     }
