51a30b5a074533438be84d26ce279704c7a8d4a9
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2026-09-24 18:50:18 +0100

    [CRT][NH] Include web/cobrands/fixmystreet-uk-councils/js.js
    This prevents new report form from jumping back to the first
    page if (e.g.) there is an error on the user details page.

--- web/cobrands/canalrivertrust/js.js
+++ web/cobrands/canalrivertrust/js.js
@@ -1,3 +1,4 @@
 (function(){
     translation_strings.report_a_problem_btn.default = 'Report';
+    translation_strings.name.validName = 'Please enter your full name. If you do not wish your name to be shown on the site, untick the box below.';
 })();
