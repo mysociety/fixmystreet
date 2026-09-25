8d5b69a4d514e47e1dfcd4e5961bbcaf0710dc42
Matthew Somerville <matthew@mysociety.org>
2026-09-25 17:06:48 +0100

    fixup! [FMS] Test needs predictable output

--- perllib/FixMyStreet/Script/SendDaemon.pm
+++ perllib/FixMyStreet/Script/SendDaemon.pm
@@ -58,7 +58,7 @@ sub look_for_update {
         ->search($params, {
             for => \'UPDATE SKIP LOCKED',
             rows => 1,
-            order_by => FixMyStreet->test_mode ? 'id' : \'RANDOM()'
+            order_by => \'RANDOM()'
         })->single or return;
 
     my $problem = $comment->problem;
